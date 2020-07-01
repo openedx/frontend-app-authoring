be48a55f9a89e0fd385ae78ad95859447902d374
Zachary Hancock <zhancock@edx.org>
2020-07-01 15:22:19 -0400

    adds transifex config and test string (#3)

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 
 export default function ExamplePage() {
   return (
     <main>
       <div className="container-fluid">
         <h1>Example Page</h1>
-        <p>Hello world!</p>
+        <p>
+          <FormattedMessage
+            id="authoring.example.hello"
+            defaultMessage="Hello World!"
+          />
+        </p>
       </div>
     </main>
   );
