31317e7fdb2371e398873cb19a8a3dae9c7a02e0
Alie Langston <alangsto@wellesley.edu>
2020-07-20 11:29:33 -0400

    Added validation logic for parity
    Removed merge conflict
    
    resolved merge conflict
    
    added validation logic for enforcing proctortrack escalation email
    
    added dependencies for email validator and testing, added code and tests for requiring valid email
    
    added validation for editing proctoring provider field
    
    added validation for editing proctoring provider field
    
    Add error handling code to the Exam settings page. Also include spinners for loading (#10)
    
    updated help text
    
    updated comment
    
    added unit tests
    
    updates for requested changes
    
    removed unnecessary line
    
    trying to see if tests will rerun
    
    testing for travis build
    
    adding another test
    
    adding another test for travis
    
    uncommented last test
    
    updated tests
    
    trying to fix test
    
    removed tests
    
    This change would fix tests so they are actually doing the testing job (#11)
    
    Update the error messages based on copy provided by Deen (#13)

--- src/proctored-exam-settings/ProctoredExamSettings.jsx
+++ src/proctored-exam-settings/ProctoredExamSettings.jsx
@@ -1,20 +1,29 @@
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
+import EmailValidator from 'email-validator';
+import moment from 'moment';
 import {
-  Button, CheckBox, Input, ValidationFormGroup,
+  Button, CheckBox, Input, ValidationFormGroup, StatusAlert,
 } from '@edx/paragon';
+import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
 
 import StudioApiService from '../data/services/StudioApiService';
 
 function ExamSettings(props) {
+  const [loading, setLoading] = useState(true);
+  const [loaded, setLoaded] = useState(false);
+  const [loadingConnectionError, setLoadingConnectionError] = useState(false);
+  const [loadingPermissionError, setLoadingPermissionError] = useState(false);
   const [enableProctoredExams, setEnableProctoredExams] = useState(true);
   const [allowOptingOut, setAllowOptingOut] = useState(false);
   const [proctoringProvider, setProctoringProvider] = useState('');
   const [availableProctoringProviders, setAvailableProctoringProviders] = useState([]);
   // TODO: we'll probably want to hide this field when proctortrack is not selected; currently,
   // this causes some errors in the browser console
-  const [proctortrackEscalationEmail, setProctortrackEscalationEmail] = useState(null);
+  const [proctortrackEscalationEmail, setProctortrackEscalationEmail] = useState('');
   const [createZendeskTickets, setCreateZendeskTickets] = useState(false);
+  const [proctortrackEscalationEmailError, setProctortrackEscalationEmailError] = useState('');
+  const [courseStartDate, setCourseStartDate] = useState('');
 
   function onEnableProctoredExamsChange(event) {
     setEnableProctoredExams(event);
@@ -44,14 +53,166 @@ function ExamSettings(props) {
   }
 
   function onButtonClick() {
+    if (proctoringProvider === 'proctortrack' && !EmailValidator.validate(proctortrackEscalationEmail)) {
+      setProctortrackEscalationEmailError('A valid escalation email must be provided if '
+                                            + 'Proctortrack is the selected provider.');
+    } else {
+      setProctortrackEscalationEmailError('');
     // TODO: implement POST
+    }
+  }
+
+  function cannotEditProctoringProvider() {
+    const currentDate = moment(moment()).format('YYYY-MM-DD[T]hh:mm:ss[Z]');
+    const isAfterCourseStart = currentDate > courseStartDate;
+
+    const isAdmin = getAuthenticatedUser().administrator;
+
+    // if the user is  not an administrator and it is after the course start date, user cannot edit proctoring provider
+    return !isAdmin && isAfterCourseStart;
+  }
+
+  function isDisabledOption(provider) {
+    let markDisabled = false;
+    if (cannotEditProctoringProvider()) {
+      markDisabled = provider !== proctoringProvider;
+    }
+    return markDisabled;
   }
 
   function getProctoringProviderOptions(providers) {
     return providers.reduce(
       (accumulator, currentValue) => {
-        accumulator.push({ value: currentValue, label: currentValue }); return accumulator;
+        accumulator.push({
+          value: currentValue,
+          label: currentValue,
+          disabled: isDisabledOption(currentValue),
+        }); return accumulator;
       }, [],
+    );
+  }
+
+  function renderContent() {
+    return (
+      <div className="content-container">
+        <ValidationFormGroup
+          for="enableProctoredExams"
+          helpText="If checked, proctored exams are enabled in your course."
+        >
+          <CheckBox
+            id="enableProctoredExams"
+            name="enable_proctored_exams"
+            label="Enable Proctored Exams"
+            checked={enableProctoredExams}
+            onChange={onEnableProctoredExamsChange}
+          />
+        </ValidationFormGroup>
+        <ValidationFormGroup
+          for="allowingOptingOut"
+          helpText="If checked, learners can choose to take proctored exams without proctoring. If not checked, all learners must take the exam with proctoring."
+        >
+          <CheckBox
+            id="allowingOptingOut"
+            name="allow_opting_out"
+            label="Allow Opting Out of Proctored Exams"
+            checked={allowOptingOut}
+            onChange={onAllowOptingOutChange}
+          />
+        </ValidationFormGroup>
+        <ValidationFormGroup
+          for="proctoringProvider"
+          helpText={cannotEditProctoringProvider() ? ('Proctoring provider cannot be modified after course start date.') : ('Select the proctoring provider you want to use for this course run.')}
+        >
+          <label htmlFor="proctoringProvider">Proctoring Provider</label>
+          <Input
+            type="select"
+            id="proctoringProvider"
+            name="proctoring_provider"
+            options={getProctoringProviderOptions(availableProctoringProviders)}
+            value={proctoringProvider}
+            onChange={onProctoringProviderChange}
+          />
+        </ValidationFormGroup>
+        <ValidationFormGroup
+          for="proctortrackEscalationEmail"
+          helpText="Required if 'proctortrack' is selected as your proctoring provider. Enter an email address to be contacted by the support team whenever there are escalations (e.g. appeals, delayed reviews, etc.)."
+        >
+          <label htmlFor="proctortrackEscalationEmail">Proctortrack Escalation Email</label>
+          <Input
+            type="text"
+            id="proctortrackEscalationEmail"
+            name="proctortrack_escalation_email"
+            value={proctortrackEscalationEmail}
+            onChange={onProctortrackEscalationEmailChange}
+          />
+        </ValidationFormGroup>
+        <ValidationFormGroup
+          for="createZendeskTickets"
+          helpText="If checked, a Zendesk ticket will be created for suspicious attempts."
+        >
+          <CheckBox
+            id="createZendeskTickets"
+            name="create_zendesk_tickets"
+            label="Create Zendesk Tickets For Suspicious Exam Attempts"
+            checked={createZendeskTickets}
+            onChange={onCreateZendeskTicketsChange}
+          />
+        </ValidationFormGroup>
+        { proctortrackEscalationEmailError
+        && (
+        <StatusAlert
+          alertType="danger"
+          className="proctortrackEscalationEmailError"
+          dialog={proctortrackEscalationEmailError}
+          open
+          dismissible={false}
+        />
+        )}
+        <Button
+          className="btn-primary mb-3"
+          onClick={onButtonClick}
+        >
+          Submit
+        </Button>
+      </div>
+    );
+  }
+
+  function renderLoading() {
+    return (
+      <div className="Loading-container">
+        <div
+          className="d-flex justify-content-center align-items-center flex-column"
+          style={{
+            height: '50vh',
+          }}
+        >
+          <div className="spinner-border text-primary" role="status" />
+        </div>
+      </div>
+    );
+  }
+
+  function renderConnectionError() {
+    return (
+      <div className="justify-content-center align-items-center flex-column">
+        <div className="alert alert-danger">
+          We encountered a technical error when loading this page.
+          This might be a temporary issue, so please try again in a few minutes.
+          If the problem persists, please go to <a href="https://support.edx.org/hc/en-us">edX Support Page</a> for help.
+        </div>
+      </div>
+    );
+  }
+
+  function renderPermissionError() {
+    return (
+      <div className="justify-content-center align-items-center flex-column">
+        <div className="alert alert-danger">
+          You are not authorized to view this page. If you feel you should have access,
+          please reach out to your course team admin to be given access.
+        </div>
+      </div>
     );
   }
 
@@ -61,7 +222,9 @@ function ExamSettings(props) {
         .then(
           response => {
             const proctoredExamSettings = response.data.proctored_exam_settings;
-
+            setLoaded(true);
+            setLoading(false);
+            setCourseStartDate(response.data.course_start_date);
             setEnableProctoredExams(proctoredExamSettings.enable_proctored_exams);
             setAllowOptingOut(proctoredExamSettings.allow_proctoring_opt_out);
             setProctoringProvider(proctoredExamSettings.proctoring_provider);
@@ -69,8 +232,17 @@ function ExamSettings(props) {
             setProctortrackEscalationEmail(proctoredExamSettings.proctoring_escalation_email);
             setCreateZendeskTickets(proctoredExamSettings.create_zendesk_tickets);
           },
+        ).catch(
+          errorDetails => {
+            if (errorDetails.customAttributes.httpErrorStatus === 403) {
+              setLoadingPermissionError(true);
+            } else {
+              setLoadingConnectionError(true);
+            }
+            setLoading(false);
+            setLoaded(false);
+          },
         );
-      // TODO: handle error
     }, [],
   );
 
@@ -79,75 +251,12 @@ function ExamSettings(props) {
       <h2 className="mb-1">
         Proctored Exam Settings
       </h2>
-      <ValidationFormGroup
-        for="enableProctoredExams"
-        helpText="If checked, proctored exams are enabled in your course."
-      >
-        <CheckBox
-          id="enableProctoredExams"
-          name="enable_proctored_exams"
-          label="Enable Proctored Exams"
-          checked={enableProctoredExams}
-          onChange={onEnableProctoredExamsChange}
-        />
-      </ValidationFormGroup>
-      <ValidationFormGroup
-        for="allowingOptingOut"
-        helpText="If checked, learners can choose to take proctored exams without proctoring. If not checked, all learners must take the exam with proctoring."
-      >
-        <CheckBox
-          id="allowingOptingOut"
-          name="allow_opting_out"
-          label="Allow Opting Out of Proctored Exams"
-          checked={allowOptingOut}
-          onChange={onAllowOptingOutChange}
-        />
-      </ValidationFormGroup>
-      <ValidationFormGroup
-        for="proctoringProvider"
-        helpText="Select the proctoring provider you want to use for this course run."
-      >
-        <label htmlFor="proctoringProvider">Proctoring Provider</label>
-        <Input
-          type="select"
-          id="proctoringProvider"
-          name="proctoring_provider"
-          options={getProctoringProviderOptions(availableProctoringProviders)}
-          value={proctoringProvider}
-          onChange={onProctoringProviderChange}
-        />
-      </ValidationFormGroup>
-      <ValidationFormGroup
-        for="proctortrackEscalationEmail"
-        helpText="Required if 'proctortrack' is selected as your proctoring provider. Enter an email address to be contacted by the support team whenever there are escalations (e.g. appeals, delayed reviews, etc.)."
-      >
-        <label htmlFor="proctortrackEscalationEmail">Proctortrack Escalation Email</label>
-        <Input
-          type="text"
-          id="proctortrackEscalationEmail"
-          name="proctortrack_escalation_email"
-          value={proctortrackEscalationEmail}
-          onChange={onProctortrackEscalationEmailChange}
-        />
-      </ValidationFormGroup>
-      <ValidationFormGroup
-        for="createZendeskTickets"
-        helpText="If checked, a Zendesk ticket will be created for suspicious attempts."
-      >
-        <CheckBox
-          id="createZendeskTickets"
-          name="create_zendesk_tickets"
-          label="Create Zendesk Tickets For Suspicious Exam Attempts"
-          checked={createZendeskTickets}
-          onChange={onCreateZendeskTicketsChange}
-        />
-      </ValidationFormGroup>
-      <Button
-        className="btn-primary mb-3"
-        onClick={onButtonClick}
-      >
-        Submit
-      </Button>
+      <div>
+        {loading ? renderLoading() : null}
+        {loaded ? renderContent() : null}
+        {loadingConnectionError ? renderConnectionError() : null}
+        {loadingPermissionError ? renderPermissionError() : null}
+      </div>
     </div>
   );
 }
