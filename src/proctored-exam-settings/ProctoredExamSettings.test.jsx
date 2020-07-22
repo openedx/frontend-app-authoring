b40204285cd8c4851c3499aa289ae6ca8745a12d
Simon Chen <schen@edx.org>
2020-07-22 14:53:04 -0400

    This change would fix tests so they are actually doing the testing job (#11)

--- src/proctored-exam-settings/ProctoredExamSettings.test.jsx
+++ src/proctored-exam-settings/ProctoredExamSettings.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import * as auth from '@edx/frontend-platform/auth';
 import ProctoredExamSettings from './ProctoredExamSettings';
@@ -8,55 +7,42 @@ const defaultProps = {
   courseId: 'course-v1%3AedX%2BDemoX%2BDemo_Course',
 };
 
-const waitForComponentToPaint = async (wrapper, callback) => {
-  await act(async () => {
-    await new Promise(resolve => setTimeout(resolve, 0));
-    wrapper.update();
-    callback(wrapper);
-  });
-};
-
 describe('ProctoredExamSettings', () => {
-  beforeEach(() => {
-    auth.getAuthenticatedHttpClient = jest.fn(() => ({
-      get: async () => ({
-        data: {
-          proctored_exam_settings: {
-            enable_proctored_exams: true,
-            allow_proctoring_opt_out: false,
-            proctoring_provider: 'proctortrack',
-            proctoring_escalation_email: 'test@example.com',
-            create_zendesk_tickets: false,
-          },
-          available_proctoring_providers: ['software_secure', 'proctortrack', 'mockproc'],
+  auth.getAuthenticatedHttpClient = jest.fn(() => ({
+    get: async () => ({
+      data: {
+        proctored_exam_settings: {
+          enable_proctored_exams: true,
+          allow_proctoring_opt_out: false,
+          proctoring_provider: 'proctortrack',
+          proctoring_escalation_email: 'test@example.com',
+          create_zendesk_tickets: false,
         },
-        catch: () => {},
-      }),
-    }));
-    auth.getAuthenticatedUser = jest.fn(() => ({ userId: 3 }));
+        available_proctoring_providers: ['software_secure', 'proctortrack', 'mockproc'],
+      },
+      catch: () => {},
+    }),
+  }));
+
+  auth.getAuthenticatedUser = jest.fn(() => ({ userId: 3 }));
+  const component = mount(<ProctoredExamSettings {...defaultProps} />);
+
+  beforeEach(() => {
+    component.update();
   });
 
   it('updates zendesk ticket field if proctortrack is provider', () => {
-    const component = mount(<ProctoredExamSettings {...defaultProps} />);
-    waitForComponentToPaint(component, (comp) => {
-      comp.find('select#proctoringProvider').simulate('change', { target: { value: 'proctortrack' } });
-      expect(comp.find('input#createZendeskTickets').prop('checked')).toEqual(false);
-    });
+    component.find('select#proctoringProvider').simulate('change', { target: { value: 'proctortrack' } });
+    expect(component.find('input#createZendeskTickets').prop('checked')).toEqual(false);
   });
 
   it('updates zendesk ticket field if software_secure is provider', () => {
-    const component = mount(<ProctoredExamSettings {...defaultProps} />);
-    waitForComponentToPaint(component, (comp) => {
-      comp.find('select#proctoringProvider').simulate('change', { target: { value: 'software_secure' } });
-      expect(comp.find('input#createZendeskTickets').prop('checked')).toEqual(true);
-    });
+    component.find('select#proctoringProvider').simulate('change', { target: { value: 'software_secure' } });
+    expect(component.find('input#createZendeskTickets').prop('checked')).toEqual(true);
   });
 
   it('does not update zendesk ticket field for any other provider', () => {
-    const component = mount(<ProctoredExamSettings {...defaultProps} />);
-    waitForComponentToPaint(component, (comp) => {
-      comp.find('select#proctoringProvider').simulate('change', { target: { value: 'mockproc' } });
-      expect(comp.find('input#createZendeskTickets').prop('checked')).toEqual(true);
-    });
+    component.find('select#proctoringProvider').simulate('change', { target: { value: 'mockproc' } });
+    expect(component.find('input#createZendeskTickets').prop('checked')).toEqual(true);
   });
 });
