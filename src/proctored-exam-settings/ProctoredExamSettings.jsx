bf90463ec2044b84caf25704a4a1cf4bf4e5c055
Bianca Severino <bseverino@edx.org>
2020-12-08 09:48:21 -0500

    Rebrand (#39)

--- src/proctored-exam-settings/ProctoredExamSettings.jsx
+++ src/proctored-exam-settings/ProctoredExamSettings.jsx
@@ -273,7 +273,7 @@ function ExamSettings(props) {
         {/* PROCTORING PROVIDER */}
         { enableProctoredExams && (
         <Form.Group controlId="formProctoringProvider">
-          <Form.Label>
+          <Form.Label as="legend">
             <FormattedMessage
               id="authoring.examsettings.provider.label"
               defaultMessage="Proctoring Provider"
@@ -368,7 +368,8 @@ function ExamSettings(props) {
           </fieldset>
         )}
         <Button
-          className="btn-primary mb-3"
+          variant="primary"
+          className="mb-3"
           data-test-id="submissionButton"
           type="submit"
           disabled={submissionInProgress}
@@ -534,7 +535,7 @@ function ExamSettings(props) {
 
   return (
     <div className="container">
-      <h2 className="mb-1">
+      <h2 className="mt-3 mb-2">
         Proctored Exam Settings
       </h2>
       <div>
