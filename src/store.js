8edd23d0ef91d5f30829d5651a6175842d6116fb
David Joy <djoy@edx.org>
2021-01-04 11:06:00 -0500

    Adding model-store from frontend-app-learning.
    This will let us save data from the server in a normalized way in redux, reducing boilerplate in React components.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { reducer as modelsReducer } from './generic/model-store';
 export default function initializeStore() {
   return configureStore({
     reducer: {
+      models: modelsReducer,
     },
   });
 }
