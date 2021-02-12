6614871dc66a6ef3a9f874dde442418cbb678fe0
David Joy <djoy@edx.org>
2021-02-12 17:06:03 -0500

    Using more paragon components - Card, CardGrid, and DataTable

--- src/pages-and-resources/discussions/DiscussionAppList.jsx
+++ src/pages-and-resources/discussions/DiscussionAppList.jsx
@@ -1,13 +1,18 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { Button, Container, Row } from '@edx/paragon';
+import {
+  Button, Card, CardGrid, Input,
+} from '@edx/paragon';
 import { useDispatch, useSelector } from 'react-redux';
 import { history } from '@edx/frontend-platform';
+import classNames from 'classnames';
 
 import { useLocation } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 import messages from './messages';
-import DiscussionAppCard from './DiscussionAppCard';
+
 import FeaturesTable from './FeaturesTable';
 import { useModel, useModels } from '../../generic/model-store';
 import { fetchApps } from './data/thunks';
@@ -42,36 +47,80 @@ function DiscussionAppList({ courseId, intl }) {
 
   return (
     <div className="m-5">
-      <Container fluid className="text-info-500">
-        <h6 className="my-4 text-center">{intl.formatMessage(messages.heading)}</h6>
-
-        <Row>
-          {apps.map(app => (
-            <DiscussionAppCard
-              key={app.id}
-              app={app}
-              selected={app.id === selectedAppId}
-              clickHandler={handleSelectApp}
+      <h2 className="my-4 text-center">{intl.formatMessage(messages.heading)}</h2>
+      <CardGrid
+        columnSizes={{
+          xs: 12,
+          sm: 6,
+          lg: 4,
+        }}
+      >
+        {apps.map(app => (
+          <Card
+            tabIndex={app.isAvailable ? '-1' : ''}
+            onClick={() => { if (app.isAvailable) { handleSelectApp(app.id); } }}
+            onKeyPress={() => { if (app.isAvailable) { handleSelectApp(app.id); } }}
+            role="radio"
+            aria-checked={app.id === selectedAppId}
+            style={{
+              cursor: 'pointer',
+            }}
+            className={classNames({
+              'border-primary': app.id === selectedAppId,
+            })}
+          >
+            <div
+              className="position-absolute"
+              style={{
+                // This positioning of 0.75rem aligns the checkbox with the top of the logo
+                top: '0.75rem',
+                right: '0.75rem',
+              }}
+            >
+              {app.isAvailable ? (
+                <Input readOnly type="checkbox" checked={app.id === selectedAppId} />
+              ) : (
+                <FontAwesomeIcon icon={faLock} />
+              )}
+            </div>
+            <Card.Img
+              variant="top"
+              style={{
+                maxHeight: 100,
+                objectFit: 'contain',
+              }}
+              className="py-3 pl-3 pr-5"
+              src={app.logo}
+              alt={intl.formatMessage(messages.appLogo, {
+                name: app.name,
+              })}
             />
-          ))}
-        </Row>
+            <Card.Body>
+              <Card.Title>{app.name}</Card.Title>
+              <Card.Text>{app.description}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              {app.supportLevel}
+            </Card.Footer>
+          </Card>
+        ))}
+      </CardGrid>
 
-        <div className="d-flex justify-content-between align-items-center">
-          <h2 className="my-3">
-            {intl.formatMessage(messages.supportedFeatures)}
-          </h2>
-          {selectedAppId && (
+      <div className="d-flex justify-content-between align-items-center">
+        <h2 className="my-3">
+          {intl.formatMessage(messages.supportedFeatures)}
+        </h2>
+        {selectedAppId && (
           <Button variant="primary" onClick={handleConfigureApp}>
             {intl.formatMessage(messages.configureApp, { name: selectedApp.name })}
           </Button>
-          )}
-        </div>
+        )}
+      </div>
 
-        <FeaturesTable
-          apps={apps}
-          features={features}
-        />
-      </Container>
+      <FeaturesTable
+        apps={apps}
+        features={features}
+      />
     </div>
   );
 }
--- src/pages-and-resources/discussions/FeaturesTable.jsx
+++ src/pages-and-resources/discussions/FeaturesTable.jsx
@@ -2,39 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { DataTable } from '@edx/paragon';
 
 export default function FeaturesTable({ apps, features }) {
   return (
-    <div className="table-responsive features-table border border-info-300 p-3 mb-4">
-      <table className="w-100">
-        <thead>
-          <tr>
-            <th>&nbsp;</th>
-            {apps.map(app => (
-              <th key={app.id} className="text-center py-3">
-                <h5>{app.name}</h5>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {features.map(feature => (
-            <tr key={feature.id}>
-              <th key={feature.id} className="py-3">
-                {feature.name}
-              </th>
-              {apps.map(app => (
-                <td className="text-center py-3" key={app.id}>
-                  {app.featureIds.includes(feature.id) && (
-                    <FontAwesomeIcon icon={faCheck} />
-                  )}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <DataTable
+        itemCount={features.length}
+        data={features.map(feature => {
+          const appChecks = {};
+          apps.forEach(app => {
+            appChecks[app.id] = app.featureIds.includes(feature.id) ? <FontAwesomeIcon icon={faCheck} /> : null;
+          });
+
+          return {
+            feature: feature.name,
+            ...appChecks,
+          };
+        })}
+        columns={[
+          {
+            Header: '',
+            accessor: 'feature',
+          },
+          ...apps.map(app => ({
+            Header: app.name,
+            accessor: app.id,
+          })),
+        ]}
+      >
+        <DataTable.Table />
+      </DataTable>
+    </>
   );
 }
 
