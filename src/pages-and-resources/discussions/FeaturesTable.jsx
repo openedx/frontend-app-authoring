8dc82edbc23a19b1154984b6f1a2be0a675c5dc2
David Joy <djoy@edx.org>
2021-02-22 13:58:43 -0500

    Adding comments to FeaturesTable.

--- src/pages-and-resources/discussions/FeaturesTable.jsx
+++ src/pages-and-resources/discussions/FeaturesTable.jsx
@@ -10,9 +10,13 @@ export default function FeaturesTable({ apps, features }) {
       <DataTable
         itemCount={features.length}
         data={features.map(feature => {
-          const appChecks = {};
+          const appCheckmarkCells = {};
+          // DataTable wants 'data' to be an array of objects where each property of an object
+          // represents a cell in that row, identified by its key.
           apps.forEach(app => {
-            appChecks[app.id] = app.featureIds.includes(feature.id) ? (
+            // If our app's set of feature Ids includes this feature, return a checkmark.
+            // i.e, if this app has the current feature, check it!
+            appCheckmarkCells[app.id] = app.featureIds.includes(feature.id) ? (
               <div key={`${app.id}&${feature.id}`}>
                 <FontAwesomeIcon icon={faCheck} />
               </div>
@@ -20,8 +24,11 @@ export default function FeaturesTable({ apps, features }) {
           });
 
           return {
-            feature: feature.name,
-            ...appChecks,
+            feature: feature.name, // 'feature' is the identifier for cells in the first column.
+            // This is spreading the app IDs from appCheckmarkCells into the return array, creating
+            // one object with 'feature' and the app.id keys from above.  The values are the JSX
+            // above with the font awesome checkmarks in 'em
+            ...appCheckmarkCells,
           };
         })}
         columns={[
@@ -29,6 +36,8 @@ export default function FeaturesTable({ apps, features }) {
             Header: '',
             accessor: 'feature',
           },
+          // We're converting our apps array into a bunch of objects with "Header" and "accessor"
+          // keys, like DataTable expects.
           ...apps.map(app => ({
             Header: app.name,
             accessor: app.id,
