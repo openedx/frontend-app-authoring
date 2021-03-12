d8f22330a0d5f3ccd6cefdbab72088cea571bac0
David Joy <djoy@edx.org>
2021-03-12 17:38:36 -0500

    Stepper now intelligently shows/hides drop shadows on the header and footer
    Also organized the component into a more Paragon-like organization with the sub-components hanging off the main one.

--- src/generic/stepper/Stepper.jsx
+++ src/generic/stepper/Stepper.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Icon from './Icon';
+import Line from './Line';
+import Body from './Body';
+import Footer from './Footer';
+import Header from './Header';
+import { StepperContextProvider } from './StepperContext';
 
 export default function Stepper({ children, className }) {
   return (
@@ -11,7 +17,9 @@ export default function Stepper({ children, className }) {
         className,
       )}
     >
-      {children}
+      <StepperContextProvider>
+        {children}
+      </StepperContextProvider>
     </div>
   );
 }
@@ -24,3 +32,9 @@ Stepper.propTypes = {
 Stepper.defaultProps = {
   className: null,
 };
+
+Stepper.Icon = Icon;
+Stepper.Line = Line;
+Stepper.Body = Body;
+Stepper.Footer = Footer;
+Stepper.Header = Header;
