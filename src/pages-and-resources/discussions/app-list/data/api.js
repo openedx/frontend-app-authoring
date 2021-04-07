93b1becdaaaa611257819e96e177fa84f9fad0c5
David Joy <djoy@edx.org>
2021-04-07 14:15:55 -0400

    fix: default discussions provider to legacy
    This defaults our discussions provider to legacy if one was not specified by the backend.
    
    It also fixes a few navigation and provider selection bugs that I encountered in testing the above.

--- src/pages-and-resources/discussions/app-list/data/api.js
+++ src/pages-and-resources/discussions/app-list/data/api.js
@@ -14,7 +14,13 @@ function normalizeApps(data) {
       id,
     })),
     appConfig: data.plugin_configuration,
-    activeAppId: data.providers.active,
+    // TODO: Defaulting to "legacy" should come from the backend.  For now this makes sense as we
+    // don't create a DiscussionsConfiguration object in the backend for legacy discussions, which
+    // is configured by default on all courses.  The endpoint doesn't know this and returns a null
+    // for `active`, which we can then interpret as "the user hasn't configured something else" and
+    // choose to fall back to legacy.  In the long run, we should be able to trust `active` to give
+    // us 'legacy' if the legacy experience is being used.
+    activeAppId: data.providers.active || 'legacy',
     apps,
   };
 }
