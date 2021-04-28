60269a32bcdef77ce60219fe2a2e5d2ff8578104
Awais Ansari <awais.ansari63@gmail.com>
2021-04-28 20:55:46 +0500

    Feedback changes

--- src/pages-and-resources/discussions/app-list/AppCard.test.jsx
+++ src/pages-and-resources/discussions/app-list/AppCard.test.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
-import { render, screen } from '@testing-library/react';
-import { Factory } from 'rosie'; // eslint-disable-line import/no-extraneous-dependencies
+import { render, queryByLabelText } from '@testing-library/react';
 
 import AppCard from './AppCard';
 import messages from './messages';
-import '../data/__factories__/index';
 
 describe('AppCard', () => {
   let app;
@@ -13,62 +11,67 @@ describe('AppCard', () => {
   let wrapper;
 
   beforeEach(() => {
-    app = Factory.build('app');
     selected = true;
-    wrapper = (data) => {
-      render(
-        <IntlProvider locale="en">
-          <AppCard
-            app={data}
-            onClick={() => jest.fn()}
-            selected={selected}
-          />
-        </IntlProvider>,
-      );
+    app = {
+      id: 'legacy',
+      hasFullSupport: false,
+      featureIds: ['discussion-page', 'embedded-course-sections', 'wcag-2.1'],
     };
+
+    wrapper = (data) => render(
+      <IntlProvider locale="en">
+        <AppCard
+          app={data}
+          onClick={() => jest.fn()}
+          selected={selected}
+        />
+      </IntlProvider>,
+    );
   });
 
   test('checkbox input is checked when AppCard is selected', () => {
-    wrapper(app);
+    const labelText = new RegExp(`Select ${messages[`appName-${app.id}`].defaultMessage}`, 'i');
+    const { container } = wrapper(app);
 
-    const radioInput = screen.getByRole('radio');
-    expect(radioInput.getAttribute('aria-checked')).toBe(selected.toString());
-    expect(screen.getByTestId('checkbox').checked).toBe(selected);
+    expect(container.querySelector('[role="radio"]')).toBeChecked();
+    expect(queryByLabelText(container, labelText, { selector: 'input' })).toBeChecked();
   });
 
-  test.each(
-    [
-      ['partial', Factory.build('app')],
-      ['full', Factory.build('app', { hasFullSupport: true })],
-    ],
-  )(
-    'title, subtitle, and text from the app are displayed with %s support', (type, data) => {
-      const support = data.hasFullSupport
-        ? messages.appFullSupport : messages.appPartialSupport;
-      const title = messages[`appName-${data.id}`].defaultMessage;
-      const subtitle = support.defaultMessage;
-      const text = messages[`appDescription-${data.id}`].defaultMessage;
+  test('title, subtitle, and text from the app are displayed with partial support', () => {
+    const title = messages[`appName-${app.id}`].defaultMessage;
+    const subtitle = messages.appPartialSupport.defaultMessage;
+    const text = messages[`appDescription-${app.id}`].defaultMessage;
+    const { container } = wrapper(app);
 
-      wrapper(data);
+    expect(container.querySelector('.card-title')).toHaveTextContent(title);
+    expect(container.querySelector('.card-subtitle')).toHaveTextContent(subtitle);
+    expect(container.querySelector('.card-text')).toHaveTextContent(text);
+  });
 
-      expect(screen.getByText(title)).toBeInTheDocument();
-      expect(screen.getByText(subtitle)).toBeInTheDocument();
-      expect(screen.getByText(text)).toBeInTheDocument();
-    },
-  );
+  test('title, subtitle, and text from the app are displayed with full support', () => {
+    const title = messages[`appName-${app.id}`].defaultMessage;
+    const subtitle = messages.appFullSupport.defaultMessage;
+    const text = messages[`appDescription-${app.id}`].defaultMessage;
+    app.hasFullSupport = true;
+    const { container } = wrapper(app);
+
+    expect(container.querySelector('.card-title')).toHaveTextContent(title);
+    expect(container.querySelector('.card-subtitle')).toHaveTextContent(subtitle);
+    expect(container.querySelector('.card-text')).toHaveTextContent(text);
+  });
 
   test('full support message shown when hasFullSupport is true', () => {
     const subtitle = messages.appFullSupport.defaultMessage;
     app.hasFullSupport = true;
-    wrapper(app);
+    const { container } = wrapper(app);
 
-    expect(screen.getByText(subtitle)).toHaveTextContent(subtitle);
+    expect(container.querySelector('.card-subtitle')).toHaveTextContent(subtitle);
   });
 
   test('partial support message shown when hasFullSupport is false', () => {
     const subtitle = messages.appPartialSupport.defaultMessage;
-    wrapper(app);
+    const { container } = wrapper(app);
 
-    expect(screen.getByText(subtitle)).toHaveTextContent(subtitle);
+    expect(container.querySelector('.card-subtitle')).toHaveTextContent(subtitle);
   });
 });
--- src/pages-and-resources/discussions/data/__factories__/app.factory.js
+++
@@ -1,7 +0,0 @@
-import { Factory } from 'rosie'; // eslint-disable-line import/no-extraneous-dependencies
-
-Factory.define('app').attrs({
-  featureIds: ['discussion-page', 'embedded-course-sections', 'wcag-2.1'],
-  hasFullSupport: false,
-  id: 'legacy',
-});
