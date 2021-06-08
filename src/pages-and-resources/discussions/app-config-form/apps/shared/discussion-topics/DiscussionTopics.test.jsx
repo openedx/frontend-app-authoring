4bbae5da195fc61ca548a1384e849230be165df7
Awais Ansari <awais.ansari63@gmail.com>
2021-06-08 14:46:12 +0500

    refactor: update variable names and test messages

--- src/pages-and-resources/discussions/app-config-form/apps/shared/discussion-topics/DiscussionTopics.test.jsx
+++ src/pages-and-resources/discussions/app-config-form/apps/shared/discussion-topics/DiscussionTopics.test.jsx
@@ -81,7 +81,7 @@ describe('DiscussionTopics', () => {
     await executeThunk(fetchApps(courseId), store.dispatch);
   };
 
-  test('Displays a collapsible card for each discussion topic', async () => {
+  test('displays a collapsible card for each discussion topic', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
 
@@ -91,18 +91,18 @@ describe('DiscussionTopics', () => {
     expect(container.querySelector('[id="13f106c6-6735-4e84-b097-0456cff55960"]')).toBeInTheDocument();
   });
 
-  test('Displays collapse view of general discussion topic', async () => {
+  test('displays collapse view of general discussion topic', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
 
-    const topicNode = container.querySelector('[id="course"]');
+    const generalTopicNode = container.querySelector('[id="course"]');
 
-    expect(topicNode.querySelector('button[aria-label="Expand"]')).toBeInTheDocument();
-    expect(topicNode.querySelector('button[aria-label="Collapse"]')).not.toBeInTheDocument();
-    expect(queryByText(topicNode, 'General')).toBeInTheDocument();
+    expect(generalTopicNode.querySelector('button[aria-label="Expand"]')).toBeInTheDocument();
+    expect(generalTopicNode.querySelector('button[aria-label="Collapse"]')).not.toBeInTheDocument();
+    expect(queryByText(generalTopicNode, 'General')).toBeInTheDocument();
   });
 
-  test('Displays collapse view of additional discussion topic', async () => {
+  test('displays collapse view of additional discussion topic', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
 
@@ -113,20 +113,20 @@ describe('DiscussionTopics', () => {
     expect(queryByText(topicNode, 'Edx')).toBeInTheDocument();
   });
 
-  test('Displays expand view of general discussion topic', async () => {
+  test('displays expand view of general discussion topic', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
-    const topicNode = container.querySelector('[id="course"]');
+    const generalTopicNode = container.querySelector('[id="course"]');
 
-    userEvent.click(queryByLabelText(topicNode, 'Expand'));
+    userEvent.click(queryByLabelText(generalTopicNode, 'Expand'));
 
-    expect(topicNode.querySelector('button[aria-label="Collapse"]')).toBeInTheDocument();
-    expect(topicNode.querySelector('button[aria-label="Expand"]')).not.toBeInTheDocument();
-    expect(topicNode.querySelector('button[aria-label="Delete Topic"]')).not.toBeInTheDocument();
-    expect(topicNode.querySelector('input')).toBeInTheDocument();
+    expect(generalTopicNode.querySelector('button[aria-label="Collapse"]')).toBeInTheDocument();
+    expect(generalTopicNode.querySelector('button[aria-label="Expand"]')).not.toBeInTheDocument();
+    expect(generalTopicNode.querySelector('button[aria-label="Delete Topic"]')).not.toBeInTheDocument();
+    expect(generalTopicNode.querySelector('input')).toBeInTheDocument();
   });
 
-  test('Displays expand view of additional discussion topic', async () => {
+  test('displays expand view of additional discussion topic', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
     const topicNode = container.querySelector('[id="13f106c6-6735-4e84-b097-0456cff55960"]');
@@ -139,7 +139,7 @@ describe('DiscussionTopics', () => {
     expect(topicNode.querySelector('input')).toBeInTheDocument();
   });
 
-  test('Displays updated discussion topic list after delete an additional topic', async () => {
+  test('displays updated discussion topic list after delete an additional topic', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
     const topicNode = container.querySelector('[id= "13f106c6-6735-4e84-b097-0456cff55960"]');
@@ -147,7 +147,7 @@ describe('DiscussionTopics', () => {
     userEvent.click(queryByLabelText(topicNode, 'Expand'));
     userEvent.click(queryByLabelText(topicNode, 'Delete Topic'));
 
-    expect(container.querySelector('.card')).toBeInTheDocument();
+    expect(container.querySelectorAll('.card')).toHaveLength(1);
 
     await act(async () => {
       userEvent.click(container.querySelector('.btn-outline-brand'));
@@ -156,7 +156,7 @@ describe('DiscussionTopics', () => {
     expect(container.querySelectorAll('.collapsible-card')).toHaveLength(1);
   });
 
-  test('Updates discussion topic name', async () => {
+  test('updates discussion topic name', async () => {
     await mockStore(legacyApiResponse);
     createComponent(appConfig);
     const topicNode = container.querySelector('[id= "13f106c6-6735-4e84-b097-0456cff55960"]');
