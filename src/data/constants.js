5cc2251af221051124d57285d9b8a3f9e0f5f42c
Kshitij Sobti <kshitij@sobti.in>
2021-09-14 11:10:05 +0530

    apply review feedback

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,3 +11,25 @@ export const RequestStatus = {
   FAILED: 'failed',
   DENIED: 'denied',
 };
+
+/**
+ * Team sizes enum
+ * @enum
+ * @type {{MIN: number, MAX: number, DEFAULT: number}}
+ */
+export const TeamSizes = {
+  DEFAULT: 50,
+  MIN: 1,
+  MAX: 500,
+};
+
+/**
+ * Team types enum
+ * @enum
+ * @type {{PRIVATE_MANAGED: string, PUBLIC_MANAGED: string, OPEN: string}}
+ */
+export const TeamSetTypes = {
+  OPEN: 'open',
+  PUBLIC_MANAGED: 'public_managed',
+  PRIVATE_MANAGED: 'private_managed',
+};
