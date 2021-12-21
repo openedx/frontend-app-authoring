a3e88a6c0960d9434c9e33d19e26c1ad5c00ed76
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2021-12-21 10:29:42 -0500

    feat: Serve New Editor Pages (#224)
    This work will add, behind a flag, the ability to route to components from frontend-lib-content-component.

--- src/CourseAuthoringRoutes.jsx
+++ src/CourseAuthoringRoutes.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, useRouteMatch } from 'react-router';
 import { PageRoute } from '@edx/frontend-platform/react';
-
 import CourseAuthoringPage from './CourseAuthoringPage';
 import { PagesAndResources } from './pages-and-resources';
 import ProctoredExamSettings from './proctored-exam-settings/ProctoredExamSettings';
+import EditorProvider from './editors/EditorProvider';
 
 /**
  * As of this writing, these routes are mounted at a path prefixed with the following:
@@ -16,6 +16,7 @@ import ProctoredExamSettings from './proctored-exam-settings/ProctoredExamSettin
  *
  * /course/:courseId/course-pages
  * /course/:courseId/proctored-exam-settings
+ * /course/:courseId/editor/:blockType/:blockId
  *
  * This component and CourseAuthoringPage should maybe be combined once we no longer need to have
  * CourseAuthoringPage split out for use in LegacyProctoringRoute.  Once that route is removed, we
@@ -32,6 +33,14 @@ export default function CourseAuthoringRoutes({ courseId }) {
         </PageRoute>
         <PageRoute path={`${path}/proctored-exam-settings`}>
           <ProctoredExamSettings courseId={courseId} />
+        </PageRoute>
+        <PageRoute path={`${path}/editor/:blockType/:blockId`}>
+          {process.env.ENABLE_NEW_EDITOR_PAGES === 'true'
+            && (
+            <EditorProvider
+              courseId={courseId}
+            />
+            )}
         </PageRoute>
       </Switch>
     </CourseAuthoringPage>
---
+++ src/editors/EditorProvider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Placeholder from '@edx/frontend-lib-content-components';
+
+const EditorProvider = () => (
+  <div className="editor-page">
+    <Placeholder />
+  </div>
+);
+EditorProvider.propTypes = {
+};
+
+export default EditorProvider;
