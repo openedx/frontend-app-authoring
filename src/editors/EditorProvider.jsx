f1c46478a3e091eacf44fc24ed2d21160f22cdb9
connorhaugh <chaugh21@amherst.edu>
2022-01-18 16:08:22 -0500

    fix merge conflicts

--- src/editors/EditorProvider.jsx
+++ src/editors/EditorProvider.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { useParams } from 'react-router';
 import {EditorPage} from '@edx/frontend-lib-content-components';
 
-
 const EditorProvider = ({
     courseId,
 }) =>{
