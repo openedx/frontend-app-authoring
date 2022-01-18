d688d92b51a6678c21851f206831f89a5ce781a4
connorhaugh <chaugh21@amherst.edu>
2022-01-18 16:11:17 -0500

    fix: lint fixes

--- src/editors/EditorProvider.jsx
+++ src/editors/EditorProvider.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router';
-import {EditorPage} from '@edx/frontend-lib-content-components';
+import { EditorPage } from '@edx/frontend-lib-content-components';
 
 const EditorProvider = ({
-    courseId,
-}) =>{
-    const {blockType, blockId} = useParams();
-    return (
-        <div className = "editor-page">
-            <EditorPage
-                courseId = {courseId}
-                blockType = {blockType}
-                blockId = {blockId}
-                studioEndpointUrl = {process.env.STUDIO_BASE_URL}
-            />
-        </div>
-    );
-}
+  courseId,
+}) => {
+  const { blockType, blockId } = useParams();
+  return (
+    <div className="editor-page">
+      <EditorPage
+        courseId={courseId}
+        blockType={blockType}
+        blockId={blockId}
+        studioEndpointUrl={process.env.STUDIO_BASE_URL}
+      />
+    </div>
+  );
+};
 EditorProvider.propTypes = {
-    courseId: PropTypes.string.isRequired,
-  };
+  courseId: PropTypes.string.isRequired,
+};
 
 export default EditorProvider;
