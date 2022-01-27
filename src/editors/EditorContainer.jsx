325d98016e3192f9c933a1ba5462071d88509a97
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2022-01-27 09:48:05 -0500

    feat: Use new TextEditor (#237)
    DO NOT MERGE UNITL https://github.com/edx/frontend-lib-content-components/pull/9/files is merged. This PR utalizes the new text editors. Also upgrade node version.

---
+++ src/editors/EditorContainer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
+import { EditorPage } from '@edx/frontend-lib-content-components';
+
+const EditorContainer = ({
+  courseId,
+}) => {
+  const { blockType, blockId } = useParams();
+  return (
+    <div className="editor-page">
+      <EditorPage
+        courseId={courseId}
+        blockType={blockType}
+        blockId={blockId}
+        studioEndpointUrl={process.env.STUDIO_BASE_URL}
+      />
+    </div>
+  );
+};
+EditorContainer.propTypes = {
+  courseId: PropTypes.string.isRequired,
+};
+
+export default EditorContainer;
