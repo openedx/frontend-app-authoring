a9ecc8975706e0be988e785b48731682a8030cef
Awais Ansari <awais.ansari63@gmail.com>
2022-01-31 18:57:56 +0500

    feat: hide PII sharing for all users

--- src/pages-and-resources/discussions/app-config-form/AppConfigFormSaveButton.jsx
+++ src/pages-and-resources/discussions/app-config-form/AppConfigFormSaveButton.jsx
@@ -2,14 +2,21 @@ import React, { useCallback, useContext } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
 import { StatefulButton } from '@edx/paragon';
 
 import messages from './messages';
 import { SAVING } from '../data/slice';
 import { AppConfigFormContext } from './AppConfigFormProvider';
+import { useModel } from '../../../generic/model-store';
 
 function AppConfigFormSaveButton({ intl, labelText }) {
   const saveStatus = useSelector(state => state.discussions.saveStatus);
+  const { selectedAppId } = useSelector((state) => state.discussions);
+
+  const app = useModel('apps', selectedAppId);
+  const canSubmit = getAuthenticatedUser().administrator || !app.adminOnlyConfig;
+
   const { formRef } = useContext(AppConfigFormContext);
 
   const submitButtonState = saveStatus === SAVING ? 'pending' : 'default';
@@ -20,16 +27,18 @@ function AppConfigFormSaveButton({ intl, labelText }) {
   }, [formRef]);
 
   return (
-    <StatefulButton
-      labels={{
-        default: labelText || intl.formatMessage(messages.saveButton),
-        pending: intl.formatMessage(messages.savingButton),
-        complete: intl.formatMessage(messages.savedButton),
-      }}
-      state={submitButtonState}
-      onClick={handleSave}
-      style={{ minWidth: '88px' }}
-    />
+    canSubmit && (
+      <StatefulButton
+        labels={{
+          default: labelText || intl.formatMessage(messages.saveButton),
+          pending: intl.formatMessage(messages.savingButton),
+          complete: intl.formatMessage(messages.savedButton),
+        }}
+        state={submitButtonState}
+        onClick={handleSave}
+        style={{ minWidth: '88px' }}
+      />
+    )
   );
 }
 
