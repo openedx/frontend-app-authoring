31dd35cf937d3009029e5382e79081641462209d
connorhaugh <chaugh21@amherst.edu>
2022-02-15 10:58:12 -0500

    fix: add even more elaborate test coverage

--- src/CourseAuthoringPage.test.jsx
+++ src/CourseAuthoringPage.test.jsx
@@ -14,6 +14,13 @@ import { executeThunk } from './utils';
 import { fetchCourseApps } from './pages-and-resources/data/thunks';
 
 const courseId = 'course-v1:edX+TestX+Test_Course';
+let mockPathname = '/evilguy/';
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => ({
+    pathname: mockPathname,
+  }),
+}));
 let axiosMock;
 let store;
 let container;
@@ -63,6 +70,14 @@ describe('DiscussionsSettings', () => {
 });
 
 describe('Editor Pages Load no header', () => {
+  const mockStoreSuccess = async () => {
+    const apiBaseUrl = getConfig().STUDIO_BASE_URL;
+    const courseAppsApiUrl = `${apiBaseUrl}/api/course_apps/v1/apps`;
+    axiosMock.onGet(`${courseAppsApiUrl}/${courseId}`).reply(200, {
+      response: { status: 200 },
+    });
+    await executeThunk(fetchCourseApps(courseId), store.dispatch);
+  };
   beforeEach(() => {
     initializeMockApp({
       authenticatedUser: {
@@ -74,16 +89,35 @@ describe('Editor Pages Load no header', () => {
     });
     store = initializeStore();
     axiosMock = new MockAdapter(getAuthenticatedHttpClient());
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useLocation: () => ({
-        pathname: '/editor/',
-      }),
-    }));
   });
   test('renders no loading wheel on editor pages', async () => {
-    await mockStore();
-    renderComponent();
-    expect(queryByTestId(container, 'loading')).not.toBeInTheDocument();
+    mockPathname = '/editor/';
+    await mockStoreSuccess();
+    const wrapper = render(
+      <AppProvider store={store}>
+        <IntlProvider locale="en">
+          <CourseAuthoringPage courseId={courseId}>
+            <PagesAndResources courseId={courseId} />
+          </CourseAuthoringPage>
+        </IntlProvider>
+      </AppProvider>
+      ,
+    );
+    expect(wrapper.queryByRole('status')).not.toBeInTheDocument();
+  });
+  test('renders loading wheel on non editor pages', async () => {
+    mockPathname = '/evilguy/';
+    await mockStoreSuccess();
+    const wrapper = render(
+      <AppProvider store={store}>
+        <IntlProvider locale="en">
+          <CourseAuthoringPage courseId={courseId}>
+            <PagesAndResources courseId={courseId} />
+          </CourseAuthoringPage>
+        </IntlProvider>
+      </AppProvider>
+      ,
+    );
+    expect(wrapper.queryByRole('status')).toBeInTheDocument();
   });
 });
