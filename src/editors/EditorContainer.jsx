d2b82798aba539d7782da709f72e70ead5a304f6
Julia Eskew <jeskew@edx.org>
2022-03-31 18:29:37 -0400

    feat: Send event to Segment on initial loading of a content editor for an XBlock.

--- src/editors/EditorContainer.jsx
+++ src/editors/EditorContainer.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router';
 import { EditorPage } from '@edx/frontend-lib-content-components';
+import { sendTrackEvent } from '@edx/frontend-platform/analytics';
 
 const EditorContainer = ({
   courseId,
 }) => {
   const { blockType, blockId } = useParams();
+  sendTrackEvent('edx.ui.authoring.editor.enter', {
+    courserun_key: courseId,
+    block_type: blockType,
+    block_id: blockId,
+  });
   return (
     <div className="editor-page">
       <EditorPage
--- src/editors/EditorContainer.test.jsx
+++ src/editors/EditorContainer.test.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { sendTrackEvent } from '@edx/frontend-platform/analytics';
 import { render, screen } from '@testing-library/react';
 import EditorContainer from './EditorContainer';
+
+jest.mock('@edx/frontend-platform/analytics');
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'), // use actual for all non-hook parts
@@ -23,11 +26,18 @@ jest.mock('@tinymce/tinymce-react', () => {
 
 describe('Editor Container', () => {
   it('shows a rich text editor loading with an error', () => {
+    sendTrackEvent.mockClear();
     render(<EditorContainer
       courseId="demoXDemocourse"
     />);
     expect(screen.getByText('Cancel')).toBeTruthy();
     expect(screen.getByText('Add To Course')).toBeTruthy();
     expect(screen.findByRole(mockRole)).toBeTruthy();
+    expect(sendTrackEvent).toHaveBeenCalledTimes(1);
+    expect(sendTrackEvent).toHaveBeenCalledWith('edx.ui.authoring.editor.enter', {
+      courserun_key: 'demoXDemocourse',
+      block_type: 'html',
+      block_id: 'company-id1',
+    });
   });
 });
