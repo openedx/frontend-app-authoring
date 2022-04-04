f9c88e07b70a7d912582c3e97ead62caed4581df
connorhaugh <chaugh21@amherst.edu>
2022-04-04 10:06:19 -0400

    test: improve tests

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,9 @@ module.exports = createConfig('jest', {
     'src/setupTest.js',
     'src/i18n',
   ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+  ],
   moduleNameMapper: {
     '^lodash-es$': 'lodash',
   },
