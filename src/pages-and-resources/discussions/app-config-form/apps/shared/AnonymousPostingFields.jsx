57cd166a38d28ec6875bed8724aa81511bf19f2e
Mehak Nasir <mehaknasir94@gmail.com>
2022-04-06 01:03:20 +0500

    feat: removed the support of allow anonymous to all from discussion configs

--- src/pages-and-resources/discussions/app-config-form/apps/shared/AnonymousPostingFields.jsx
+++ src/pages-and-resources/discussions/app-config-form/apps/shared/AnonymousPostingFields.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { TransitionReplace } from '@edx/paragon';
 import FormSwitchGroup from '../../../../../generic/FormSwitchGroup';
 import messages from '../../messages';
 import AppConfigFormDivider from './AppConfigFormDivider';
@@ -15,30 +14,15 @@ function AnonymousPostingFields({
   return (
     <>
       <h5 className="mt-4 text-gray-500">{intl.formatMessage(messages.anonymousPosting)}</h5>
+      <AppConfigFormDivider />
       <FormSwitchGroup
         onChange={onChange}
         onBlur={onBlur}
-        id="allowAnonymousPosts"
-        checked={values.allowAnonymousPosts}
-        label={intl.formatMessage(messages.allowAnonymousPostsLabel)}
-        helpText={intl.formatMessage(messages.allowAnonymousPostsHelp)}
+        id="allowAnonymousPostsPeers"
+        checked={values.allowAnonymousPostsPeers}
+        label={intl.formatMessage(messages.allowAnonymousPostsPeersLabel)}
+        helpText={intl.formatMessage(messages.allowAnonymousPostsPeersHelp)}
       />
-      <TransitionReplace>
-        {values.allowAnonymousPosts ? (
-          <React.Fragment key="open">
-            <AppConfigFormDivider />
-            <FormSwitchGroup
-              onChange={onChange}
-              onBlur={onBlur}
-              className="ml-4"
-              id="allowAnonymousPostsPeers"
-              checked={values.allowAnonymousPostsPeers}
-              label={intl.formatMessage(messages.allowAnonymousPostsPeersLabel)}
-              helpText={intl.formatMessage(messages.allowAnonymousPostsPeersHelp)}
-            />
-          </React.Fragment>
-        ) : <React.Fragment key="closed" />}
-      </TransitionReplace>
     </>
   );
 }
@@ -48,7 +32,6 @@ AnonymousPostingFields.propTypes = {
   onChange: PropTypes.func.isRequired,
   intl: intlShape.isRequired,
   values: PropTypes.shape({
-    allowAnonymousPosts: PropTypes.bool,
     allowAnonymousPostsPeers: PropTypes.bool,
   }).isRequired,
 };
