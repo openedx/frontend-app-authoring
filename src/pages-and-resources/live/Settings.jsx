908ca9fb526d1ec5210f671852ea9c677a1e8d71
Awais Ansari <awais.ansari63@gmail.com>
2022-05-11 15:59:36 +0500

    feat: update the pii sharing flow for live app

--- src/pages-and-resources/live/Settings.jsx
+++ src/pages-and-resources/live/Settings.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { camelCase } from 'lodash';
 import { SelectableBox, Icon } from '@edx/paragon';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import PropTypes from 'prop-types';
 import * as Yup from 'yup';
-import { fetchLiveData, saveLiveConfiguration } from './data/thunks';
+
+import { fetchLiveData, saveLiveConfiguration, saveLiveConfigurationAsDraft } from './data/thunks';
+import { selectApp } from './data/slice';
 import FormikControl from '../../generic/FormikControl';
 import AppSettingsModal from '../app-settings-modal/AppSettingsModal';
+import { useModel } from '../../generic/model-store';
+import Loading from '../../generic/Loading';
 import iconsSrc from './constants';
+import { RequestStatus } from '../../data/constants';
 import messages from './messages';
 
 function LiveSettings({
@@ -16,13 +21,27 @@ function LiveSettings({
   onClose,
 }) {
   const dispatch = useDispatch();
+  const [isLoading, setLoading] = useState(true);
   const courseId = useSelector(state => state.courseDetail.courseId);
-  const liveConfiguration = useSelector((state) => state.live.configuration);
   const availableProviders = useSelector((state) => state.live.appIds);
+  const {
+    piiSharingAllowed, selectedAppId, enabled, status,
+  } = useSelector(state => state.live);
 
-  useEffect(() => {
-    dispatch(fetchLiveData(courseId));
-  }, [courseId]);
+  const appConfig = useModel('liveAppConfigs', selectedAppId);
+  const app = useModel('liveApps', selectedAppId);
+
+  const liveConfiguration = {
+    enabled: enabled || false,
+    consumerKey: appConfig?.consumerKey || '',
+    consumerSecret: appConfig?.consumerSecret || '',
+    launchUrl: appConfig?.launchUrl || '',
+    launchEmail: appConfig?.launchEmail || '',
+    provider: selectedAppId || 'zoom',
+    piiSharingEnable: piiSharingAllowed || false,
+    piiSharingUsername: app?.piiSharing.username || false,
+    piiSharingEmail: app?.piiSharing.email || false,
+  };
 
   const validationSchema = {
     enabled: Yup.boolean(),
@@ -32,91 +51,107 @@ function LiveSettings({
     launchEmail: Yup.string().required(intl.formatMessage(messages.launchEmailRequired)),
   };
 
+  const handleProviderChange = (providerId, setFieldValue, values) => {
+    dispatch(saveLiveConfigurationAsDraft(values));
+    dispatch(selectApp({ appId: providerId }));
+    setFieldValue('provider', providerId);
+  };
+
   const handleSettingsSave = async (values) => {
     await dispatch(saveLiveConfiguration(courseId, values));
   };
-  const handleProviderChange = (selectedProvider, setFieldValue) => {
-    setFieldValue('provider', selectedProvider);
-  };
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(fetchLiveData(courseId));
+      setLoading(false);
+    })();
+  }, [courseId]);
 
   return (
-    <AppSettingsModal
-      appId="live"
-      title={intl.formatMessage(messages.heading)}
-      enableAppHelp={intl.formatMessage(messages.enableLiveHelp)}
-      enableAppLabel={intl.formatMessage(messages.enableLiveLabel)}
-      learnMoreText={intl.formatMessage(messages.enableLiveLink)}
-      onClose={onClose}
-      initialValues={liveConfiguration}
-      validationSchema={validationSchema}
-      onSettingsSave={handleSettingsSave}
-      configureBeforeEnable
-      enableReinitialize
-    >
-      {
-        ({ values, setFieldValue }) => (
+    <>
+      <AppSettingsModal
+        appId="live"
+        title={intl.formatMessage(messages.heading)}
+        enableAppHelp={intl.formatMessage(messages.enableLiveHelp)}
+        enableAppLabel={intl.formatMessage(messages.enableLiveLabel)}
+        learnMoreText={intl.formatMessage(messages.enableLiveLink)}
+        onClose={onClose}
+        initialValues={liveConfiguration}
+        validationSchema={validationSchema}
+        onSettingsSave={handleSettingsSave}
+        configureBeforeEnable
+        enableReinitialize
+      >
+        {({ values, setFieldValue }) => (
           <>
-            <h4 className="my-3">{intl.formatMessage(messages.selectProvider)}</h4>
-            <SelectableBox.Set
-              type="checkbox"
-              value={values.provider}
-              onChange={(event) => handleProviderChange(event.target.value, setFieldValue)}
-              name="provider"
-              columns={3}
-              className="mb-3"
-            >
-              {availableProviders.map((app) => (
-                <SelectableBox value={app.id} type="checkbox" key={app.id}>
-                  <div className="d-flex flex-column align-items-center">
-                    <Icon src={iconsSrc[`${camelCase(app.id)}`]} alt={app.id} />
-                    <span>{intl.formatMessage(messages[`appName-${camelCase(app.id)}`])}</span>
-                  </div>
-                </SelectableBox>
-              ))}
-            </SelectableBox.Set>
-            {values.piiSharingEnable ? (
-              <>
-                <p data-testid="helper-text">
-                  {intl.formatMessage(messages.providerHelperText, { providerName: values.provider })}
-                </p>
-                <p className="pb-2">{intl.formatMessage(messages.formInstructions)}</p>
-                <FormikControl
-                  name="consumerKey"
-                  value={values.consumerKey}
-                  floatingLabel={intl.formatMessage(messages.consumerKey)}
-                  className="pb-1"
-                  type="input"
-                />
-                <FormikControl
-                  name="consumerSecret"
-                  value={values.consumerSecret}
-                  floatingLabel={intl.formatMessage(messages.consumerSecret)}
-                  className="pb-1"
-                  type="input"
-                />
-                <FormikControl
-                  name="launchUrl"
-                  value={values.launchUrl}
-                  floatingLabel={intl.formatMessage(messages.launchUrl)}
-                  className="pb-1"
-                  type="input"
-                />
-                <FormikControl
-                  name="launchEmail"
-                  value={values.launchEmail}
-                  floatingLabel={intl.formatMessage(messages.launchEmail)}
-                  type="input"
-                />
-              </>
-            ) : (
-              <p data-testid="request-pii-sharing">
-                {intl.formatMessage(messages.requestPiiSharingEnable, { provider: values.provider })}
-              </p>
-            )}
+            {(status === RequestStatus.IN_PROGRESS || isLoading) ? (
+              <Loading />
+              ) : (
+                <>
+                  <h4 className="my-3">{intl.formatMessage(messages.selectProvider)}</h4>
+                  <SelectableBox.Set
+                    type="checkbox"
+                    value={values.provider}
+                    onChange={(event) => handleProviderChange(event.target.value, setFieldValue, values)}
+                    name="provider"
+                    columns={3}
+                    className="mb-3"
+                  >
+                    {availableProviders.map((provider) => (
+                      <SelectableBox value={provider} type="checkbox" key={provider}>
+                        <div className="d-flex flex-column align-items-center">
+                          <Icon src={iconsSrc[`${camelCase(provider)}`]} alt={provider} />
+                          <span>{intl.formatMessage(messages[`appName-${camelCase(provider)}`])}</span>
+                        </div>
+                      </SelectableBox>
+                    ))}
+                  </SelectableBox.Set>
+                  {(!values.piiSharingEnable && (values.piiSharingEmail || values.piiSharingUsername)) ? (
+                    <p data-testid="request-pii-sharing">
+                      {intl.formatMessage(messages.requestPiiSharingEnable, { provider: values.provider })}
+                    </p>
+                  ) : (
+                    <>
+                      <p data-testid="helper-text">
+                        {intl.formatMessage(messages.providerHelperText, { providerName: values.provider })}
+                      </p>
+                      <p className="pb-2">{intl.formatMessage(messages.formInstructions)}</p>
+                      <FormikControl
+                        name="consumerKey"
+                        value={values.consumerKey}
+                        floatingLabel={intl.formatMessage(messages.consumerKey)}
+                        className="pb-1"
+                        type="input"
+                      />
+                      <FormikControl
+                        name="consumerSecret"
+                        value={values.consumerSecret}
+                        floatingLabel={intl.formatMessage(messages.consumerSecret)}
+                        className="pb-1"
+                        type="input"
+                      />
+                      <FormikControl
+                        name="launchUrl"
+                        value={values.launchUrl}
+                        floatingLabel={intl.formatMessage(messages.launchUrl)}
+                        className="pb-1"
+                        type="input"
+                      />
+                      <FormikControl
+                        name="launchEmail"
+                        value={values.launchEmail}
+                        floatingLabel={intl.formatMessage(messages.launchEmail)}
+                        type="input"
+                      />
+                    </>
+                  )}
+                </>
+              )}
           </>
-        )
-      }
-    </AppSettingsModal>
+          )}
+      </AppSettingsModal>
+    </>
   );
 }
 
