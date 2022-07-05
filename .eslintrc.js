2473c9a875fd62802c58d5acba6bb6c1b48dfb97
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2022-07-05 09:08:15 -0400

    chore: upgrade paragon dep (#89)
    * chore: upgrade paragon dep
    
    * fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ const config = createConfig('eslint', {
     'import/no-named-as-default-member': 'off',
     'import/no-self-import': 'off',
     'spaced-comment': ['error', 'always', { 'block': { 'exceptions': ['*'] } }],
+    'react-hooks/rules-of-hooks': 'off',
+    'react-hooks/exhaustive-deps': 'off',
   },
 });
 
