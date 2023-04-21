492ee27d8e54c4aaf6f0402ecf6b4e1b25ede431
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2023-04-21 09:19:39 -0400

    fix: settings helper text font size (#313)

--- src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/CardSection.jsx
+++ src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/CardSection.jsx
@@ -11,7 +11,7 @@ const CardSection = ({
   if (!show) { return null; }
 
   return (
-    <Card.Section>
+    <Card.Section className="pt-0">
       <Collapsible.Advanced
         open={!isCardCollapsibleOpen}
       >
