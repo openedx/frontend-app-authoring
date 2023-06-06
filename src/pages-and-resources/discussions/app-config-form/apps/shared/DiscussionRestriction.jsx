8c9ecb46909f1434fda0200e9409b1e24245d846
Awais Ansari <awais.ansari63@gmail.com>
2023-06-06 14:39:22 +0500

    refactor: discussion restriction component

--- src/pages-and-resources/discussions/app-config-form/apps/shared/DiscussionRestriction.jsx
+++ src/pages-and-resources/discussions/app-config-form/apps/shared/DiscussionRestriction.jsx
@@ -28,14 +28,14 @@ const DiscussionRestriction = () => {
     }
   }, []);
 
-  const handleConfirmation = () => {
+  const handleConfirmation = useCallback(() => {
     setSelectedRestrictionOption(discussionRestriction.ENABLED);
     setFieldValue('postingRestrictions', discussionRestriction.ENABLED);
-  };
+  }, []);
 
-  const handleCancel = () => {
+  const handleCancel = useCallback(() => {
     setSelectedRestrictionOption(postingRestrictions);
-  };
+  }, [postingRestrictions]);
 
   const discussionRestrictionButtons = useMemo(() => discussionRestrictionOptions.map((restriction) => (
     <Button
@@ -50,6 +50,10 @@ const DiscussionRestriction = () => {
     </Button>
   )), [selectedRestrictionOption]);
 
+  const selectedRestrictionMessage = useMemo(() => (
+    discussionRestrictionOptions.find(option => option.value === selectedRestrictionOption).message
+  ), [selectedRestrictionOption]);
+
   return (
     <div className="discussion-restriction">
       <h5 className="text-gray-500 mt-4 mb-3 line-height-20">
@@ -58,29 +62,25 @@ const DiscussionRestriction = () => {
       <ButtonGroup className="mb-2 w-100" toggle size="sm">
         {discussionRestrictionButtons}
       </ButtonGroup>
-
       <div className="small text-muted font-size-14 height-24">
-        {intl.formatMessage(
-          discussionRestrictionOptions.find(option => option.value === selectedRestrictionOption).message,
-          )}
+        {intl.formatMessage(selectedRestrictionMessage)}
       </div>
-
       {(postingRestrictions !== discussionRestriction.ENABLED
-        && selectedRestrictionOption === discussionRestriction.ENABLED) && (
-          <ConfirmationPopup
-            label={intl.formatMessage(messages.enableRestrictedDatesConfirmationLabel)}
-            bodyText={intl.formatMessage(messages.enableRestrictedDatesConfirmationHelp)}
-            onCancel={handleCancel}
-            onConfirm={handleConfirmation}
-            confirmLabel={intl.formatMessage(messages.ok)}
-            cancelLabel={intl.formatMessage(messages.cancelButton)}
-            confirmVariant="plain"
-            confirmButtonClass="bg-primary-500 text-white rounded-0 action-btn"
-            cancelButtonClass="rounded-0 action-btn w-92"
-            sectionClasses="card-body-section"
-          />
-        )}
-
+        && selectedRestrictionOption === discussionRestriction.ENABLED
+      ) && (
+        <ConfirmationPopup
+          label={intl.formatMessage(messages.enableRestrictedDatesConfirmationLabel)}
+          bodyText={intl.formatMessage(messages.enableRestrictedDatesConfirmationHelp)}
+          onCancel={handleCancel}
+          onConfirm={handleConfirmation}
+          confirmLabel={intl.formatMessage(messages.ok)}
+          cancelLabel={intl.formatMessage(messages.cancelButton)}
+          confirmVariant="plain"
+          confirmButtonClass="bg-primary-500 text-white rounded-0 action-btn"
+          cancelButtonClass="rounded-0 action-btn w-92"
+          sectionClasses="card-body-section"
+        />
+      )}
       {selectedRestrictionOption === discussionRestriction.SCHEDULED && <RestrictionSchedules />}
     </div>
   );
