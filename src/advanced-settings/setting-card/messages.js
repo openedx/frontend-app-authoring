6c558026208970002b49612a5e4a6b81b13a7718
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2023-06-13 12:37:30 +0300

    fix: fixed loader and changing help text on the page Advanced settings (#4)
    * fix: fixed loader and changing help text on the page Advanced settings

--- src/advanced-settings/setting-card/messages.js
+++ src/advanced-settings/setting-card/messages.js
@@ -5,6 +5,10 @@ const messages = defineMessages({
     id: 'course-authoring.advanced-settings.button.deprecated',
     defaultMessage: 'Deprecated',
   },
+  helpButtonText: {
+    id: 'course-authoring.advanced-settings.button.help',
+    defaultMessage: 'Show help text',
+  },
 });
 
 export default messages;
