b0fef766eb583d06cf20854e8de0f50c0615ff1e
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2023-06-15 15:28:12 -0400

    feat: add ErrorAlert to exports (#349)

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import messages from './i18n/index';
 import EditorPage from './editors/EditorPage';
 import VideoSelectorPage from './editors/VideoSelectorPage';
 import DraggableList, { SortableItem } from './editors/sharedComponents/DraggableList';
+import ErrorAlert from './editors/sharedComponents/ErrorAlerts/ErrorAlert';
 
 export {
   messages,
@@ -10,5 +11,6 @@ export {
   VideoSelectorPage,
   DraggableList,
   SortableItem,
+  ErrorAlert,
 };
 export default Placeholder;
