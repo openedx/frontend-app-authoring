a4bd6f5765a62eec50052218f22d098844694750
Emad Ehsanrad <codewithemad@gmail.com>
2023-08-08 12:45:51 +0330

    refactor: better variable name for languages

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,7 +4,7 @@ import arMessages from './messages/ar.json';
 import faIRMessages from './messages/fa_IR.json';
 import frMessages from './messages/fr.json';
 import es419Messages from './messages/es_419.json';
-import zhcnMessages from './messages/zh_CN.json';
+import zhCNMessages from './messages/zh_CN.json';
 import ptMessages from './messages/pt.json';
 import itMessages from './messages/it.json';
 import ukMessages from './messages/uk.json';
@@ -12,9 +12,9 @@ import deMessages from './messages/de.json';
 import ruMessages from './messages/ru.json';
 import hiMessages from './messages/hi.json';
 import frCAMessages from './messages/fr_CA.json';
-import dedeMessages from './messages/de_DE.json';
-import ititMessages from './messages/it_IT.json';
-import ptptMessages from './messages/pt_PT.json';
+import deDEMessages from './messages/de_DE.json';
+import itITMessages from './messages/it_IT.json';
+import ptPTMessages from './messages/pt_PT.json';
 // no need to import en messages-- they are in the defaultMessage field
 
 const appMessages = {
@@ -22,7 +22,7 @@ const appMessages = {
   'es-419': es419Messages,
   'fa-ir': faIRMessages,
   fr: frMessages,
-  'zh-cn': zhcnMessages,
+  'zh-cn': zhCNMessages,
   pt: ptMessages,
   it: itMessages,
   de: deMessages,
@@ -30,9 +30,9 @@ const appMessages = {
   'fr-ca': frCAMessages,
   ru: ruMessages,
   uk: ukMessages,
-  'de-de': dedeMessages,
-  'it-it': ititMessages,
-  'pt-pt': ptptMessages,
+  'de-de': deDEMessages,
+  'it-it': itITMessages,
+  'pt-pt': ptPTMessages,
 };
 
 export default [
