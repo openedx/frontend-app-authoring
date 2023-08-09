71484daf4d2fc9e462307642ade42a9ad33b6f08
David Nuon <davidnuongm@gmail.com>
2023-08-09 12:25:47 -0700

    fix: Change wording to not crowd xpert tile in preferences page

--- src/pages-and-resources/xpert-unit-summary/appInfo.js
+++ src/pages-and-resources/xpert-unit-summary/appInfo.js
@@ -2,7 +2,7 @@ export default {
   id: 'xpert-unit-summary',
   enabled: false,
   name: 'Xpert unit summaries',
-  description: 'Harness generative AI to generate concise summaries of your text and video transcript content to help reinforce learning.',
+  description: 'Harness generative AI to display concise summaries of text and video content to help reinforce learning.',
   allowedOperations: {
     enable: true,
     configure: true,
