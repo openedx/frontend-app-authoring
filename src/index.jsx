9438a5b89aeacc2f1b3d7bdbccf2359e9d8ac616
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2023-08-16 18:18:06 -0400

    feat: add typeahead component (#375)

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import ErrorAlert from './editors/sharedComponents/ErrorAlerts/ErrorAlert';
 import Footer from './footer';
 import { TinyMceWidget } from './editors/sharedComponents/TinyMceWidget';
 import { prepareEditorRef } from './editors/sharedComponents/TinyMceWidget/hooks';
+import TypeaheadDropdown from './editors/sharedComponents/TypeaheadDropdown';
 
 export {
   messages,
@@ -18,5 +19,6 @@ export {
   Footer,
   TinyMceWidget,
   prepareEditorRef,
+  TypeaheadDropdown,
 };
 export default Placeholder;
