8559201439f0fddf4e93202a2b89232d6e853972
KristinAoki <kaoki@2u.com>
2023-09-01 11:58:14 -0400

    fix: course summary link text size

--- src/schedule-and-details/basic-section/index.jsx
+++ src/schedule-and-details/basic-section/index.jsx
@@ -101,7 +101,7 @@ const BasicSection = ({
       <Card.Section className="px-3 py-1">
         <Hyperlink
           destination={lmsLinkForAboutPage}
-          className="lead info-500 text-decoration-none"
+          className="lead info-500 small text-decoration-none"
           target="_blank"
           showLaunchIcon={false}
         >
