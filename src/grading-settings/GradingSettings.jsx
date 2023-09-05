373b58ad50e6a5546ffe09d6f8cd00f6c6f78337
KristinAoki <kaoki@2u.com>
2023-09-05 15:56:42 -0400

    fix: sidebar wrap

--- src/grading-settings/GradingSettings.jsx
+++ src/grading-settings/GradingSettings.jsx
@@ -110,7 +110,7 @@ const GradingSettings = ({ intl, courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="m-4">
+      <Container size="xl" className="px-4">
         <div className="mt-5">
           <AlertMessage
             show={showSuccessAlert}
