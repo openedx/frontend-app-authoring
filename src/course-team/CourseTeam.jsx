raccoongang/frontend-app-course-authoring

fix: fixed sidebar margin top (#73)

src/course-team/CourseTeam.jsx
@@ -58,7 +58,7 @@ const CourseTeam = ({ courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="m-4">
+      <Container size="xl" className="course-team m-4">
         <section className="course-team-container mb-4">
           <Layout
             lg={[{ span: 9 }, { span: 3 }]}

src/grading-settings/GradingSettings.jsx
@@ -110,7 +110,7 @@ const GradingSettings = ({ intl, courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="m-4">
+      <Container size="xl" className="grading m-4">
         <div className="mt-5">
           <AlertMessage
             show={showSuccessAlert}

src/schedule-and-details/index.jsx
@@ -159,7 +159,7 @@ const ScheduleAndDetails = ({ intl, courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="m-4">
+      <Container size="xl" className="schedule-and-details m-4">
         <div className="mt-5">
           <AlertMessage
             show={showSuccessfulAlert}

openedx/frontend-app-authoring

src/course-team/CourseTeam.jsx
@@ -62,7 +62,7 @@ const CourseTeam = ({ courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="px-4">
+      <Container size="xl" className="course-team px-4">
         <section className="course-team-container mb-4">
           <Layout
             lg={[{ span: 9 }, { span: 3 }]}

src/grading-settings/GradingSettings.jsx
@@ -115,7 +115,7 @@ const GradingSettings = ({ intl, courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="px-4">
+      <Container size="xl" className="grading px-4">
         <div className="mt-5">
           <AlertMessage
             show={showSuccessAlert}

src/schedule-and-details/index.jsx
@@ -161,7 +161,7 @@ const ScheduleAndDetails = ({ intl, courseId }) => {
 
   return (
     <>
-      <Container size="xl" className="px-4">
+      <Container size="xl" className="schedule-and-details px-4">
         <div className="mt-5">
           <AlertMessage
             show={showSuccessfulAlert}