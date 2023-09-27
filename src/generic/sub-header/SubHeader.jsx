d1894926500baa76a04a5204ded4f6318696112e
KristinAoki <kaoki@2u.com>
2023-09-26 17:08:56 -0700

    fix: padding on header with no sub header

--- src/generic/sub-header/SubHeader.jsx
+++ src/generic/sub-header/SubHeader.jsx
@@ -5,7 +5,7 @@ import { ActionRow } from '@edx/paragon';
 const SubHeader = ({
   title, subtitle, contentTitle, description, instruction, headerActions,
 }) => (
-  <>
+  <div className="border-bottom border-light-400 mb-3">
     <header className="sub-header">
       <h2 className="sub-header-title">
         <small className="sub-header-title-subtitle">{subtitle}</small>
@@ -17,14 +17,16 @@ const SubHeader = ({
         </ActionRow>
       )}
     </header>
-    <header className="sub-header-content">
-      <h2 className="sub-header-content-title">{contentTitle}</h2>
-      <span className="small text-gray-700">{description}</span>
-    </header>
+    {contentTitle && (
+      <header className="sub-header-content">
+        <h2 className="sub-header-content-title">{contentTitle}</h2>
+        <span className="small text-gray-700">{description}</span>
+      </header>
+    )}
     {instruction && (
       <p className="sub-header-instructions mb-4">{instruction}</p>
     )}
-  </>
+  </div>
 );
 SubHeader.defaultProps = {
   instruction: '',
