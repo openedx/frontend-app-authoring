382a811cc1c3e901843d2f6fd75fb4ab058e59b7
KristinAoki <kaoki@2u.com>
2023-10-24 10:40:38 -0400

    fix: broken files-and-uploads test

--- src/files-and-videos/assets/messages.js
+++ src/files-and-videos/assets/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   heading: {
     id: 'course-authoring.files-and-uploads.heading',
-    defaultMessage: 'Files and uploads',
+    defaultMessage: 'Files',
   },
   thumbnailAltMessage: {
     id: 'course-authoring.files-and-uploads.thumbnail.alt',
