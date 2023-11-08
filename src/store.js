8d26ef218347dcfddd680829c6aad45df1cb4f86
Braden MacDonald <braden@opencraft.com>
2023-11-08 11:37:33 -0800

    feat: better tests for <SettingsComponent>

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import { reducer as liveReducer } from '@openedx-plugins/course-app-live/data/sl
 
 import { reducer as modelsReducer } from './generic/model-store';
 import { reducer as courseDetailReducer } from './data/slice';
-import { reducer as discussionsReducer } from './pages-and-resources/discussions';
+import { reducer as discussionsReducer } from './pages-and-resources/discussions/data/slice';
 import { reducer as pagesAndResourcesReducer } from './pages-and-resources/data/slice';
 import { reducer as customPagesReducer } from './custom-pages/data/slice';
 import { reducer as advancedSettingsReducer } from './advanced-settings/data/slice';
