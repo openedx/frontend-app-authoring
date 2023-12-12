17499ebab7026980d9b6ec65a6be68703437c744
Jesper Hodge <jhodge@outlook.de>
2023-12-12 23:05:21 +0000

    Revert "fix: import api to chunk file (#734)"
    This reverts commit 6f7a9928479936b9367ce6788f5bc531956fb5d1.

--- src/import-page/data/api.js
+++ src/import-page/data/api.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 import { camelCaseObject, getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 
@@ -13,42 +14,9 @@ export const getImportStatusApiUrl = (courseId, fileName) => `${getApiBaseUrl()}
  * @returns {Promise<Object>}
  */
 export async function startCourseImporting(courseId, fileData, requestConfig) {
-  const chunkSize = 20 * 1000000; // 20 MB
-  const fileSize = fileData.size || 0;
-  const chunkLength = Math.ceil(fileSize / chunkSize);
-  let resp;
-
-  const upload = async (blob, start, stop) => {
-    const contentRange = `bytes ${start}-${stop}/${fileSize}`;
-    const contentDisposition = `attachment; filename="${fileData.name}"`;
-    const headers = {
-      'Content-Range': contentRange,
-      'Content-Disposition': contentDisposition,
-    };
-    const formData = new FormData();
-    formData.append('course-data', blob, fileData.name);
-    const { data } = await getAuthenticatedHttpClient()
-      .post(
-        postImportCourseApiUrl(courseId),
-        formData,
-        { headers, ...requestConfig },
-      );
-    resp = camelCaseObject(data);
-  };
-
-  const chunkUpload = async (file, index) => {
-    const start = index * chunkSize;
-    const stop = start + chunkSize < fileSize ? start + chunkSize : fileSize;
-    const blob = file.slice(start, stop, file.type);
-    await upload(blob, start, stop - 1);
-  };
-
-  /* eslint-disable no-await-in-loop */
-  for (let i = 0; i < chunkLength; i++) {
-    await chunkUpload(fileData, i);
-  }
-
-  return resp;
+  const { data } = await getAuthenticatedHttpClient()
+    .post(postImportCourseApiUrl(courseId), { 'course-data': fileData }, { headers: { 'content-type': 'multipart/form-data' }, ...requestConfig });
+  return camelCaseObject(data);
 }
 
 /**
