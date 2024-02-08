1f6d30ff239a5062f3c0d1c9aeefa2983f16c497
Jhon Vente <jhon.vente@edunext.co>
2024-02-08 13:56:25 -0500

    test: adding test for studio home slice

---
+++ src/studio-home/data/slice.test.js
@@ -0,0 +1,42 @@
+import { reducer, updateStudioHomeCoursesCustomParams } from './slice'; // Assuming the file is named slice.js
+
+import { RequestStatus } from '../../data/constants';
+
+describe('updateStudioHomeCoursesCustomParams action', () => {
+  const initialState = {
+    loadingStatuses: {
+      studioHomeLoadingStatus: RequestStatus.IN_PROGRESS,
+      courseNotificationLoadingStatus: RequestStatus.IN_PROGRESS,
+      courseLoadingStatus: RequestStatus.IN_PROGRESS,
+      libraryLoadingStatus: RequestStatus.IN_PROGRESS,
+    },
+    savingStatuses: {
+      courseCreatorSavingStatus: '',
+      deleteNotificationSavingStatus: '',
+    },
+    studioHomeData: {},
+    studioHomeCoursesCustomParams: {
+      currentPage: 1,
+    },
+  };
+
+  it('should return the initial state', () => {
+    const result = reducer(undefined, { type: undefined });
+    expect(result).toEqual(initialState);
+  });
+
+  it('should update the currentPage in studioHomeCoursesCustomParams', () => {
+    const newState = {
+      ...initialState,
+      studioHomeCoursesCustomParams: {
+        currentPage: 2,
+      },
+    };
+    const payload = {
+      currentPage: 2,
+    };
+
+    const result = reducer(initialState, updateStudioHomeCoursesCustomParams(payload));
+    expect(result).toEqual(newState);
+  });
+});
