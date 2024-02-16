effca983e2c8798eb00a1c67d4e510ee4bffcd31
Rômulo Penido <romulo@opencraft.com>
2024-02-16 10:34:58 -0300

    test: check menu titles instead of count

--- src/header/utils.test.js
+++ src/header/utils.test.js
@@ -5,7 +5,7 @@ const props = {
   studioBaseUrl: 'UrLSTuiO',
   courseId: '123',
   intl: {
-    formatMessage: jest.fn(),
+    formatMessage: jest.fn(message => message.defaultMessage),
   },
 };
 
@@ -28,22 +28,32 @@ describe('header utils', () => {
       expect(actualItems).toHaveLength(4);
     });
   });
+
   describe('getToolsMenuItems', () => {
     it('should include export tags option', () => {
       setConfig({
         ...getConfig(),
         ENABLE_TAGGING_TAXONOMY_PAGES: 'true',
       });
-      const actualItems = getToolsMenuItems(props);
-      expect(actualItems).toHaveLength(4);
+      const actualItemsTitle = getToolsMenuItems(props).map((item) => item.title);
+      expect(actualItemsTitle).toEqual([
+        'Import',
+        'Export Course',
+        'Export Tags',
+        'Checklists',
+      ]);
     });
     it('should not include export tags option', () => {
       setConfig({
         ...getConfig(),
         ENABLE_TAGGING_TAXONOMY_PAGES: 'false',
       });
-      const actualItems = getToolsMenuItems(props);
-      expect(actualItems).toHaveLength(3);
+      const actualItemsTitle = getToolsMenuItems(props).map((item) => item.title);
+      expect(actualItemsTitle).toEqual([
+        'Import',
+        'Export',
+        'Checklists',
+      ]);
     });
   });
 });
