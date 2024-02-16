90d5ac4ffc0f456f7242bbee529b6a7ad4d08c87
Jhon Vente <134975835+johnvente@users.noreply.github.com>
2024-02-16 14:02:34 -0500

    feat: TinyMCE plugin insert iframe (#427)

--- src/editors/sharedComponents/TinyMceWidget/index.jsx
+++ src/editors/sharedComponents/TinyMceWidget/index.jsx
@@ -14,6 +14,7 @@ import { selectors } from '../../data/redux';
 import ImageUploadModal from '../ImageUploadModal';
 import SourceCodeModal from '../SourceCodeModal';
 import * as hooks from './hooks';
+import './customTinyMcePlugins/embedIframePlugin';
 
 const editorConfigDefaultProps = {
   setEditorRef: undefined,
--- src/editors/sharedComponents/TinyMceWidget/pluginConfig.js
+++ src/editors/sharedComponents/TinyMceWidget/pluginConfig.js
@@ -33,6 +33,7 @@ const pluginConfig = ({ isLibrary, placeholder, editorType }) => {
         quickToolbar,
         plugins.a11ychecker,
         plugins.powerpaste,
+        plugins.embediframe,
       ].join(' '),
       menubar: false,
       toolbar: toolbar ? mapToolbars([
@@ -54,7 +55,7 @@ const pluginConfig = ({ isLibrary, placeholder, editorType }) => {
         ],
         [imageUploadButton, buttons.link, buttons.unlink, buttons.blockQuote, buttons.codeBlock],
         [buttons.table, buttons.emoticons, buttons.charmap, buttons.hr],
-        [buttons.removeFormat, codeButton, buttons.a11ycheck],
+        [buttons.removeFormat, codeButton, buttons.a11ycheck, buttons.embediframe],
       ]) : false,
       imageToolbar: mapToolbars([
         // [buttons.rotate.left, buttons.rotate.right],
