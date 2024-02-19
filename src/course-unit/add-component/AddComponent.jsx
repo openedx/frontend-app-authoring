raccoongang/frontend-app-course-authoring

feat: [AXIMST-19, 20, 22] Course unit - Modal windows for course unit page components (#118)
* feat: added modal windows for course unit page components

* refactor: code refactoring

* refactor: added translations

* refactor: refactoring after review

* refactor: after review

src/course-unit/add-component/AddComponent.jsx
@@ -2,28 +2,56 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useIntl } from '@edx/frontend-platform/i18n';
-import { Button } from '@edx/paragon';
+import { useToggle } from '@edx/paragon';
 
 import { getCourseSectionVertical } from '../data/selectors';
 import { COMPONENT_ICON_TYPES } from '../constants';
-import ComponentIcon from './ComponentIcon';
+import ComponentModalView from './add-component-modals/ComponentModalView';
+import AddComponentButton from './add-component-btn';
 import messages from './messages';
 
-const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
+const AddComponent = ({ blockId, handleCreateNewCourseXBlock }) => {
   const navigate = useNavigate();
   const intl = useIntl();
+  const [isOpenAdvanced, openAdvanced, closeAdvanced] = useToggle(false);
+  const [isOpenHtml, openHtml, closeHtml] = useToggle(false);
+  const [isOpenOpenAssessment, openOpenAssessment, closeOpenAssessment] = useToggle(false);
   const { componentTemplates } = useSelector(getCourseSectionVertical);
 
-  const handleCreateNewXblock = (type) => () => {
+  const handleCreateNewXBlock = (type, moduleName) => {
     switch (type) {
     case COMPONENT_ICON_TYPES.discussion:
     case COMPONENT_ICON_TYPES.dragAndDrop:
-      handleCreateNewCourseXblock({ type, parentLocator: blockId });
+      handleCreateNewCourseXBlock({ type, parentLocator: blockId });
       break;
     case COMPONENT_ICON_TYPES.problem:
     case COMPONENT_ICON_TYPES.video:
-      handleCreateNewCourseXblock({ type, parentLocator: blockId }, ({ courseKey, locator }) => {
+      handleCreateNewCourseXBlock({ type, parentLocator: blockId }, ({ courseKey, locator }) => {
         navigate(`/course/${courseKey}/editor/${type}/${locator}`);
+      });
+      break;
+    // TODO: The library functional will be a bit different of current legacy (CMS)
+    //  behaviour and this ticket is on hold (blocked by other development team).
+    case COMPONENT_ICON_TYPES.library:
+      handleCreateNewCourseXBlock({ type, category: 'library_content', parentLocator: blockId });
+      break;
+    case COMPONENT_ICON_TYPES.advanced:
+      handleCreateNewCourseXBlock({
+        type: moduleName, category: moduleName, parentLocator: blockId,
+      });
+      break;
+    case COMPONENT_ICON_TYPES.openassessment:
+      handleCreateNewCourseXBlock({
+        boilerplate: moduleName, category: type, parentLocator: blockId,
+      });
+      break;
+    case COMPONENT_ICON_TYPES.html:
+      handleCreateNewCourseXBlock({
+        type,
+        boilerplate: moduleName,
+        parentLocator: blockId,
+      }, ({ courseKey, locator }) => {
+        navigate(`/course/${courseKey}/editor/html/${locator}`);
       });
       break;
     default:
@@ -38,19 +66,53 @@ const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
     <div className="py-4">
       <h5 className="h3 mb-4 text-center">{intl.formatMessage(messages.title)}</h5>
       <ul className="new-component-type list-unstyled m-0 d-flex flex-wrap justify-content-center">
-        {Object.keys(componentTemplates).map((component) => (
-          <li key={componentTemplates[component].type}>
-            <Button
-              variant="outline-primary"
-              className="add-component-button flex-column rounded-sm"
-              onClick={handleCreateNewXblock(componentTemplates[component].type)}
-            >
-              <ComponentIcon type={componentTemplates[component].type} />
-              <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
-              <span className="small mt-2">{componentTemplates[component].displayName}</span>
-            </Button>
-          </li>
-        ))}
+        {componentTemplates.map((component) => {
+          const { type, displayName } = component;
+          let modalParams;
+
+          switch (type) {
+          case COMPONENT_ICON_TYPES.advanced:
+            modalParams = {
+              open: openAdvanced,
+              close: closeAdvanced,
+              isOpen: isOpenAdvanced,
+            };
+            break;
+          case COMPONENT_ICON_TYPES.html:
+            modalParams = {
+              open: openHtml,
+              close: closeHtml,
+              isOpen: isOpenHtml,
+            };
+            break;
+          case COMPONENT_ICON_TYPES.openassessment:
+            modalParams = {
+              open: openOpenAssessment,
+              close: closeOpenAssessment,
+              isOpen: isOpenOpenAssessment,
+            };
+            break;
+          default:
+            return (
+              <li key={type}>
+                <AddComponentButton
+                  onClick={() => handleCreateNewXBlock(type)}
+                  displayName={displayName}
+                  type={type}
+                />
+              </li>
+            );
+          }
+
+          return (
+            <ComponentModalView
+              key={type}
+              component={component}
+              handleCreateNewXBlock={handleCreateNewXBlock}
+              modalParams={modalParams}
+            />
+          );
+        })}
       </ul>
     </div>
   );
@@ -58,7 +120,7 @@ const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
 
 AddComponent.propTypes = {
   blockId: PropTypes.string.isRequired,
-  handleCreateNewCourseXblock: PropTypes.func.isRequired,
+  handleCreateNewCourseXBlock: PropTypes.func.isRequired,
 };
 
 export default AddComponent;

src/course-unit/add-component/add-component-btn/index.jsx
@@ -1,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Button } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import messages from '../messages';
+import AddComponentIcon from './AddComponentIcon';
+
+const AddComponentButton = ({ type, displayName, onClick }) => {
+  const intl = useIntl();
+
+  return (
+    <Button
+      variant="outline-primary"
+      className="add-component-button flex-column rounded-sm"
+      onClick={onClick}
+    >
+      <AddComponentIcon type={type} />
+      <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
+      <span className="small mt-2">{displayName}</span>
+    </Button>
+  );
+};
+
+AddComponentButton.propTypes = {
+  type: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddComponentButton;

src/course-unit/add-component/add-component-modals/ComponentModalView.jsx
@@ -1,0 +1,93 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Form } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { updateQueryPendingStatus } from '../../data/slice';
+import AddComponentButton from '../add-component-btn';
+import messages from '../messages';
+import ModalContainer from './ModalContainer';
+
+const ComponentModalView = ({
+  component,
+  modalParams,
+  handleCreateNewXBlock,
+}) => {
+  const intl = useIntl();
+  const dispatch = useDispatch();
+  const [moduleTitle, setModuleTitle] = useState('');
+  const { open, close, isOpen } = modalParams;
+  const { type, displayName, templates } = component;
+
+  const handleSubmit = () => {
+    handleCreateNewXBlock(type, moduleTitle);
+    dispatch(updateQueryPendingStatus(true));
+    setModuleTitle('');
+  };
+
+  return (
+    <>
+      <li>
+        <AddComponentButton
+          onClick={open}
+          type={type}
+          displayName={displayName}
+        />
+      </li>
+      <ModalContainer
+        isOpen={isOpen}
+        close={close}
+        title={intl.formatMessage(messages.modalContainerTitle, { componentTitle: displayName.toLowerCase() })}
+        btnText={intl.formatMessage(messages.modalBtnText)}
+        onSubmit={handleSubmit}
+        resetDisabled={() => setModuleTitle('')}
+        hasValue={!moduleTitle.length}
+      >
+        <Form.Group>
+          <Form.RadioSet
+            name={displayName}
+            onChange={(e) => setModuleTitle(e.target.value)}
+          >
+            {templates.map((componentTemplate) => {
+              const value = componentTemplate.boilerplateName || componentTemplate.category;
+
+              return (
+                <Form.Radio
+                  key={componentTemplate.displayName}
+                  className="add-component-modal-radio mb-2.5"
+                  value={value}
+                >
+                  {componentTemplate.displayName}
+                </Form.Radio>
+              );
+            })}
+          </Form.RadioSet>
+        </Form.Group>
+      </ModalContainer>
+    </>
+  );
+};
+
+ComponentModalView.propTypes = {
+  modalParams: PropTypes.shape({
+    open: PropTypes.func,
+    close: PropTypes.func,
+    isOpen: PropTypes.bool,
+  }).isRequired,
+  handleCreateNewXBlock: PropTypes.func.isRequired,
+  component: PropTypes.shape({
+    displayName: PropTypes.string.isRequired,
+    category: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    templates: PropTypes.arrayOf(
+      PropTypes.shape({
+        boilerplateName: PropTypes.string,
+        category: PropTypes.string,
+        displayName: PropTypes.string.isRequired,
+      }),
+    ),
+  }).isRequired,
+};
+
+export default ComponentModalView;

openedx/frontend-app-authoring

src/course-unit/add-component/AddComponent.jsx
@@ -2,28 +2,56 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useIntl } from '@edx/frontend-platform/i18n';
-import { Button } from '@openedx/paragon';
+import { useToggle } from '@openedx/paragon';
 
 import { getCourseSectionVertical } from '../data/selectors';
 import { COMPONENT_ICON_TYPES } from '../constants';
-import ComponentIcon from './ComponentIcon';
+import ComponentModalView from './add-component-modals/ComponentModalView';
+import AddComponentButton from './add-component-btn';
 import messages from './messages';
 
-const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
+const AddComponent = ({ blockId, handleCreateNewCourseXBlock }) => {
   const navigate = useNavigate();
   const intl = useIntl();
+  const [isOpenAdvanced, openAdvanced, closeAdvanced] = useToggle(false);
+  const [isOpenHtml, openHtml, closeHtml] = useToggle(false);
+  const [isOpenOpenAssessment, openOpenAssessment, closeOpenAssessment] = useToggle(false);
   const { componentTemplates } = useSelector(getCourseSectionVertical);
 
-  const handleCreateNewXblock = (type) => () => {
+  const handleCreateNewXBlock = (type, moduleName) => {
     switch (type) {
     case COMPONENT_ICON_TYPES.discussion:
     case COMPONENT_ICON_TYPES.dragAndDrop:
-      handleCreateNewCourseXblock({ type, parentLocator: blockId });
+      handleCreateNewCourseXBlock({ type, parentLocator: blockId });
       break;
     case COMPONENT_ICON_TYPES.problem:
     case COMPONENT_ICON_TYPES.video:
-      handleCreateNewCourseXblock({ type, parentLocator: blockId }, ({ courseKey, locator }) => {
+      handleCreateNewCourseXBlock({ type, parentLocator: blockId }, ({ courseKey, locator }) => {
         navigate(`/course/${courseKey}/editor/${type}/${locator}`);
+      });
+      break;
+    // TODO: The library functional will be a bit different of current legacy (CMS)
+    //  behaviour and this ticket is on hold (blocked by other development team).
+    case COMPONENT_ICON_TYPES.library:
+      handleCreateNewCourseXBlock({ type, category: 'library_content', parentLocator: blockId });
+      break;
+    case COMPONENT_ICON_TYPES.advanced:
+      handleCreateNewCourseXBlock({
+        type: moduleName, category: moduleName, parentLocator: blockId,
+      });
+      break;
+    case COMPONENT_ICON_TYPES.openassessment:
+      handleCreateNewCourseXBlock({
+        boilerplate: moduleName, category: type, parentLocator: blockId,
+      });
+      break;
+    case COMPONENT_ICON_TYPES.html:
+      handleCreateNewCourseXBlock({
+        type,
+        boilerplate: moduleName,
+        parentLocator: blockId,
+      }, ({ courseKey, locator }) => {
+        navigate(`/course/${courseKey}/editor/html/${locator}`);
       });
       break;
     default:
@@ -38,19 +66,53 @@ const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
     <div className="py-4">
       <h5 className="h3 mb-4 text-center">{intl.formatMessage(messages.title)}</h5>
       <ul className="new-component-type list-unstyled m-0 d-flex flex-wrap justify-content-center">
-        {Object.keys(componentTemplates).map((component) => (
-          <li key={componentTemplates[component].type}>
-            <Button
-              variant="outline-primary"
-              className="add-component-button flex-column rounded-sm"
-              onClick={handleCreateNewXblock(componentTemplates[component].type)}
-            >
-              <ComponentIcon type={componentTemplates[component].type} />
-              <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
-              <span className="small mt-2">{componentTemplates[component].displayName}</span>
-            </Button>
-          </li>
-        ))}
+        {componentTemplates.map((component) => {
+          const { type, displayName } = component;
+          let modalParams;
+
+          switch (type) {
+          case COMPONENT_ICON_TYPES.advanced:
+            modalParams = {
+              open: openAdvanced,
+              close: closeAdvanced,
+              isOpen: isOpenAdvanced,
+            };
+            break;
+          case COMPONENT_ICON_TYPES.html:
+            modalParams = {
+              open: openHtml,
+              close: closeHtml,
+              isOpen: isOpenHtml,
+            };
+            break;
+          case COMPONENT_ICON_TYPES.openassessment:
+            modalParams = {
+              open: openOpenAssessment,
+              close: closeOpenAssessment,
+              isOpen: isOpenOpenAssessment,
+            };
+            break;
+          default:
+            return (
+              <li key={type}>
+                <AddComponentButton
+                  onClick={() => handleCreateNewXBlock(type)}
+                  displayName={displayName}
+                  type={type}
+                />
+              </li>
+            );
+          }
+
+          return (
+            <ComponentModalView
+              key={type}
+              component={component}
+              handleCreateNewXBlock={handleCreateNewXBlock}
+              modalParams={modalParams}
+            />
+          );
+        })}
       </ul>
     </div>
   );
@@ -58,7 +120,7 @@ const AddComponent = ({ blockId, handleCreateNewCourseXblock }) => {
 
 AddComponent.propTypes = {
   blockId: PropTypes.string.isRequired,
-  handleCreateNewCourseXblock: PropTypes.func.isRequired,
+  handleCreateNewCourseXBlock: PropTypes.func.isRequired,
 };
 
 export default AddComponent;

src/course-unit/add-component/add-component-btn/index.jsx
@@ -1,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { Button } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import messages from '../messages';
+import AddComponentIcon from './AddComponentIcon';
+
+const AddComponentButton = ({ type, displayName, onClick }) => {
+  const intl = useIntl();
+
+  return (
+    <Button
+      variant="outline-primary"
+      className="add-component-button flex-column rounded-sm"
+      onClick={onClick}
+    >
+      <AddComponentIcon type={type} />
+      <span className="sr-only">{intl.formatMessage(messages.buttonText)}</span>
+      <span className="small mt-2">{displayName}</span>
+    </Button>
+  );
+};
+
+AddComponentButton.propTypes = {
+  type: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddComponentButton;

src/course-unit/add-component/add-component-modals/ComponentModalView.jsx
@@ -1,0 +1,93 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Form } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { updateQueryPendingStatus } from '../../data/slice';
+import AddComponentButton from '../add-component-btn';
+import messages from '../messages';
+import ModalContainer from './ModalContainer';
+
+const ComponentModalView = ({
+  component,
+  modalParams,
+  handleCreateNewXBlock,
+}) => {
+  const intl = useIntl();
+  const dispatch = useDispatch();
+  const [moduleTitle, setModuleTitle] = useState('');
+  const { open, close, isOpen } = modalParams;
+  const { type, displayName, templates } = component;
+
+  const handleSubmit = () => {
+    handleCreateNewXBlock(type, moduleTitle);
+    dispatch(updateQueryPendingStatus(true));
+    setModuleTitle('');
+  };
+
+  return (
+    <>
+      <li>
+        <AddComponentButton
+          onClick={open}
+          type={type}
+          displayName={displayName}
+        />
+      </li>
+      <ModalContainer
+        isOpen={isOpen}
+        close={close}
+        title={intl.formatMessage(messages.modalContainerTitle, { componentTitle: displayName.toLowerCase() })}
+        btnText={intl.formatMessage(messages.modalBtnText)}
+        onSubmit={handleSubmit}
+        resetDisabled={() => setModuleTitle('')}
+        hasValue={!moduleTitle.length}
+      >
+        <Form.Group>
+          <Form.RadioSet
+            name={displayName}
+            onChange={(e) => setModuleTitle(e.target.value)}
+          >
+            {templates.map((componentTemplate) => {
+              const value = componentTemplate.boilerplateName || componentTemplate.category;
+
+              return (
+                <Form.Radio
+                  key={componentTemplate.displayName}
+                  className="add-component-modal-radio mb-2.5"
+                  value={value}
+                >
+                  {componentTemplate.displayName}
+                </Form.Radio>
+              );
+            })}
+          </Form.RadioSet>
+        </Form.Group>
+      </ModalContainer>
+    </>
+  );
+};
+
+ComponentModalView.propTypes = {
+  modalParams: PropTypes.shape({
+    open: PropTypes.func,
+    close: PropTypes.func,
+    isOpen: PropTypes.bool,
+  }).isRequired,
+  handleCreateNewXBlock: PropTypes.func.isRequired,
+  component: PropTypes.shape({
+    displayName: PropTypes.string.isRequired,
+    category: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    templates: PropTypes.arrayOf(
+      PropTypes.shape({
+        boilerplateName: PropTypes.string,
+        category: PropTypes.string,
+        displayName: PropTypes.string.isRequired,
+      }),
+    ),
+  }).isRequired,
+};
+
+export default ComponentModalView;