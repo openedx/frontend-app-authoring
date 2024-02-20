d0b1bec109f6c6e266653c3a2f42d74611b05124
Rômulo Penido <romulo@opencraft.com>
2024-02-20 10:02:57 -0300

    fix: wrong link

--- src/header/utils.js
+++ src/header/utils.js
@@ -64,7 +64,7 @@ export const getToolsMenuItems = ({ studioBaseUrl, courseId, intl }) => ([
     title: intl.formatMessage(messages['header.links.import']),
   },
   {
-    href: `${studioBaseUrl}/import/${courseId}`,
+    href: `${studioBaseUrl}/export/${courseId}`,
     title: intl.formatMessage(messages['header.links.exportCourse']),
   },
   ...(getConfig().ENABLE_TAGGING_TAXONOMY_PAGES === 'true'
