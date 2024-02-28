50794f549c8bf694f8af0bd0e64985d45e32cb21
XnpioChV <xnpiochv@gmail.com>
2024-02-28 12:08:42 -0500

    feat: Add button feat to Tag count

--- src/generic/tag-count/TagCount.test.jsx
+++ src/generic/tag-count/TagCount.test.jsx
@@ -4,14 +4,19 @@ import TagCount from '.';
 
 describe('<TagCount>', () => {
   it('should render the component', () => {
-    const count = 17;
-    render(<TagCount count={count} />);
+    render(<TagCount count={17} />);
     expect(screen.getByText('17')).toBeInTheDocument();
   });
 
   it('should render the component with zero', () => {
-    const count = 0;
-    render(<TagCount count={count} />);
+    render(<TagCount count={0} />);
     expect(screen.getByText('0')).toBeInTheDocument();
+  });
+
+  it('should render a button with onClick', () => {
+    render(<TagCount count={17} onClick={() => {}} />);
+    expect(screen.getByRole('button', {
+      name: /17/i,
+    }));
   });
 });
