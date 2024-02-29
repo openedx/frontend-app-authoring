raccoongang/frontend-app-course-authoring

feat: [AXIMST-23] Course unit - Sidebar with unit info (#117)
* feat: added Sidebar with unit info

* feat: added unit location

* refactor: added legacy behavior

* feat: added live variant

* refactor: code refactoring

* feat: added tests and translations

* feat: added new font size

* refactor: after review

src/course-unit/sidebar/components/SidebarBody.jsx
@@ -1,0 +1,65 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Card, Stack } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { getCourseUnitData } from '../../data/selectors';
+import { getPublishInfo } from '../utils';
+import messages from '../messages';
+import ReleaseInfoComponent from './ReleaseInfoComponent';
+
+const SidebarBody = ({ releaseLabel, isDisplayUnitLocation, locationId }) => {
+  const intl = useIntl();
+  const {
+    editedOn,
+    editedBy,
+    hasChanges,
+    publishedBy,
+    publishedOn,
+  } = useSelector(getCourseUnitData);
+
+  return (
+    <Card.Body className="course-unit-sidebar-date">
+      <Stack>
+        {isDisplayUnitLocation ? (
+          <span>
+            <h5 className="course-unit-sidebar-date-stage m-0">
+              {intl.formatMessage(messages.unitLocationTitle)}
+            </h5>
+            <p className="m-0 font-weight-bold">
+              {locationId}
+            </p>
+          </span>
+        ) : (
+          <>
+            <span>
+              {getPublishInfo(intl, hasChanges, editedBy, editedOn, publishedBy, publishedOn)}
+            </span>
+            <span className="mt-3.5">
+              <h5 className="course-unit-sidebar-date-stage m-0">
+                {releaseLabel}
+              </h5>
+              <ReleaseInfoComponent />
+            </span>
+            <p className="mt-3.5 mb-0">
+              {intl.formatMessage(messages.sidebarBodyNote)}
+            </p>
+          </>
+        )}
+      </Stack>
+    </Card.Body>
+  );
+};
+
+SidebarBody.propTypes = {
+  releaseLabel: PropTypes.string.isRequired,
+  isDisplayUnitLocation: PropTypes.bool,
+  locationId: PropTypes.string,
+};
+
+SidebarBody.defaultProps = {
+  isDisplayUnitLocation: false,
+  locationId: null,
+};
+
+export default SidebarBody;

src/course-unit/sidebar/components/SidebarHeader.jsx
@@ -1,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Icon, Stack } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { getCourseUnitData } from '../../data/selectors';
+import { getIconVariant } from '../utils';
+import messages from '../messages';
+
+const SidebarHeader = ({ title, visibilityState, isDisplayUnitLocation }) => {
+  const intl = useIntl();
+  const { hasChanges, published } = useSelector(getCourseUnitData);
+  const { iconSrc, colorVariant } = getIconVariant(visibilityState, published, hasChanges);
+
+  return (
+    <Stack className="course-unit-sidebar-header" direction="horizontal">
+      {!isDisplayUnitLocation && (
+        <Icon
+          className="course-unit-sidebar-header-icon"
+          svgAttrs={{ color: colorVariant }}
+          src={iconSrc}
+        />
+      )}
+      <h3 className="course-unit-sidebar-header-title m-0">
+        {isDisplayUnitLocation ? intl.formatMessage(messages.sidebarHeaderUnitLocationTitle) : title}
+      </h3>
+    </Stack>
+  );
+};
+
+SidebarHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  visibilityState: PropTypes.string.isRequired,
+  isDisplayUnitLocation: PropTypes.bool,
+};
+
+SidebarHeader.defaultProps = {
+  isDisplayUnitLocation: false,
+};
+
+export default SidebarHeader;

src/course-unit/sidebar/components/sidebar-footer/ActionButtons.jsx
@@ -1,0 +1,49 @@
+import { useSelector } from 'react-redux';
+import { Button } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { getCourseUnitData } from '../../../data/selectors';
+import messages from '../../messages';
+
+const ActionButtons = () => {
+  const intl = useIntl();
+  const {
+    published,
+    hasChanges,
+    enableCopyPasteUnits,
+  } = useSelector(getCourseUnitData);
+
+  return (
+    <>
+      {(!published || hasChanges) && (
+        <Button
+          className="mt-3.5"
+          variant="outline-primary"
+          size="sm"
+        >
+          {intl.formatMessage(messages.actionButtonPublishTitle)}
+        </Button>
+      )}
+      {(published && hasChanges) && (
+        <Button
+          className="mt-2"
+          variant="link"
+          size="sm"
+        >
+          {intl.formatMessage(messages.actionButtonDiscardChangesTitle)}
+        </Button>
+      )}
+      {enableCopyPasteUnits && (
+        <Button
+          className="mt-2"
+          variant="outline-primary"
+          size="sm"
+        >
+          {intl.formatMessage(messages.actionButtonCopyUnitTitle)}
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default ActionButtons;

src/course-unit/sidebar/components/sidebar-footer/index.jsx
@@ -1,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { Card, Stack } from '@edx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import messages from '../../messages';
+import UnitVisibilityInfo from './UnitVisibilityInfo';
+import ActionButtons from './ActionButtons';
+
+const SidebarFooter = ({ isDisplayUnitLocation, locationId }) => {
+  const intl = useIntl();
+
+  return (
+    <Card.Footer className="course-unit-sidebar-footer" orientation="horizontal">
+      <Stack className="course-unit-sidebar-visibility">
+        {isDisplayUnitLocation ? (
+          <small className="course-unit-sidebar-location-description">
+            {intl.formatMessage(messages.unitLocationDescription, { id: locationId })}
+          </small>
+        ) : (
+          <>
+            <UnitVisibilityInfo />
+            <ActionButtons />
+          </>
+        )}
+      </Stack>
+    </Card.Footer>
+  );
+};
+
+SidebarFooter.propTypes = {
+  isDisplayUnitLocation: PropTypes.bool,
+  locationId: PropTypes.string,
+};
+
+SidebarFooter.defaultProps = {
+  isDisplayUnitLocation: false,
+  locationId: null,
+};
+
+export default SidebarFooter;

src/course-unit/sidebar/utils.js
@@ -1,0 +1,102 @@
+import {
+  CheckCircle as CheckCircleIcon,
+  CheckCircleOutline as CheckCircleOutlineIcon,
+  InfoOutline as InfoOutlineIcon,
+} from '@edx/paragon/icons';
+
+import { COLORS, UNIT_VISIBILITY_STATES } from '../constants';
+import messages from './messages';
+
+/**
+ * Get information about the publishing status.
+ * @param {Object} intl - The internationalization object.
+ * @param {boolean} hasChanges - Indicates if there are unpublished changes.
+ * @param {string} editedBy - The user who edited the content.
+ * @param {string} editedOn - The timestamp when the content was edited.
+ * @param {string} publishedBy - The user who last published the content.
+ * @param {string} publishedOn - The timestamp when the content was last published.
+ * @returns {string} Publish information based on the provided parameters.
+ */
+export const getPublishInfo = (intl, hasChanges, editedBy, editedOn, publishedBy, publishedOn) => {
+  let publishInfoText;
+  if (hasChanges && editedOn && editedBy) {
+    publishInfoText = intl.formatMessage(messages.publishInfoDraftSaved, { editedOn, editedBy });
+  } else if (publishedOn && publishedBy) {
+    publishInfoText = intl.formatMessage(messages.publishLastPublished, { publishedOn, publishedBy });
+  } else {
+    publishInfoText = intl.formatMessage(messages.publishInfoPreviouslyPublished);
+  }
+
+  return publishInfoText;
+};
+
+/**
+ * Get information about the release status.
+ * @param {Object} intl - The internationalization object.
+ * @param {string} releaseDate - The release date of the content.
+ * @param {string} releaseDateFrom - The section name associated with the release date.
+ * @returns {string|ReactElement} Release information based on the provided parameters.
+ */
+export const getReleaseInfo = (intl, releaseDate, releaseDateFrom) => {
+  if (releaseDate) {
+    return {
+      isScheduled: true,
+      releaseDate,
+      releaseDateFrom,
+      sectionNameMessage: intl.formatMessage(messages.releaseInfoWithSection, { sectionName: releaseDateFrom }),
+    };
+  }
+  return {
+    isScheduled: false,
+    message: intl.formatMessage(messages.releaseInfoUnscheduled),
+  };
+};
+
+/**
+ * Get the visibility title.
+ * @param {Object} intl - The internationalization object.
+ * @param {boolean} releasedToStudents - Indicates if the content is released to students.
+ * @param {boolean} published - Indicates if the content is published.
+ * @param {boolean} hasChanges - Indicates if there are unpublished changes.
+ * @returns {string} The visibility title determined by the provided parameters.
+ */
+export const getVisibilityTitle = (intl, releasedToStudents, published, hasChanges) => {
+  if (releasedToStudents && published && !hasChanges) {
+    return intl.formatMessage(messages.visibilityIsVisibleToTitle);
+  }
+
+  return intl.formatMessage(messages.visibilityWillBeVisibleToTitle);
+};
+
+/**
+ * Get the icon variant based on the provided visibility state and publication status.
+ * @param {string} visibilityState - The visibility state of the content.
+ * @param {boolean} published - Indicates if the content is published.
+ * @param {boolean} hasChanges - Indicates if there are unpublished changes.
+ * @returns {Object} An object containing the icon component and color variant.
+ *   - iconSrc: The source component for the icon.
+ *   - colorVariant: The color variant for the icon.
+ */
+export const getIconVariant = (visibilityState, published, hasChanges) => {
+  const iconVariants = {
+    [UNIT_VISIBILITY_STATES.staffOnly]: { iconSrc: InfoOutlineIcon, colorVariant: COLORS.BLACK },
+    [UNIT_VISIBILITY_STATES.live]: { iconSrc: CheckCircleIcon, colorVariant: COLORS.GREEN },
+    publishedNoChanges: { iconSrc: CheckCircleOutlineIcon, colorVariant: COLORS.BLACK },
+    publishedWithChanges: { iconSrc: InfoOutlineIcon, colorVariant: COLORS.BLACK },
+    default: { iconSrc: InfoOutlineIcon, colorVariant: COLORS.BLACK },
+  };
+  if (visibilityState in iconVariants) {
+    return iconVariants[visibilityState];
+  }
+  if (published) {
+    return hasChanges ? iconVariants.publishedWithChanges : iconVariants.publishedNoChanges;
+  }
+  return iconVariants.default;
+};
+
+/**
+ * Extracts the clear course unit ID from the given course unit data.
+ * @param {string} id - The course unit ID.
+ * @returns {string} The clear course unit ID extracted from the provided data.
+ */
+export const extractCourseUnitId = (id) => id.match(/block@(.+)$/)[1];

openedx/frontend-app-authoring

src/course-unit/sidebar/components/SidebarBody.jsx
@@ -1,0 +1,65 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Card, Stack } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { getCourseUnitData } from '../../data/selectors';
+import { getPublishInfo } from '../utils';
+import messages from '../messages';
+import ReleaseInfoComponent from './ReleaseInfoComponent';
+
+const SidebarBody = ({ releaseLabel, isDisplayUnitLocation, locationId }) => {
+  const intl = useIntl();
+  const {
+    editedOn,
+    editedBy,
+    hasChanges,
+    publishedBy,
+    publishedOn,
+  } = useSelector(getCourseUnitData);
+
+  return (
+    <Card.Body className="course-unit-sidebar-date">
+      <Stack>
+        {isDisplayUnitLocation ? (
+          <span>
+            <h5 className="course-unit-sidebar-date-stage m-0">
+              {intl.formatMessage(messages.unitLocationTitle)}
+            </h5>
+            <p className="m-0 font-weight-bold">
+              {locationId}
+            </p>
+          </span>
+        ) : (
+          <>
+            <span>
+              {getPublishInfo(intl, hasChanges, editedBy, editedOn, publishedBy, publishedOn)}
+            </span>
+            <span className="mt-3.5">
+              <h5 className="course-unit-sidebar-date-stage m-0">
+                {releaseLabel}
+              </h5>
+              <ReleaseInfoComponent />
+            </span>
+            <p className="mt-3.5 mb-0">
+              {intl.formatMessage(messages.sidebarBodyNote)}
+            </p>
+          </>
+        )}
+      </Stack>
+    </Card.Body>
+  );
+};
+
+SidebarBody.propTypes = {
+  releaseLabel: PropTypes.string.isRequired,
+  isDisplayUnitLocation: PropTypes.bool,
+  locationId: PropTypes.string,
+};
+
+SidebarBody.defaultProps = {
+  isDisplayUnitLocation: false,
+  locationId: null,
+};
+
+export default SidebarBody;

src/course-unit/sidebar/components/SidebarHeader.jsx
@@ -1,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { Icon, Stack } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { getCourseUnitData } from '../../data/selectors';
+import { getIconVariant } from '../utils';
+import messages from '../messages';
+
+const SidebarHeader = ({ title, visibilityState, isDisplayUnitLocation }) => {
+  const intl = useIntl();
+  const { hasChanges, published } = useSelector(getCourseUnitData);
+  const { iconSrc, colorVariant } = getIconVariant(visibilityState, published, hasChanges);
+
+  return (
+    <Stack className="course-unit-sidebar-header" direction="horizontal">
+      {!isDisplayUnitLocation && (
+        <Icon
+          className="course-unit-sidebar-header-icon"
+          svgAttrs={{ color: colorVariant }}
+          src={iconSrc}
+        />
+      )}
+      <h3 className="course-unit-sidebar-header-title m-0">
+        {isDisplayUnitLocation ? intl.formatMessage(messages.sidebarHeaderUnitLocationTitle) : title}
+      </h3>
+    </Stack>
+  );
+};
+
+SidebarHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  visibilityState: PropTypes.string.isRequired,
+  isDisplayUnitLocation: PropTypes.bool,
+};
+
+SidebarHeader.defaultProps = {
+  isDisplayUnitLocation: false,
+};
+
+export default SidebarHeader;

src/course-unit/sidebar/components/sidebar-footer/ActionButtons.jsx
@@ -1,0 +1,49 @@
+import { useSelector } from 'react-redux';
+import { Button } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import { getCourseUnitData } from '../../../data/selectors';
+import messages from '../../messages';
+
+const ActionButtons = () => {
+  const intl = useIntl();
+  const {
+    published,
+    hasChanges,
+    enableCopyPasteUnits,
+  } = useSelector(getCourseUnitData);
+
+  return (
+    <>
+      {(!published || hasChanges) && (
+        <Button
+          className="mt-3.5"
+          variant="outline-primary"
+          size="sm"
+        >
+          {intl.formatMessage(messages.actionButtonPublishTitle)}
+        </Button>
+      )}
+      {(published && hasChanges) && (
+        <Button
+          className="mt-2"
+          variant="link"
+          size="sm"
+        >
+          {intl.formatMessage(messages.actionButtonDiscardChangesTitle)}
+        </Button>
+      )}
+      {enableCopyPasteUnits && (
+        <Button
+          className="mt-2"
+          variant="outline-primary"
+          size="sm"
+        >
+          {intl.formatMessage(messages.actionButtonCopyUnitTitle)}
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default ActionButtons;

src/course-unit/sidebar/components/sidebar-footer/index.jsx
@@ -1,0 +1,40 @@
+import PropTypes from 'prop-types';
+import { Card, Stack } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
+
+import messages from '../../messages';
+import UnitVisibilityInfo from './UnitVisibilityInfo';
+import ActionButtons from './ActionButtons';
+
+const SidebarFooter = ({ isDisplayUnitLocation, locationId }) => {
+  const intl = useIntl();
+
+  return (
+    <Card.Footer className="course-unit-sidebar-footer" orientation="horizontal">
+      <Stack className="course-unit-sidebar-visibility">
+        {isDisplayUnitLocation ? (
+          <small className="course-unit-sidebar-location-description">
+            {intl.formatMessage(messages.unitLocationDescription, { id: locationId })}
+          </small>
+        ) : (
+          <>
+            <UnitVisibilityInfo />
+            <ActionButtons />
+          </>
+        )}
+      </Stack>
+    </Card.Footer>
+  );
+};
+
+SidebarFooter.propTypes = {
+  isDisplayUnitLocation: PropTypes.bool,
+  locationId: PropTypes.string,
+};
+
+SidebarFooter.defaultProps = {
+  isDisplayUnitLocation: false,
+  locationId: null,
+};
+
+export default SidebarFooter;

src/course-unit/sidebar/utils.js
@@ -1,0 +1,102 @@
+import {
+  CheckCircle as CheckCircleIcon,
+  CheckCircleOutline as CheckCircleOutlineIcon,
+  InfoOutline as InfoOutlineIcon,
+} from '@openedx/paragon/icons';
+
+import { COLORS, UNIT_VISIBILITY_STATES } from '../constants';
+import messages from './messages';
+
+/**
+ * Get information about the publishing status.
+ * @param {Object} intl - The internationalization object.
+ * @param {boolean} hasChanges - Indicates if there are unpublished changes.
+ * @param {string} editedBy - The user who edited the content.
+ * @param {string} editedOn - The timestamp when the content was edited.
+ * @param {string} publishedBy - The user who last published the content.
+ * @param {string} publishedOn - The timestamp when the content was last published.
+ * @returns {string} Publish information based on the provided parameters.
+ */
+export const getPublishInfo = (intl, hasChanges, editedBy, editedOn, publishedBy, publishedOn) => {
+  let publishInfoText;
+  if (hasChanges && editedOn && editedBy) {
+    publishInfoText = intl.formatMessage(messages.publishInfoDraftSaved, { editedOn, editedBy });
+  } else if (publishedOn && publishedBy) {
+    publishInfoText = intl.formatMessage(messages.publishLastPublished, { publishedOn, publishedBy });
+  } else {
+    publishInfoText = intl.formatMessage(messages.publishInfoPreviouslyPublished);
+  }
+
+  return publishInfoText;
+};
+
+/**
+ * Get information about the release status.
+ * @param {Object} intl - The internationalization object.
+ * @param {string} releaseDate - The release date of the content.
+ * @param {string} releaseDateFrom - The section name associated with the release date.
+ * @returns {string|ReactElement} Release information based on the provided parameters.
+ */
+export const getReleaseInfo = (intl, releaseDate, releaseDateFrom) => {
+  if (releaseDate) {
+    return {
+      isScheduled: true,
+      releaseDate,
+      releaseDateFrom,
+      sectionNameMessage: intl.formatMessage(messages.releaseInfoWithSection, { sectionName: releaseDateFrom }),
+    };
+  }
+  return {
+    isScheduled: false,
+    message: intl.formatMessage(messages.releaseInfoUnscheduled),
+  };
+};
+
+/**
+ * Get the visibility title.
+ * @param {Object} intl - The internationalization object.
+ * @param {boolean} releasedToStudents - Indicates if the content is released to students.
+ * @param {boolean} published - Indicates if the content is published.
+ * @param {boolean} hasChanges - Indicates if there are unpublished changes.
+ * @returns {string} The visibility title determined by the provided parameters.
+ */
+export const getVisibilityTitle = (intl, releasedToStudents, published, hasChanges) => {
+  if (releasedToStudents && published && !hasChanges) {
+    return intl.formatMessage(messages.visibilityIsVisibleToTitle);
+  }
+
+  return intl.formatMessage(messages.visibilityWillBeVisibleToTitle);
+};
+
+/**
+ * Get the icon variant based on the provided visibility state and publication status.
+ * @param {string} visibilityState - The visibility state of the content.
+ * @param {boolean} published - Indicates if the content is published.
+ * @param {boolean} hasChanges - Indicates if there are unpublished changes.
+ * @returns {Object} An object containing the icon component and color variant.
+ *   - iconSrc: The source component for the icon.
+ *   - colorVariant: The color variant for the icon.
+ */
+export const getIconVariant = (visibilityState, published, hasChanges) => {
+  const iconVariants = {
+    [UNIT_VISIBILITY_STATES.staffOnly]: { iconSrc: InfoOutlineIcon, colorVariant: COLORS.BLACK },
+    [UNIT_VISIBILITY_STATES.live]: { iconSrc: CheckCircleIcon, colorVariant: COLORS.GREEN },
+    publishedNoChanges: { iconSrc: CheckCircleOutlineIcon, colorVariant: COLORS.BLACK },
+    publishedWithChanges: { iconSrc: InfoOutlineIcon, colorVariant: COLORS.BLACK },
+    default: { iconSrc: InfoOutlineIcon, colorVariant: COLORS.BLACK },
+  };
+  if (visibilityState in iconVariants) {
+    return iconVariants[visibilityState];
+  }
+  if (published) {
+    return hasChanges ? iconVariants.publishedWithChanges : iconVariants.publishedNoChanges;
+  }
+  return iconVariants.default;
+};
+
+/**
+ * Extracts the clear course unit ID from the given course unit data.
+ * @param {string} id - The course unit ID.
+ * @returns {string} The clear course unit ID extracted from the provided data.
+ */
+export const extractCourseUnitId = (id) => id.match(/block@(.+)$/)[1];