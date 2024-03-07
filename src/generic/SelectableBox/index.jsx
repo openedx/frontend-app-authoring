6d39f614c33e8b93434052d6538e89d5905539cb
Jesper Hodge <19345795+jesperhodge@users.noreply.github.com>
2024-03-07 16:09:42 -0500

    Revert "Fix  radio context provider missing (#874)"
    This reverts commit 896969c7de84dd1e2e0a20d702a48f8314540c54.

---
+++ src/generic/SelectableBox/index.jsx
@@ -0,0 +1,7 @@
+import { SelectableBox as ParagonSelectableBox } from '@openedx/paragon';
+import SelectableBoxSet from './SelectableBoxSet';
+
+const SelectableBox = ParagonSelectableBox;
+
+SelectableBox.Set = SelectableBoxSet;
+export default SelectableBox;
