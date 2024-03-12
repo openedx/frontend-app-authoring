70bf9413989e8dc864344af466b21225946fd90b
Ihor Romaniuk <notsuchasall25@gmail.com>
2024-03-12 10:42:35 +0100

    feat: [FC-0044] Unit page - display component support label

--- src/course-unit/add-component/messages.js
+++ src/course-unit/add-component/messages.js
@@ -21,6 +21,40 @@ const messages = defineMessages({
     id: 'course-authoring.course-unit.modal.container.cancel.button.text',
     defaultMessage: 'Cancel',
   },
+  modalComponentSupportLabelFullySupported: {
+    id: 'course-authoring.course-unit.modal.component.support.label.fully-supported',
+    defaultMessage: 'Fully supported',
+  },
+  modalComponentSupportLabelProvisionallySupported: {
+    id: 'course-authoring.course-unit.modal.component.support.label.provisionally-support',
+    defaultMessage: 'Provisionally supported',
+  },
+  modalComponentSupportLabelNotSupported: {
+    id: 'course-authoring.course-unit.modal.component.support.label.not-supported',
+    defaultMessage: 'Not supported',
+  },
+  modalComponentSupportTooltipFullySupported: {
+    id: 'course-authoring.course-unit.modal.component.support.tooltip.fully-supported',
+    defaultMessage: 'Fully supported tools and features are available on edX, are '
+      + 'fully tested, have user interfaces where applicable, and are documented in the '
+      + 'official edX guides that are available on docs.edx.org.',
+  },
+  modalComponentSupportTooltipNotSupported: {
+    id: 'course-authoring.course-unit.modal.component.support.tooltip.not-supported',
+    defaultMessage: 'Tools with no support are not maintained by edX, and might be '
+      + 'deprecated in the future. They are not recommended for use in courses due to '
+      + 'non-compliance with one or more of the base requirements, such as testing, '
+      + 'accessibility, internationalization, and documentation.',
+  },
+  modalComponentSupportTooltipProvisionallySupported: {
+    id: 'course-authoring.course-unit.modal.component.support.tooltip.provisionally-support',
+    defaultMessage: 'Provisionally supported tools might lack the robustness of functionality '
+      + 'that your courses require. edX does not have control over the quality of the software, '
+      + 'or of the content that can be provided using these tools. Test these tools thoroughly '
+      + 'before using them in your course, especially in graded sections. Complete documentation '
+      + 'might not be available for provisionally supported tools, or documentation might be '
+      + 'available from sources other than edX.',
+  },
 });
 
 export default messages;
