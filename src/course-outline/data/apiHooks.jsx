e6bc92da028d60943948d21643ab2f2f62fdd226
Rômulo Penido <romulo@opencraft.com>
2024-03-12 11:56:39 -0300

    docs: add comments to useContentTagsCount

--- src/course-outline/data/apiHooks.jsx
+++ src/course-outline/data/apiHooks.jsx
@@ -11,13 +11,15 @@ import { getTagsCount } from './api';
 export const useContentTagsCount = (contentId) => {
   let contentPattern;
   if (contentId.includes('course-v1')) {
+    // If the contentId is a course, we want to get the tags count only for the course
     contentPattern = contentId;
   } else {
+    // If the contentId is not a course, we want to get the tags count for all the content of the course
     contentPattern = contentId.replace(/\+type@.*$/, '*');
   }
   return useQuery({
     queryKey: ['contentTagsCount', contentPattern],
     queryFn: /* istanbul ignore next */ () => getTagsCount(contentPattern),
-    select: (data) => data[contentId] || 0,
+    select: (data) => data[contentId] || 0, // Return the tags count of the specific contentId
   });
 };
