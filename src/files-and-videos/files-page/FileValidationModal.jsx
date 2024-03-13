9740974bbd4806eb8f7acaf166751049f59cfedd
Kristin Aoki <42981026+KristinAoki@users.noreply.github.com>
2024-03-13 12:09:00 -0400

    feat: add duplicate file validation for asset upload (#885)
    * feat: add duplicate file validation for asset upload
    
    * fix: modal only appearing once
    
    * feat: add tests for overwrite modal
    
    * fix: input not allowing second upload of same file
    
    * fix: default pageSize for asset details

---
+++ src/files-and-videos/files-page/FileValidationModal.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { injectIntl, FormattedMessage, intlShape } from '@edx/frontend-platform/i18n';
+import {
+  ActionRow,
+  Button,
+  ModalDialog,
+  useToggle,
+} from '@openedx/paragon';
+import { isEmpty } from 'lodash';
+
+import messages from './messages';
+
+const FileValidationModal = ({
+  handleFileOverwrite,
+  // injected
+  intl,
+}) => {
+  const [isOpen, open, close] = useToggle();
+
+  const { duplicateFiles } = useSelector(state => state.assets);
+
+  useEffect(() => {
+    if (!isEmpty(duplicateFiles)) {
+      open();
+    }
+  }, [duplicateFiles]);
+
+  return (
+    <ModalDialog
+      title={intl.formatMessage(messages.overwriteModalTitle)}
+      isOpen={isOpen}
+      onClose={close}
+    >
+      <ModalDialog.Header>
+        <ModalDialog.Title>
+          <FormattedMessage {...messages.overwriteModalTitle} />
+        </ModalDialog.Title>
+      </ModalDialog.Header>
+      <ModalDialog.Body>
+        <FormattedMessage {...messages.overwriteConfirmMessage} />
+        <ul className="mt-2">
+          {Object.keys(duplicateFiles).map(file => <li>{file}</li>)}
+        </ul>
+      </ModalDialog.Body>
+      <ModalDialog.Footer>
+        <ActionRow>
+          <ModalDialog.CloseButton variant="tertiary">
+            <FormattedMessage {...messages.cancelOverwriteButtonLabel} />
+          </ModalDialog.CloseButton>
+          <Button onClick={() => handleFileOverwrite(close, duplicateFiles)}>
+            <FormattedMessage {...messages.confirmOverwriteButtonLabel} />
+          </Button>
+        </ActionRow>
+      </ModalDialog.Footer>
+    </ModalDialog>
+  );
+};
+
+FileValidationModal.propTypes = {
+  handleFileOverwrite: PropTypes.func.isRequired,
+  // injected
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(FileValidationModal);
