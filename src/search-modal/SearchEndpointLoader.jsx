0e96c7d2fa1e3bc831a6dc2e1223ebb1128000af
Rômulo Penido <romulo@opencraft.com>
2024-03-29 12:21:19 -0300

    refactor: moving api and adding more tests

--- src/search-modal/SearchEndpointLoader.jsx
+++ src/search-modal/SearchEndpointLoader.jsx
@@ -3,14 +3,12 @@
 import React from 'react';
 import { ModalDialog } from '@openedx/paragon';
 import { ErrorAlert } from '@edx/frontend-lib-content-components';
-import { getConfig } from '@edx/frontend-platform';
-import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { useIntl } from '@edx/frontend-platform/i18n';
-import { useQuery } from '@tanstack/react-query';
 
-import messages from './messages';
 import { LoadingSpinner } from '../generic/Loading';
+import { useContentSearch } from './data/apiHooks';
 import SearchUI from './SearchUI';
+import messages from './messages';
 
 /** @type {React.FC<{courseId: string}>} */
 const SearchEndpointLoader = ({ courseId }) => {
@@ -22,20 +20,7 @@ const SearchEndpointLoader = ({ courseId }) => {
     data: searchEndpointData,
     isLoading,
     error,
-  } = useQuery({
-    queryKey: ['content_search'],
-    queryFn: async () => {
-      const url = new URL('api/content_search/v2/studio/', getConfig().STUDIO_BASE_URL).href;
-      const response = await getAuthenticatedHttpClient().get(url);
-      return {
-        url: response.data.url,
-        indexName: response.data.index_name,
-        apiKey: response.data.api_key,
-      };
-    },
-    staleTime: 60 * 60, // If cache is up to one hour old, no need to re-fetch
-    refetchOnWindowFocus: false, // This doesn't need to be refreshed when the user switches back to this tab.
-  });
+  } = useContentSearch();
 
   const title = intl.formatMessage(messages.title);
 
