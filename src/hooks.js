ce521ea8e0152413b7af46eb5e8e5be84462559f
Rômulo Penido <romulo@opencraft.com>
2024-03-29 00:34:09 -0300

    refactor: remove unused code

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { history } from '@edx/frontend-platform';
 
-// eslint-disable-next-line import/prefer-default-export
 export const useScrollToHashElement = ({ isLoading }) => {
   useEffect(() => {
     const currentHash = window.location.hash;
@@ -31,20 +30,4 @@ export const useEscapeClick = ({ onEscape, dependency }) => {
       window.removeEventListener('keydown', handleEscapeClick);
     };
   }, [dependency]);
-};
-
-export const useKeyHandler = ({ keyName, handler, dependencies = [] }) => {
-  useEffect(() => {
-    const checkKey = (event) => {
-      if (event.key === keyName) {
-        handler();
-      }
-    };
-
-    window.addEventListener('keydown', checkKey);
-
-    return () => {
-      window.removeEventListener('keydown', checkKey);
-    };
-  }, dependencies);
 };
