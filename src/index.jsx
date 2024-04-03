f832b963dc5e1ebe3f1481e73f841a62afe6e094
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-03 09:39:58 -0500

    feat: add `ENABLE_GRADING_METHOD_IN_PROBLEMS` feature flag

--- src/index.jsx
+++ src/index.jsx
@@ -124,6 +124,7 @@ initialize({
         ENABLE_TAGGING_TAXONOMY_PAGES: process.env.ENABLE_TAGGING_TAXONOMY_PAGES || 'false',
         ENABLE_HOME_PAGE_COURSE_API_V2: process.env.ENABLE_HOME_PAGE_COURSE_API_V2 === 'true',
         ENABLE_CHECKLIST_QUALITY: process.env.ENABLE_CHECKLIST_QUALITY || 'true',
+        ENABLE_GRADING_METHOD_IN_PROBLEMS: process.env.ENABLE_GRADING_METHOD_IN_PROBLEMS === 'true',
       }, 'CourseAuthoringConfig');
     },
   },
