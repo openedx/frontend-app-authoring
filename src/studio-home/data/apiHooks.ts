9c6e1e6fc7d4206146993edd042ae846b90be217
Yusuf Musleh <yusuf@opencraft.com>
2024-05-30 18:56:20 +0300

    feat: Add pagination support for lib v2s

--- src/studio-home/data/apiHooks.ts
+++ src/studio-home/data/apiHooks.ts
@@ -2,12 +2,20 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getStudioHomeLibrariesV2 } from './api';
 
+
+interface CustomParams {
+  type?: string,
+  page?: number,
+  pageSize?: number,
+  pagination?: boolean,
+}
+
 /**
  * Builds the query to fetch list of V2 Libraries
  */
-export const useListStudioHomeV2Libraries = () => (
+export const useListStudioHomeV2Libraries = (customParams: CustomParams) => (
   useQuery({
-    queryKey: ['listV2Libraries'],
-    queryFn: () => getStudioHomeLibrariesV2(),
+    queryKey: ['listV2Libraries', customParams],
+    queryFn: () => getStudioHomeLibrariesV2(customParams),
   })
 );
