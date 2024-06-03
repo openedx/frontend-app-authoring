efbc625aaa03a51e0f010ae25a7821b0404b854a
Yusuf Musleh <yusuf@opencraft.com>
2024-06-03 17:03:01 +0300

    temp: This removes TS code to get tests to run
    This commit is temporary as the current frontend build system in tests doesnt support TS syntax. That should be fixed soon, and this commit should be removed.

--- src/studio-home/data/apiHooks.ts
+++ src/studio-home/data/apiHooks.ts
@@ -3,17 +3,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getStudioHomeLibrariesV2 } from './api';
 
 
-interface CustomParams {
-  type?: string,
-  page?: number,
-  pageSize?: number,
-  pagination?: boolean,
-}
-
 /**
  * Builds the query to fetch list of V2 Libraries
  */
-export const useListStudioHomeV2Libraries = (customParams: CustomParams) => (
+export const useListStudioHomeV2Libraries = (customParams) => (
   useQuery({
     queryKey: ['listV2Libraries', customParams],
     queryFn: () => getStudioHomeLibrariesV2(customParams),
