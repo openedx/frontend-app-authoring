94909639865b9b7820d0201e2cd1f03987d622f8
XnpioChV <xnpiochv@gmail.com>
2024-06-19 14:28:39 -0500

    style: Style nits on card component

--- src/library-authoring/data/apiHook.ts
+++ src/library-authoring/data/apiHook.ts
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { MeiliSearch } from 'meilisearch';
 
 import { useContentSearchConnection, useContentSearchResults } from '../../search-modal';
-import { getContentLibrary } from './api';
+import { getContentLibrary, getLibraryBlockTypes } from './api';
 
 /**
  * Hook to fetch a content library by its ID.
@@ -12,6 +12,17 @@ export const useContentLibrary = (libraryId?: string) => (
   useQuery({
     queryKey: ['contentLibrary', libraryId],
     queryFn: () => getContentLibrary(libraryId),
+  })
+);
+
+/**
+ * Hook to fetch a content library by its ID.
+ * @param {string} [libraryId] - The ID of the library to fetch.
+ */
+export const useLibraryBlockTypes = (libraryId) => (
+  useQuery({
+    queryKey: ['contentLibrary', 'libraryBlockTypes', libraryId],
+    queryFn: () => getLibraryBlockTypes(libraryId),
   })
 );
 
