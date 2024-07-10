2ed545d9dcb66f31a930f31baa12b984a7cc6767
Rômulo Penido <romulo.penido@gmail.com>
2024-07-10 15:34:08 +0200

    fix: move block type intl

--- src/search-manager/messages.ts
+++ src/search-manager/messages.ts
@@ -50,6 +50,61 @@ const messages = defineMessages({
     defaultMessage: 'Sorry, not all tags could be loaded',
     description: 'Label shown when the system is not able to display all of the available tag options.',
   },
+  'blockType.annotatable': {
+    id: 'course-authoring.course-search.blockType.annotatable',
+    defaultMessage: 'Annotation',
+    description: 'Name of the "Annotation" component type in Studio',
+  },
+  'blockType.chapter': {
+    id: 'course-authoring.course-search.blockType.chapter',
+    defaultMessage: 'Section',
+    description: 'Name of the "Section" course outline level in Studio',
+  },
+  'blockType.discussion': {
+    id: 'course-authoring.course-search.blockType.discussion',
+    defaultMessage: 'Discussion',
+    description: 'Name of the "Discussion" component type in Studio',
+  },
+  'blockType.drag-and-drop-v2': {
+    id: 'course-authoring.course-search.blockType.drag-and-drop-v2',
+    defaultMessage: 'Drag and Drop',
+    description: 'Name of the "Drag and Drop" component type in Studio',
+  },
+  'blockType.html': {
+    id: 'course-authoring.course-search.blockType.html',
+    defaultMessage: 'Text',
+    description: 'Name of the "Text" component type in Studio',
+  },
+  'blockType.library_content': {
+    id: 'course-authoring.course-search.blockType.library_content',
+    defaultMessage: 'Library Content',
+    description: 'Name of the "Library Content" component type in Studio',
+  },
+  'blockType.openassessment': {
+    id: 'course-authoring.course-search.blockType.openassessment',
+    defaultMessage: 'Open Response Assessment',
+    description: 'Name of the "Open Response Assessment" component type in Studio',
+  },
+  'blockType.problem': {
+    id: 'course-authoring.course-search.blockType.problem',
+    defaultMessage: 'Problem',
+    description: 'Name of the "Problem" component type in Studio',
+  },
+  'blockType.sequential': {
+    id: 'course-authoring.course-search.blockType.sequential',
+    defaultMessage: 'Subsection',
+    description: 'Name of the "Subsection" course outline level in Studio',
+  },
+  'blockType.vertical': {
+    id: 'course-authoring.course-search.blockType.vertical',
+    defaultMessage: 'Unit',
+    description: 'Name of the "Unit" course outline level in Studio',
+  },
+  'blockType.video': {
+    id: 'course-authoring.course-search.blockType.video',
+    defaultMessage: 'Video',
+    description: 'Name of the "Video" component type in Studio',
+  },
   blockTagsFilter: {
     id: 'course-authoring.search-manager.blockTagsFilter',
     defaultMessage: 'Tags',
--- src/search-modal/messages.ts
+++ src/search-modal/messages.ts
@@ -5,61 +5,6 @@ import type { defineMessages as defineMessagesType } from 'react-intl';
 const defineMessages = _defineMessages as typeof defineMessagesType;
 
 const messages = defineMessages({
-  'blockType.annotatable': {
-    id: 'course-authoring.course-search.blockType.annotatable',
-    defaultMessage: 'Annotation',
-    description: 'Name of the "Annotation" component type in Studio',
-  },
-  'blockType.chapter': {
-    id: 'course-authoring.course-search.blockType.chapter',
-    defaultMessage: 'Section',
-    description: 'Name of the "Section" course outline level in Studio',
-  },
-  'blockType.discussion': {
-    id: 'course-authoring.course-search.blockType.discussion',
-    defaultMessage: 'Discussion',
-    description: 'Name of the "Discussion" component type in Studio',
-  },
-  'blockType.drag-and-drop-v2': {
-    id: 'course-authoring.course-search.blockType.drag-and-drop-v2',
-    defaultMessage: 'Drag and Drop',
-    description: 'Name of the "Drag and Drop" component type in Studio',
-  },
-  'blockType.html': {
-    id: 'course-authoring.course-search.blockType.html',
-    defaultMessage: 'Text',
-    description: 'Name of the "Text" component type in Studio',
-  },
-  'blockType.library_content': {
-    id: 'course-authoring.course-search.blockType.library_content',
-    defaultMessage: 'Library Content',
-    description: 'Name of the "Library Content" component type in Studio',
-  },
-  'blockType.openassessment': {
-    id: 'course-authoring.course-search.blockType.openassessment',
-    defaultMessage: 'Open Response Assessment',
-    description: 'Name of the "Open Response Assessment" component type in Studio',
-  },
-  'blockType.problem': {
-    id: 'course-authoring.course-search.blockType.problem',
-    defaultMessage: 'Problem',
-    description: 'Name of the "Problem" component type in Studio',
-  },
-  'blockType.sequential': {
-    id: 'course-authoring.course-search.blockType.sequential',
-    defaultMessage: 'Subsection',
-    description: 'Name of the "Subsection" course outline level in Studio',
-  },
-  'blockType.vertical': {
-    id: 'course-authoring.course-search.blockType.vertical',
-    defaultMessage: 'Unit',
-    description: 'Name of the "Unit" course outline level in Studio',
-  },
-  'blockType.video': {
-    id: 'course-authoring.course-search.blockType.video',
-    defaultMessage: 'Video',
-    description: 'Name of the "Video" component type in Studio',
-  },
   searchAllCourses: {
     id: 'course-authoring.course-search.searchAllCourses',
     defaultMessage: 'All courses',
