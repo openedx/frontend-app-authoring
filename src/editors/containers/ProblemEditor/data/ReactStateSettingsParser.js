26c919a070f5cb7a25396b1b0be23e86f133cdcd
Dima Alipov <dimaalipov@192.168.1.101>
2024-09-11 14:27:06 +0300

    fix: default setting not updating with updated default settings

--- src/editors/containers/ProblemEditor/data/ReactStateSettingsParser.js
+++ src/editors/containers/ProblemEditor/data/ReactStateSettingsParser.js
@@ -33,13 +33,13 @@ class ReactStateSettingsParser {
 
     settings = popuplateItem(settings, 'number', 'max_attempts', stateSettings.scoring.attempts, defaultSettings?.maxAttempts, true);
     settings = popuplateItem(settings, 'weight', 'weight', stateSettings.scoring);
-    settings = popuplateItem(settings, 'on', 'showanswer', stateSettings.showAnswer, defaultSettings?.showanswer);
+    settings = popuplateItem(settings, 'on', 'showanswer', stateSettings.showAnswer, defaultSettings?.showanswer, true);
     if (_.includes(numberOfAttemptsChoice, stateSettings.showAnswer.on)) {
       settings = popuplateItem(settings, 'afterAttempts', 'attempts_before_showanswer_button', stateSettings.showAnswer);
     }
-    settings = popuplateItem(settings, 'showResetButton', 'show_reset_button', stateSettings, defaultSettings?.showResetButton);
+    settings = popuplateItem(settings, 'showResetButton', 'show_reset_button', stateSettings, defaultSettings?.showResetButton, true);
     settings = popuplateItem(settings, 'timeBetween', 'submission_wait_seconds', stateSettings);
-    settings = popuplateItem(settings, 'randomization', 'rerandomize', stateSettings, defaultSettings?.rerandomize);
+    settings = popuplateItem(settings, 'randomization', 'rerandomize', stateSettings, defaultSettings?.rerandomize, true);
 
     return settings;
   }
