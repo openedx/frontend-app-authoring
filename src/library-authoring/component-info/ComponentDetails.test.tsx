f0d6e882634afca199cc4cd5a0d1c109d428fcae
Braden MacDonald <braden@opencraft.com>
2024-10-18 21:50:59 -0700

    fix: expand and internationalize the "component usage" text

--- src/library-authoring/component-info/ComponentDetails.test.tsx
+++ src/library-authoring/component-info/ComponentDetails.test.tsx
@@ -46,7 +46,7 @@ describe('<ComponentDetails />', () => {
     render(mockLibraryBlockMetadata.usageKeyNeverPublished);
     expect(await screen.findByText('Component Usage')).toBeInTheDocument();
     // TODO: replace with actual data when implement course list
-    expect(screen.queryByText('This will show the courses that use this component.')).toBeInTheDocument();
+    expect(screen.queryByText(/This will show the courses that use this component./)).toBeInTheDocument();
   });
 
   it('should render the component history', async () => {
