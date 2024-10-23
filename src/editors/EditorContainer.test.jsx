aafc5ff22bebc878c7f8aed0c00ba9301bc7545a
Navin Karkera <navin@opencraft.com>
2024-10-23 12:25:25 +0530

    test: fix editor container test

--- src/editors/EditorContainer.test.jsx
+++ src/editors/EditorContainer.test.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { shallow } from '@edx/react-unit-test-utils';
 import EditorContainer from './EditorContainer';
 
-jest.mock('react-router', () => ({
-  ...jest.requireActual('react-router'), // use actual for all non-hook parts
+const mockPathname = '/editor/';
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
   useParams: () => ({
     blockId: 'company-id1',
     blockType: 'html',
   }),
-  useLocation: () => {},
+  useLocation: () => ({
+    pathname: mockPathname,
+  }),
+  useSearchParams: () => [{
+    get: () => 'lb:Axim:TEST:html:571fe018-f3ce-45c9-8f53-5dafcb422fdd',
+  }],
+}));
+
+jest.mock('@edx/frontend-platform/i18n', () => ({
+  ...jest.requireActual('@edx/frontend-platform/i18n'),
+  useIntl: () => ({
+    formatMessage: (message) => message.defaultMessage,
+  }),
 }));
 
 const props = { learningContextId: 'cOuRsEId' };
