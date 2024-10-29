0819a60265852645a279b1c81635692c8ddc7225
Navin Karkera <navin@opencraft.com>
2024-10-29 15:59:43 +0530

    test: fix tests

--- src/generic/promptIfDirty/usePromptIfDirty.test.jsx
+++ src/generic/promptIfDirty/usePromptIfDirty.test.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import PromptIfDirty from './usePromptIfDirty';
+import { renderHook } from '@testing-library/react-hooks';
+import usePromptIfDirty from './usePromptIfDirty';
 
-describe('PromptIfDirty', () => {
-  let container = null;
+describe('usePromptIfDirty', () => {
   let mockEvent = null;
 
   beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
     mockEvent = new Event('beforeunload');
     jest.spyOn(window, 'addEventListener');
     jest.spyOn(window, 'removeEventListener');
     jest.spyOn(mockEvent, 'preventDefault');
     Object.defineProperty(mockEvent, 'returnValue', { writable: true });
-    mockEvent.returnValue = '';
   });
 
   afterEach(() => {
@@ -23,49 +17,32 @@ describe('PromptIfDirty', () => {
     window.removeEventListener.mockRestore();
     mockEvent.preventDefault.mockRestore();
     mockEvent = null;
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
   });
 
   it('should add event listener on mount', () => {
-    act(() => {
-      render(<PromptIfDirty dirty />, container);
-    });
+    renderHook(() => usePromptIfDirty(() => true));
 
     expect(window.addEventListener).toHaveBeenCalledWith('beforeunload', expect.any(Function));
   });
 
   it('should remove event listener on unmount', () => {
-    act(() => {
-      render(<PromptIfDirty dirty />, container);
-    });
-    act(() => {
-      unmountComponentAtNode(container);
-    });
+    const { unmount } = renderHook(() => usePromptIfDirty(() => true));
+    unmount();
 
     expect(window.removeEventListener).toHaveBeenCalledWith('beforeunload', expect.any(Function));
   });
 
   it('should call preventDefault and set returnValue when dirty is true', () => {
-    act(() => {
-      render(<PromptIfDirty dirty />, container);
-    });
-    act(() => {
-      window.dispatchEvent(mockEvent);
-    });
+    renderHook(() => usePromptIfDirty(() => true));
+    window.dispatchEvent(mockEvent);
 
     expect(mockEvent.preventDefault).toHaveBeenCalled();
-    expect(mockEvent.returnValue).toBe('');
+    expect(mockEvent.returnValue).toBe(true);
   });
 
   it('should not call preventDefault when dirty is false', () => {
-    act(() => {
-      render(<PromptIfDirty dirty={false} />, container);
-    });
-    act(() => {
-      window.dispatchEvent(mockEvent);
-    });
+    renderHook(() => usePromptIfDirty(() => false));
+    window.dispatchEvent(mockEvent);
 
     expect(mockEvent.preventDefault).not.toHaveBeenCalled();
   });
