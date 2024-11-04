acbc4285443a5b6efe197176048f0e0f71dae96a
Rômulo Penido <romulo.penido@gmail.com>
2024-11-04 09:28:15 -0300

    fix: add spacing to searchbar and simplify render conditions

--- src/library-authoring/component-picker/SelectLibrary.tsx
+++ src/library-authoring/component-picker/SelectLibrary.tsx
@@ -83,44 +83,44 @@ const SelectLibrary = ({ selectedLibrary, setSelectedLibrary }: SelectLibraryPro
         value={searchQuery}
         placeholder={intl.formatMessage(messages.selectLibrarySearchPlaceholder)}
       />
-      <div>
-        {data.results.length === 0 && (<EmptyState hasSearchQuery={!!searchQuery} />)}
-        <Form.RadioSet
-          name="selected-library"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSelectedLibrary(e.target.value)}
-          value={selectedLibrary}
-        >
-          {data.results.map((library) => (
-            <Card
-              key={library.id}
-              isClickable
-              onClick={() => setSelectedLibrary(library.id)}
-              className="card-item"
-            >
-              <Card.Header
-                size="sm"
-                title={<span className="card-item-title">{library.title}</span>}
-                subtitle={`${library.org} / ${library.slug}`}
-                actions={(
-                  <Form.Radio value={library.id} name={`select-library-${library.id}`}>{' '}</Form.Radio>
-                )}
-              />
-              <Card.Body>
-                <p>{library.description}</p>
-              </Card.Body>
-            </Card>
-          ))}
-        </Form.RadioSet>
-      </div>
-      {data.results.length !== 0 && (
-        <Pagination
-          paginationLabel={intl.formatMessage(messages.selectLibraryPaginationLabel)}
-          pageCount={data!.numPages}
-          currentPage={data!.currentPage}
-          onPageSelect={(page: number) => setCurrentPage(page)}
-          variant="secondary"
-          className="align-self-center"
-        />
+      {data.results.length === 0 ? (<EmptyState hasSearchQuery={!!searchQuery} />) : (
+        <>
+          <Form.RadioSet
+            name="selected-library"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSelectedLibrary(e.target.value)}
+            value={selectedLibrary}
+            className="mt-4"
+          >
+            {data.results.map((library) => (
+              <Card
+                key={library.id}
+                isClickable
+                onClick={() => setSelectedLibrary(library.id)}
+                className="card-item"
+              >
+                <Card.Header
+                  size="sm"
+                  title={<span className="card-item-title">{library.title}</span>}
+                  subtitle={`${library.org} / ${library.slug}`}
+                  actions={(
+                    <Form.Radio value={library.id} name={`select-library-${library.id}`}>{' '}</Form.Radio>
+                  )}
+                />
+                <Card.Body>
+                  <p>{library.description}</p>
+                </Card.Body>
+              </Card>
+            ))}
+          </Form.RadioSet>
+          <Pagination
+            paginationLabel={intl.formatMessage(messages.selectLibraryPaginationLabel)}
+            pageCount={data!.numPages}
+            currentPage={data!.currentPage}
+            onPageSelect={setCurrentPage}
+            variant="secondary"
+            className="align-self-center"
+          />
+        </>
       )}
     </Stack>
   );
