a87b2d4509f2ac50f0761b041de97fa69444c6bb
Navin Karkera <navin@opencraft.com>
2025-02-24 20:01:02 +0530

    refactor: libraries tab in course libraries page

--- src/course-libraries/data/apiHooks.ts
+++ src/course-libraries/data/apiHooks.ts
@@ -1,7 +1,7 @@
 import {
   useQuery,
 } from '@tanstack/react-query';
-import { getEntityLinksByDownstreamContext } from './api';
+import { getEntityLinksByDownstreamContext, getEntityLinksSummaryByDownstreamContext } from './api';
 
 export const courseLibrariesQueryKeys = {
   all: ['courseLibraries'],
@@ -11,15 +11,29 @@ export const courseLibrariesQueryKeys = {
   courseReadyToSyncLibraries: (courseKey?: string, readyToSync?: boolean) => {
     return [...courseLibrariesQueryKeys.courseLibraries(courseKey), readyToSync]
   },
+  courseLibrariesSummary: (courseKey?: string) => {
+    return [...courseLibrariesQueryKeys.courseLibraries(courseKey), "summary"]
+  },
 };
 
 /**
- * Hook to fetch a content library by its ID.
+ * Hook to fetch publishable entity links by course key.
  */
 export const useEntityLinksByDownstreamContext = (courseKey?: string, readyToSync?: boolean) => (
   useQuery({
     queryKey: courseLibrariesQueryKeys.courseReadyToSyncLibraries(courseKey, readyToSync),
     queryFn: () => getEntityLinksByDownstreamContext(courseKey!, readyToSync),
+    enabled: courseKey !== undefined,
+  })
+);
+
+/**
+ * Hook to fetch publishable entity links summary by course key.
+ */
+export const useEntityLinksSummaryByDownstreamContext = (courseKey?: string) => (
+  useQuery({
+    queryKey: courseLibrariesQueryKeys.courseLibrariesSummary(courseKey),
+    queryFn: () => getEntityLinksSummaryByDownstreamContext(courseKey!),
     enabled: courseKey !== undefined,
   })
 );
