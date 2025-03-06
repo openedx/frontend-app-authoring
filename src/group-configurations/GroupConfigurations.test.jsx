e5278cba51dc91d3f752f6512da0e223e9e04b3d
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-03-06 13:30:46 -0500

    feat: add team groups section in group configurations

--- src/group-configurations/GroupConfigurations.test.jsx
+++ src/group-configurations/GroupConfigurations.test.jsx
@@ -21,6 +21,7 @@ let store;
 const courseId = 'course-v1:org+101+101';
 const enrollmentTrackGroups = groupConfigurationResponseMock.allGroupConfigurations[0];
 const contentGroups = groupConfigurationResponseMock.allGroupConfigurations[1];
+const teamGroups = groupConfigurationResponseMock.allGroupConfigurations[2];
 
 const renderComponent = () => render(
   <AppProvider store={store}>
@@ -72,6 +73,7 @@ describe('<GroupConfigurations />', () => {
       ).toBeInTheDocument();
       expect(getByText(contentGroups.name)).toBeInTheDocument();
       expect(getByText(enrollmentTrackGroups.name)).toBeInTheDocument();
+      expect(getByText(teamGroups.name)).toBeInTheDocument();
     });
   });
 
