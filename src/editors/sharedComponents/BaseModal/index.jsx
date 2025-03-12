8e4a9c27f755298ebd7edf45cecd3b54409c5670
XnpioChV <xnpiochv@gmail.com>
2025-03-12 13:48:45 -0500

    refactor: Build new action buttons for cancel confirmation modal

--- src/editors/sharedComponents/BaseModal/index.jsx
+++ src/editors/sharedComponents/BaseModal/index.jsx
@@ -22,6 +22,7 @@ const BaseModal = ({
   isFullscreenScroll,
   bodyStyle,
   className,
+  hideCancelButton,
 }) => (
   <ModalDialog
     isOpen={isOpen}
@@ -49,9 +50,11 @@ const BaseModal = ({
       <ActionRow>
         {footerAction}
         <ActionRow.Spacer />
-        <ModalDialog.CloseButton variant="tertiary" onClick={close}>
-          <FormattedMessage {...messages.cancelButtonLabel} />
-        </ModalDialog.CloseButton>
+        {!hideCancelButton && (
+          <ModalDialog.CloseButton variant="tertiary" onClick={close}>
+            <FormattedMessage {...messages.cancelButtonLabel} />
+          </ModalDialog.CloseButton>
+        )}
         {confirmAction}
       </ActionRow>
     </ModalDialog.Footer>
@@ -65,6 +68,7 @@ BaseModal.defaultProps = {
   isFullscreenScroll: true,
   bodyStyle: null,
   className: undefined,
+  hideCancelButton: false,
 };
 
 BaseModal.propTypes = {
@@ -79,6 +83,7 @@ BaseModal.propTypes = {
   isFullscreenScroll: PropTypes.bool,
   bodyStyle: PropTypes.shape({}),
   className: PropTypes.string,
+  hideCancelButton: PropTypes.bool,
 };
 
 export default BaseModal;
