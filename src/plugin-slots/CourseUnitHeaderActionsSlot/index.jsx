aed02752dfa7551672fb8435fd92426644d10319
Arunmozhi <arunmozhi@opencraft.com>
2025-03-17 20:19:09 +1100

    refactor: replace CourseUnitAnalyticsSlot with CourseUnitHeaderActionsSlot

---
+++ src/plugin-slots/CourseUnitHeaderActionsSlot/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { PluginSlot } from '@openedx/frontend-plugin-framework';
+
+import HeaderNavigations from 'CourseAuthoring/course-unit/header-navigations/HeaderNavigations';
+import { COURSE_BLOCK_NAMES } from 'CourseAuthoring/constants';
+
+const CourseUnitHeaderActionsSlot = ({
+  headerNavigationsActions, unitCategory, unitTitle, verticalBlocks,
+}) => {
+  const isUnitVerticalType = unitCategory === COURSE_BLOCK_NAMES.vertical.id;
+  return (
+    <PluginSlot id="course_unit_header_actions_slot" pluginProps={{ unitTitle, isUnitVerticalType, verticalBlocks }}>
+      <HeaderNavigations headerNavigationsActions={headerNavigationsActions} unitCategory={unitCategory} />
+    </PluginSlot>
+  );
+};
+
+CourseUnitHeaderActionsSlot.propTypes = {
+  headerNavigationsActions: PropTypes.shape({
+    handleViewLive: PropTypes.func.isRequired,
+    handlePreview: PropTypes.func.isRequired,
+    handleEdit: PropTypes.func.isRequired,
+  }).isRequired,
+  unitCategory: PropTypes.string.isRequired,
+  unitTitle: PropTypes.string.isRequired,
+  verticalBlocks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      blockId: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      blockType: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default CourseUnitHeaderActionsSlot;
--- src/plugin-slots/README.md
+++ src/plugin-slots/README.md
@@ -1,4 +1,4 @@
 # `frontend-app-authoring` Plugin Slots
 
 * [`course_outline_header_actions_slot`](./CourseOutlineHeaderActionsSlot/)
-* [`course_unit_analytics_slot`](./CourseUnitAnalyticsSlot/)
+* [`course_unit_header_actions_slot`](./CourseUnitHeaderActionsSlot/)
