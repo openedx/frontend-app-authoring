001146908568c17615bade1fb0a50f3f75669d14
Navin Karkera <navin@opencraft.com>
2025-03-20 10:12:49 +0530

    refactor: don't dismiss out of sync alert on review
    Clicking review button on out of sync alert should not dismiss the alert
    and it should be displayed again in outline or all tab.

--- src/course-libraries/OutOfSyncAlert.tsx
+++ src/course-libraries/OutOfSyncAlert.tsx
@@ -13,9 +13,17 @@ interface OutOfSyncAlertProps {
   onDismiss?: () => void;
   onReview: () => void;
 }
-/*
-Shows an alert when library components used in the current course were updated and the blocks in course can be updated.
-Dismiss or review action is persisted using localStorage to avoid displaying the alert on every refresh.
+/**
+* Shows an alert when library components used in the current course were updated and the blocks
+* in course can be updated. Following are the conditions for displaying the alert.
+*
+* * The alert is displayed if components are out of sync.
+* * If the user clicks on dismiss button, the state is stored in localstorage of user
+*   in this format: outOfSyncCountAlert-${courseId} = <number of out of sync components>.
+* * If the number of sync components don't change for the course and the user opens outline
+*   in the same browser, they don't see the alert again.
+* * If the number changes, i.e., if a new component is out of sync or the user updates or ignores
+*   a component, the alert is displayed again.
 */
 export const OutOfSyncAlert: React.FC<OutOfSyncAlertProps> = ({
   showAlert,
@@ -48,11 +56,6 @@ export const OutOfSyncAlert: React.FC<OutOfSyncAlertProps> = ({
     onDismiss?.();
   };
 
-  const reviewAlert = () => {
-    dismissAlert();
-    onReview();
-  };
-
   return (
     <AlertMessage
       title={intl.formatMessage(messages.outOfSyncCountAlertTitle, { outOfSyncCount })}
@@ -63,7 +66,7 @@ export const OutOfSyncAlert: React.FC<OutOfSyncAlertProps> = ({
       onClose={dismissAlert}
       actions={[
         <Button
-          onClick={reviewAlert}
+          onClick={onReview}
         >
           {intl.formatMessage(messages.outOfSyncCountAlertReviewBtn)}
         </Button>,
