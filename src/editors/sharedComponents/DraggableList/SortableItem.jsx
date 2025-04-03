b5894013ec4b8a7c4e37da4a59b6fe2b8d9d2fda
Navin Karkera <navin@opencraft.com>
2025-04-03 21:03:40 +0530

    feat: component draggable cards

--- src/editors/sharedComponents/DraggableList/SortableItem.jsx
+++ src/editors/sharedComponents/DraggableList/SortableItem.jsx
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 import { intlShape, injectIntl } from '@edx/frontend-platform/i18n';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { Icon, IconButtonWithTooltip, Row } from '@openedx/paragon';
+import { ActionRow, Icon, IconButtonWithTooltip, Stack } from '@openedx/paragon';
 import { DragIndicator } from '@openedx/paragon/icons';
 import messages from './messages';
 
 const SortableItem = ({
   id,
   componentStyle,
+  actions,
+  actionStyle,
   children,
   // injected
   intl,
@@ -29,32 +31,39 @@ const SortableItem = ({
   };
 
   return (
-    <Row
+    <Stack
       ref={setNodeRef}
       style={style}
       className="mx-0"
     >
+      <ActionRow style={actionStyle}>
+        {actions}
+        <IconButtonWithTooltip
+          key="drag-to-reorder-icon"
+          tooltipPlacement="top"
+          tooltipContent={intl.formatMessage(messages.tooltipContent)}
+          src={DragIndicator}
+          iconAs={Icon}
+          variant="secondary"
+          alt={intl.formatMessage(messages.tooltipContent)}
+          {...attributes}
+          {...listeners}
+        />
+      </ActionRow>
       {children}
-      <IconButtonWithTooltip
-        key="drag-to-reorder-icon"
-        tooltipPlacement="top"
-        tooltipContent={intl.formatMessage(messages.tooltipContent)}
-        src={DragIndicator}
-        iconAs={Icon}
-        variant="secondary"
-        alt={intl.formatMessage(messages.tooltipContent)}
-        {...attributes}
-        {...listeners}
-      />
-    </Row>
+    </Stack>
   );
 };
 SortableItem.defaultProps = {
   componentStyle: null,
+  actions: null,
+  actionStyle: null,
 };
 SortableItem.propTypes = {
   id: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  actions: PropTypes.node,
+  actionStyle: PropTypes.shape({}),
   componentStyle: PropTypes.shape({}),
   // injected
   intl: intlShape.isRequired,
--- src/library-authoring/units/LibraryUnitPage.tsx
+++ src/library-authoring/units/LibraryUnitPage.tsx
@@ -1,13 +1,15 @@
 import { useIntl } from "@edx/frontend-platform/i18n";
-import { Breadcrumb, Button, Card, Container } from "@openedx/paragon";
+import { ActionRow, Breadcrumb, Button, Card, Container, Icon, Stack } from "@openedx/paragon";
 import { Add, InfoOutline } from "@openedx/paragon/icons";
 import classNames from "classnames";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Helmet } from 'react-helmet';
 import { Link } from "react-router-dom";
 import { blockTypes } from "../../editors/data/constants/app";
+import DraggableList, { SortableItem } from "../../editors/sharedComponents/DraggableList";
 
 import ErrorAlert from '../../generic/alert-error';
+import { getItemIcon } from "../../generic/block-type-utils";
 import { IframeProvider } from "../../generic/hooks/context/iFrameContext";
 import Loading from "../../generic/Loading";
 import NotFoundAlert from "../../generic/NotFoundAlert";
@@ -15,6 +17,8 @@ import SubHeader from "../../generic/sub-header/SubHeader";
 import Header from "../../header";
 import { useLibraryContext } from "../common/context/LibraryContext";
 import { useSidebarContext } from "../common/context/SidebarContext";
+import ComponentMenu from "../components";
+import { LibraryBlockMetadata } from "../data/api";
 import { useContainer, useContainerChildren, useContentLibrary } from "../data/apiHooks";
 import { LibrarySidebar } from "../library-sidebar";
 import { SubHeaderTitle } from "../LibraryAuthoringPage";
@@ -45,6 +49,7 @@ const HeaderActions = () => {
 };
 
 const UnitBlocks = () => {
+  const [orderedBlocks, setOrderedBlocks] = useState<LibraryBlockMetadata[]>([]);
   const {
     libraryId,
     unitId,
@@ -58,6 +63,8 @@ const UnitBlocks = () => {
     error,
   } = useContainerChildren(libraryId, unitId);
 
+  useEffect(() => setOrderedBlocks(blocks || []), [blocks])
+
   if (isLoading) {
     return <Loading />;
   }
@@ -65,11 +72,39 @@ const UnitBlocks = () => {
   if (isError) {
     return <ErrorAlert error={error} />;
   }
+  const handleReorder = () => (newOrder) => {
+    // TODO: update order of components in unit
+  };
 
-  const renderedBlocks = blocks?.map((block) => (
+  const renderedBlocks = orderedBlocks?.map((block) => (
     <IframeProvider key={block.id}>
-      <Card className="my-3 p-3">
-        <div className={classNames({
+      <SortableItem
+        id={block.id}
+        componentStyle={{
+          background: 'white',
+          borderRadius: '8px',
+          padding: '0px',
+          marginBottom: '1rem',
+          border: 'solid 1px #E1DDDB'
+        }}
+        actions={
+          <>
+            <Stack direction="horizontal" gap={2} className="font-weight-bold">
+              <Icon src={getItemIcon(block.blockType)} />
+              {block.displayName}
+            </Stack>
+            <ActionRow.Spacer />
+            <ComponentMenu usageKey={block.id} />
+          </>
+        }
+        actionStyle={{
+          borderRadius: '8px 8px 0px 0px',
+          padding: '0.5rem 1rem',
+          backgroundColor: '#FBFAF9',
+          borderBottom: 'solid 1px #E1DDDB'
+        }}
+      >
+        <div className={classNames('p-3', {
           "container-mw-md": block.blockType === blockTypes.video,
         })}>
           <LibraryBlock
@@ -77,10 +112,14 @@ const UnitBlocks = () => {
             version={showOnlyPublished ? 'published' : undefined}
           />
         </div>
-      </Card>
+      </SortableItem>
     </IframeProvider>
   ));
-  return (<>{renderedBlocks}</>);
+  return (
+    <DraggableList itemList={orderedBlocks} setState={setOrderedBlocks} updateOrder={handleReorder}>
+      {renderedBlocks}
+    </DraggableList>
+  );
 }
 
 export const LibraryUnitPage = () => {
