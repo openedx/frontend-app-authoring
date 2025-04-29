fa2eea56fe0359a6e42fa02d8e8c27f97bffd4d5
Jacobo Dominguez <jacobo.dominguez@wgu.edu>
2025-04-29 15:34:00 -0600

    fix: adding messages for i18n issues related to placeholders

--- src/editors/containers/EditorContainer/components/TitleHeader/EditableHeader.jsx
+++ src/editors/containers/EditorContainer/components/TitleHeader/EditableHeader.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Form } from '@openedx/paragon';
+import { useIntl } from '@edx/frontend-platform/i18n';
 import EditConfirmationButtons from './EditConfirmationButtons';
+
+import messages from './messages';
 
 const EditableHeader = ({
   handleChange,
@@ -11,20 +14,23 @@ const EditableHeader = ({
   inputRef,
   localTitle,
   cancelEdit,
-}) => (
-  <Form.Group onBlur={(e) => updateTitle(e)}>
-    <Form.Control
-      style={{ paddingInlineEnd: 'calc(1rem + 84px)' }}
-      autoFocus
-      trailingElement={<EditConfirmationButtons {...{ updateTitle, cancelEdit }} />}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-      placeholder="Title"
-      ref={inputRef}
-      value={localTitle}
-    />
-  </Form.Group>
-);
+}) => {
+  const intl = useIntl();
+  return (
+    <Form.Group onBlur={(e) => updateTitle(e)}>
+      <Form.Control
+        style={{ paddingInlineEnd: 'calc(1rem + 84px)' }}
+        autoFocus
+        trailingElement={<EditConfirmationButtons {...{ updateTitle, cancelEdit }} />}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        placeholder={intl.formatMessage(messages.editTitlePlaceholder)}
+        ref={inputRef}
+        value={localTitle}
+      />
+    </Form.Group>
+  );
+};
 EditableHeader.defaultProps = {
   inputRef: null,
 };
