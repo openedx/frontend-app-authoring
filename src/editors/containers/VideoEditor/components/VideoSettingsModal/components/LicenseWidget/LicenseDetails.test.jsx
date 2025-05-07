133c23c9badd66b02733f1f51451932d449ee1e7
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-05-07 13:16:15 -0600

    "revert: fixing failing snapshot test"
    This reverts commit 81794768cf4eeb6920a48a201605c0fe6aa550e5.

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/LicenseWidget/LicenseDetails.test.jsx
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/LicenseWidget/LicenseDetails.test.jsx
@@ -52,11 +52,8 @@ describe('LicenseDetails', () => {
       ).toMatchSnapshot();
     });
     test('snapshots: renders as expected with level set to block and license set to Creative Commons', () => {
-      const mockIntl = {
-        formatMessage: jest.fn(({ defaultMessage }) => defaultMessage),
-      };
       expect(
-        shallow(<LicenseDetails {...props} level="block" license="creative-commons" intl={mockIntl} />).snapshot,
+        shallow(<LicenseDetails {...props} level="block" license="creative-commons" />).snapshot,
       ).toMatchSnapshot();
     });
   });
