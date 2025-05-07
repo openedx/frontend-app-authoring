c933db2c2d01471c4242adb9c0310b4ab48c35e5
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-05-07 13:12:56 -0600

    revert: rewording commit messages
    This reverts commit e9ef789a2bd6e87d44703297a0c10d43d5440643.

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/LicenseWidget/LicenseDetails.jsx
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/LicenseWidget/LicenseDetails.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import {
   FormattedMessage,
   injectIntl,
-  intlShape,
 } from '@edx/frontend-platform/i18n';
 import {
   ActionRow,
@@ -30,7 +29,6 @@ const LicenseDetails = ({
   level,
   // redux
   updateField,
-  intl,
 }) => (
   level !== LicenseLevel.course && details && license !== 'select' ? (
     <div className="x-small border-primary-100 border-bottom m-0 pr-1">
@@ -38,11 +36,11 @@ const LicenseDetails = ({
         <div className="mb-3">
           <FormattedMessage {...messages.detailsSubsectionTitle} />
         </div>
-        {license === LicenseTypes.allRightsReserved && (
+        {license === LicenseTypes.allRightsReserved ? (
           <div className="mt-2">
             <FormattedMessage {...messages.allRightsReservedSectionMessage} />
           </div>
-        )}
+        ) : null}
         {license === LicenseTypes.creativeCommons
           ? (
             <Stack gap={4}>
@@ -57,7 +55,7 @@ const LicenseDetails = ({
                     <CheckboxControl
                       disabled
                       checked
-                      aria-label={intl.formatMessage(messages.attributionCheckboxAriaLabel)}
+                      aria-label="Checkbox"
                     />
                   </ActionRow>
                 </Form.Group>
@@ -82,7 +80,7 @@ const LicenseDetails = ({
                           noncommercial: e.target.checked,
                         },
                       })}
-                      aria-label={intl.formatMessage(messages.noncommercialCheckboxAriaLabel)}
+                      aria-label="Checkbox"
                     />
                   </ActionRow>
                 </Form.Group>
@@ -108,7 +106,7 @@ const LicenseDetails = ({
                           shareAlike: e.target.checked ? false : details.shareAlike,
                         },
                       })}
-                      aria-label={intl.formatMessage(messages.noDerivativesCheckboxAriaLabel)}
+                      aria-label="Checkbox"
                     />
                   </ActionRow>
                 </Form.Group>
@@ -134,7 +132,7 @@ const LicenseDetails = ({
                           noDerivatives: e.target.checked ? false : details.noDerivatives,
                         },
                       })}
-                      aria-label={intl.formatMessage(messages.shareAlikeCheckboxAriaLabel)}
+                      aria-label="Checkbox"
                     />
                   </ActionRow>
                 </Form.Group>
@@ -160,7 +158,6 @@ LicenseDetails.propTypes = {
   level: PropTypes.string.isRequired,
   // redux
   updateField: PropTypes.func.isRequired,
-  intl: intlShape.isRequired,
 };
 
 export const mapStateToProps = () => ({});
