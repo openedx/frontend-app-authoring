d06cbc1ae544893dd90e3eeeca949d0efc6f0414
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-05-07 08:55:50 -0600

    refactor: injecting intl object to prevent extra modifications on license details component

--- src/editors/containers/VideoEditor/components/VideoSettingsModal/components/LicenseWidget/LicenseDetails.jsx
+++ src/editors/containers/VideoEditor/components/VideoSettingsModal/components/LicenseWidget/LicenseDetails.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import {
   FormattedMessage,
   injectIntl,
-  useIntl,
+  intlShape,
 } from '@edx/frontend-platform/i18n';
 import {
   ActionRow,
@@ -30,126 +30,124 @@ const LicenseDetails = ({
   level,
   // redux
   updateField,
-}) => {
-  const intl = useIntl();
-  return (
-    level !== LicenseLevel.course && details && license !== 'select' ? (
-      <div className="x-small border-primary-100 border-bottom m-0 pr-1">
-        <Form.Group className="pb-2">
-          <div className="mb-3">
-            <FormattedMessage {...messages.detailsSubsectionTitle} />
+  intl,
+}) => (
+  level !== LicenseLevel.course && details && license !== 'select' ? (
+    <div className="x-small border-primary-100 border-bottom m-0 pr-1">
+      <Form.Group className="pb-2">
+        <div className="mb-3">
+          <FormattedMessage {...messages.detailsSubsectionTitle} />
+        </div>
+        {license === LicenseTypes.allRightsReserved && (
+          <div className="mt-2">
+            <FormattedMessage {...messages.allRightsReservedSectionMessage} />
           </div>
-          {license === LicenseTypes.allRightsReserved && (
-            <div className="mt-2">
-              <FormattedMessage {...messages.allRightsReservedSectionMessage} />
-            </div>
-          )}
-          {license === LicenseTypes.creativeCommons
-            ? (
-              <Stack gap={4}>
-                <div className="border-primary-100 border-bottom pb-4">
-                  <Form.Group>
-                    <ActionRow>
-                      <Icon className="text-primary-500" src={Attribution} />
-                      <Form.Label className="my-0 text-primary-500">
-                        <FormattedMessage {...messages.attributionCheckboxLabel} />
-                      </Form.Label>
-                      <ActionRow.Spacer />
-                      <CheckboxControl
-                        disabled
-                        checked
-                        aria-label={intl.formatMessage(messages.attributionCheckboxAriaLabel)}
-                      />
-                    </ActionRow>
-                  </Form.Group>
-                  <div>
-                    <FormattedMessage {...messages.attributionSectionDescription} />
-                  </div>
-                </div>
-                <div className="border-primary-100 border-bottom pb-4">
-                  <Form.Group>
-                    <ActionRow>
-                      <Icon src={Nc} />
-                      <Form.Label className="my-0 text-primary-500">
-                        <FormattedMessage {...messages.noncommercialCheckboxLabel} />
-                      </Form.Label>
-                      <ActionRow.Spacer />
-                      <CheckboxControl
-                        checked={details.noncommercial}
-                        disabled={level === LicenseLevel.course}
-                        onChange={(e) => updateField({
-                          licenseDetails: {
-                            ...details,
-                            noncommercial: e.target.checked,
-                          },
-                        })}
-                        aria-label={intl.formatMessage(messages.noncommercialCheckboxAriaLabel)}
-                      />
-                    </ActionRow>
-                  </Form.Group>
-                  <div>
-                    <FormattedMessage {...messages.noncommercialSectionDescription} />
-                  </div>
-                </div>
-                <div className="border-primary-100 border-bottom pb-4">
-                  <Form.Group>
-                    <ActionRow>
-                      <Icon src={Nd} />
-                      <Form.Label className="my-0 text-primary-500">
-                        <FormattedMessage {...messages.noDerivativesCheckboxLabel} />
-                      </Form.Label>
-                      <ActionRow.Spacer />
-                      <CheckboxControl
-                        checked={details.noDerivatives}
-                        disabled={level === LicenseLevel.course}
-                        onChange={(e) => updateField({
-                          licenseDetails: {
-                            ...details,
-                            noDerivatives: e.target.checked,
-                            shareAlike: e.target.checked ? false : details.shareAlike,
-                          },
-                        })}
-                        aria-label={intl.formatMessage(messages.noDerivativesCheckboxAriaLabel)}
-                      />
-                    </ActionRow>
-                  </Form.Group>
-                  <div>
-                    <FormattedMessage {...messages.noDerivativesSectionDescription} />
-                  </div>
-                </div>
+        )}
+        {license === LicenseTypes.creativeCommons
+          ? (
+            <Stack gap={4}>
+              <div className="border-primary-100 border-bottom pb-4">
+                <Form.Group>
+                  <ActionRow>
+                    <Icon className="text-primary-500" src={Attribution} />
+                    <Form.Label className="my-0 text-primary-500">
+                      <FormattedMessage {...messages.attributionCheckboxLabel} />
+                    </Form.Label>
+                    <ActionRow.Spacer />
+                    <CheckboxControl
+                      disabled
+                      checked
+                      aria-label={intl.formatMessage(messages.attributionCheckboxAriaLabel)}
+                    />
+                  </ActionRow>
+                </Form.Group>
                 <div>
-                  <Form.Group>
-                    <ActionRow>
-                      <Icon src={Sa} />
-                      <Form.Label className="my-0 text-primary-500">
-                        <FormattedMessage {...messages.shareAlikeCheckboxLabel} />
-                      </Form.Label>
-                      <ActionRow.Spacer />
-                      <CheckboxControl
-                        checked={details.shareAlike}
-                        disabled={level === LicenseLevel.course}
-                        onChange={(e) => updateField({
-                          licenseDetails: {
-                            ...details,
-                            shareAlike: e.target.checked,
-                            noDerivatives: e.target.checked ? false : details.noDerivatives,
-                          },
-                        })}
-                        aria-label={intl.formatMessage(messages.shareAlikeCheckboxAriaLabel)}
-                      />
-                    </ActionRow>
-                  </Form.Group>
-                  <div>
-                    <FormattedMessage {...messages.shareAlikeSectionDescription} />
-                  </div>
+                  <FormattedMessage {...messages.attributionSectionDescription} />
                 </div>
-              </Stack>
-            ) : null}
-        </Form.Group>
-      </div>
-    ) : null
-  );
-};
+              </div>
+              <div className="border-primary-100 border-bottom pb-4">
+                <Form.Group>
+                  <ActionRow>
+                    <Icon src={Nc} />
+                    <Form.Label className="my-0 text-primary-500">
+                      <FormattedMessage {...messages.noncommercialCheckboxLabel} />
+                    </Form.Label>
+                    <ActionRow.Spacer />
+                    <CheckboxControl
+                      checked={details.noncommercial}
+                      disabled={level === LicenseLevel.course}
+                      onChange={(e) => updateField({
+                        licenseDetails: {
+                          ...details,
+                          noncommercial: e.target.checked,
+                        },
+                      })}
+                      aria-label={intl.formatMessage(messages.noncommercialCheckboxAriaLabel)}
+                    />
+                  </ActionRow>
+                </Form.Group>
+                <div>
+                  <FormattedMessage {...messages.noncommercialSectionDescription} />
+                </div>
+              </div>
+              <div className="border-primary-100 border-bottom pb-4">
+                <Form.Group>
+                  <ActionRow>
+                    <Icon src={Nd} />
+                    <Form.Label className="my-0 text-primary-500">
+                      <FormattedMessage {...messages.noDerivativesCheckboxLabel} />
+                    </Form.Label>
+                    <ActionRow.Spacer />
+                    <CheckboxControl
+                      checked={details.noDerivatives}
+                      disabled={level === LicenseLevel.course}
+                      onChange={(e) => updateField({
+                        licenseDetails: {
+                          ...details,
+                          noDerivatives: e.target.checked,
+                          shareAlike: e.target.checked ? false : details.shareAlike,
+                        },
+                      })}
+                      aria-label={intl.formatMessage(messages.noDerivativesCheckboxAriaLabel)}
+                    />
+                  </ActionRow>
+                </Form.Group>
+                <div>
+                  <FormattedMessage {...messages.noDerivativesSectionDescription} />
+                </div>
+              </div>
+              <div>
+                <Form.Group>
+                  <ActionRow>
+                    <Icon src={Sa} />
+                    <Form.Label className="my-0 text-primary-500">
+                      <FormattedMessage {...messages.shareAlikeCheckboxLabel} />
+                    </Form.Label>
+                    <ActionRow.Spacer />
+                    <CheckboxControl
+                      checked={details.shareAlike}
+                      disabled={level === LicenseLevel.course}
+                      onChange={(e) => updateField({
+                        licenseDetails: {
+                          ...details,
+                          shareAlike: e.target.checked,
+                          noDerivatives: e.target.checked ? false : details.noDerivatives,
+                        },
+                      })}
+                      aria-label={intl.formatMessage(messages.shareAlikeCheckboxAriaLabel)}
+                    />
+                  </ActionRow>
+                </Form.Group>
+                <div>
+                  <FormattedMessage {...messages.shareAlikeSectionDescription} />
+                </div>
+              </div>
+            </Stack>
+          ) : null}
+      </Form.Group>
+    </div>
+  ) : null
+);
 
 LicenseDetails.propTypes = {
   license: PropTypes.string.isRequired,
@@ -162,6 +160,7 @@ LicenseDetails.propTypes = {
   level: PropTypes.string.isRequired,
   // redux
   updateField: PropTypes.func.isRequired,
+  intl: intlShape.isRequired,
 };
 
 export const mapStateToProps = () => ({});
