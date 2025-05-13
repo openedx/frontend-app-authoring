5167b167eb4175a2aa3e79c4deaab8cab17bcb1a
Ahtisham Shahid <ahtisham300@gmail.com>
2025-05-13 14:22:56 +0500

    feat: added process to sync discussions topic on page load
    feat: added process to sync discussions topic on page load
    
    fix: updated failing unit tests
    
    feat: added course creation date in index api
    
    feat: added course creation date in index api
    
    fix: updated unit tests

--- src/course-outline/CourseOutline.test.jsx
+++ src/course-outline/CourseOutline.test.jsx
@@ -27,7 +27,7 @@ import { RequestStatus } from '../data/constants';
 import {
   fetchCourseBestPracticesQuery,
   fetchCourseLaunchQuery,
-  fetchCourseOutlineIndexQuery,
+  fetchCourseOutlineIndexQuery, syncDiscussionsTopics,
   updateCourseSectionHighlightsQuery,
 } from './data/thunk';
 import initializeStore from '../store';
@@ -126,6 +126,10 @@ jest.mock('@dnd-kit/core', () => ({
   closestCorners: jest.fn(),
 }));
 
+jest.mock('./data/api', () => ({
+  ...jest.requireActual('./data/api'),
+  createDiscussionsTopics: jest.fn().mockResolvedValue(undefined),
+}));
 // eslint-disable-next-line no-promise-executor-return
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -173,6 +177,7 @@ describe('<CourseOutline />', () => {
       }))
       .reply(200, courseLaunchMock);
     await executeThunk(fetchCourseOutlineIndexQuery(courseId), store.dispatch);
+    await executeThunk(syncDiscussionsTopics(courseId), store.dispatch);
   });
 
   afterEach(() => {
