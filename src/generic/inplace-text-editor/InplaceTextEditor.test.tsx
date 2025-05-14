3095249856b81af695f326ceaef8f61029cf6284
Rômulo Penido <romulo.penido@gmail.com>
2025-05-14 18:10:37 -0300

    test: add test for pending text and error

--- src/generic/inplace-text-editor/InplaceTextEditor.test.tsx
+++ src/generic/inplace-text-editor/InplaceTextEditor.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
-import { fireEvent, render as baseRender, screen } from '@testing-library/react';
+import {
+  act,
+  fireEvent,
+  render as baseRender,
+  screen,
+} from '@testing-library/react';
 import { InplaceTextEditor } from '.';
 
 const mockOnSave = jest.fn();
@@ -67,5 +72,42 @@ describe('<InplaceTextEditor />', () => {
 
     expect(textBox).not.toBeInTheDocument();
     expect(mockOnSave).not.toHaveBeenCalled();
+  });
+
+  it('should show the new text while processing and roolback in case of error', async () => {
+    let rejecter: (err: Error) => void;
+    const longMockOnSave = jest.fn().mockReturnValue(
+      new Promise<void>((_resolve, reject) => {
+        rejecter = reject;
+      }),
+    );
+    render(<InplaceTextEditor text="Test text" onSave={longMockOnSave} />);
+
+    const text = screen.getByText('Test text');
+    expect(text).toBeInTheDocument();
+
+    const editButton = screen.getByRole('button', { name: /edit/i });
+    expect(editButton).toBeInTheDocument();
+    fireEvent.click(editButton);
+
+    const textBox = screen.getByRole('textbox');
+
+    fireEvent.change(textBox, { target: { value: 'New text' } });
+    fireEvent.keyDown(textBox, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+    expect(textBox).not.toBeInTheDocument();
+    expect(longMockOnSave).toHaveBeenCalledWith('New text');
+
+    // Show pending new text
+    const newText = screen.getByText('New text');
+    expect(newText).toBeInTheDocument();
+
+    await act(async () => { rejecter(new Error('error')); });
+
+    // Remove pending new text on error
+    expect(newText).not.toBeInTheDocument();
+
+    // Show original text
+    expect(screen.getByText('Test text')).toBeInTheDocument();
   });
 });
