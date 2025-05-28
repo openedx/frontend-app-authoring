e844343be0b2f0c86339db0d9831d1c2c9d3eb4d
skongala-psi <skongala@primesoft.net>
2025-05-28 17:36:07 +0530

    Refactor MyCourses layout as per latest UX.

--- src/my-courses/course-card/CourseCard.tsx
+++ src/my-courses/course-card/CourseCard.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import { Card, Button } from '@openedx/paragon';
+import { Card, Button, IconButtonWithTooltip, Icon } from '@openedx/paragon';
 import './CourseCard.scss';
+import { Repeat } from '@openedx/paragon/icons';
 
 type CourseCardProps = {
   imageSrc?: string;
@@ -22,22 +23,23 @@ const CourseCard: React.FC<CourseCardProps> = ({
   onEditCourse,
   onReRun,
 }) => (
-  <Card style={{ width: '18rem' }} className="course-card">
+  <Card style={{ width: '19rem' }} className="course-card">
     <Card.ImageCap
       src={imageSrc === null ? fallbackImage : imageSrc}
       srcAlt="Course cover"
       className="course-card-image"
     />
-    <Card.Section className="text-center p-4">
+    <Card.Section className="text-center card-content-section">
       <div className="title-container">
         <h6 className="card-title fw-bold">{title}</h6>
+        <p className="text-muted small">{metadata}</p>
       </div>
-      <hr />
-      <p className="text-muted small">{metadata}</p>
-      <div className="d-flex justify-content-between mt-3 gap-2 course-card-buttons">
+      {/* <hr /> */}
+      {/* <p className="text-muted">{metadata}</p> */}
+      <div className="d-flex justify-content-between gap-2 course-card-buttons">
         <Button size="sm" variant="primary" onClick={onViewLive}>View live</Button>
         <Button size="sm" variant="outline-primary" onClick={onEditCourse}>Edit course</Button>
-        <Button size="sm" variant="primary" onClick={onReRun}>Re-run</Button>
+        <IconButtonWithTooltip src={Repeat} size="sm" variant="primary" iconAs={Icon} alt="Re-run" tooltipPlacement="bottom" tooltipContent="Re-run" onClick={onReRun} />
       </div>
     </Card.Section>
   </Card>
