7b9926eb93fdb342b16442f3423f21cf20fe6a0f
Tony Busa <tony.busa@wgu.edu>
2025-06-02 10:40:45 -0600

    fix: backport changes for html button in text component markdown editor

--- src/editors/sharedComponents/SourceCodeModal/index.jsx
+++ src/editors/sharedComponents/SourceCodeModal/index.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -41,6 +40,7 @@ const SourceCodeModal = ({
         <CodeEditor
           innerRef={ref}
           value={value}
+          lang="html" // hardcoded value to do lookup on CODEMIRROR_LANGUAGES
         />
       </div>
     </BaseModal>
