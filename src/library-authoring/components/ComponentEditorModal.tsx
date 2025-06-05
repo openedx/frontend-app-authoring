a8feaf214dc69c4d8034f4fd95cd8c61c31a5aa8
Braden MacDonald <braden@opencraft.com>
2025-06-05 16:52:13 -0700

    fix: enable markdown editor for problems in libraries too
    This fix is also achieved on master via 5991fd39976ad336cde0f068e50af198d737bdad / https://github.com/openedx/frontend-app-authoring/pull/2068 but this is a simpler fix, not a direct backport of that refactor.

--- src/library-authoring/components/ComponentEditorModal.tsx
+++ src/library-authoring/components/ComponentEditorModal.tsx
@@ -1,7 +1,9 @@
 import { getConfig } from '@edx/frontend-platform';
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import { useQueryClient } from '@tanstack/react-query';
+
+import { getWaffleFlags } from '../../data/selectors';
 import EditorPage from '../../editors/EditorPage';
 import { getBlockType } from '../../generic/key-utils';
 import { useLibraryContext } from '../common/context/LibraryContext';
@@ -21,6 +23,7 @@ export function canEditComponent(usageKey: string): boolean {
 export const ComponentEditorModal: React.FC<Record<never, never>> = () => {
   const { componentBeingEdited, closeComponentEditor, libraryId } = useLibraryContext();
   const queryClient = useQueryClient();
+  const { useReactMarkdownEditor } = useSelector(getWaffleFlags);
 
   if (componentBeingEdited === undefined) {
     return null;
@@ -37,6 +40,7 @@ export const ComponentEditorModal: React.FC<Record<never, never>> = () => {
       courseId={libraryId}
       blockType={blockType}
       blockId={componentBeingEdited.usageKey}
+      isMarkdownEditorEnabledForCourse={useReactMarkdownEditor}
       studioEndpointUrl={getConfig().STUDIO_BASE_URL}
       lmsEndpointUrl={getConfig().LMS_BASE_URL}
       onClose={onClose}
