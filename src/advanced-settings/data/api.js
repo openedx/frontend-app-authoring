b440fc4bc07f0dd82fdf098abf4e23ed79ddcee1
Jose Ignacio Palma <jignaciopm13@gmail.com>
2025-06-06 13:58:30 -0400

    fix: add comments

--- src/advanced-settings/data/api.js
+++ src/advanced-settings/data/api.js
@@ -19,7 +19,9 @@ const getProctoringErrorsApiUrl = () => `${getApiBaseUrl()}/api/contentstore/v1/
 export async function getCourseAdvancedSettings(courseId) {
   const { data } = await getAuthenticatedHttpClient()
     .get(`${getCourseAdvancedSettingsApiUrl(courseId)}?fetch_all=0`);
+  // we start with: { camelCaseField: { value: { shouldBeSnakeCase: '123' } }, ... }
   const objectFormatted = camelCaseObject(data);
+  // we want: { camelCaseField: { value: { should_be_snake_case: '123' } }, ... }
   Object.keys(objectFormatted).forEach((key) => {
     if (objectFormatted[key]?.value) {
       objectFormatted[key].value = snakeCaseObject(objectFormatted[key].value);
@@ -37,7 +39,9 @@ export async function getCourseAdvancedSettings(courseId) {
 export async function updateCourseAdvancedSettings(courseId, settings) {
   const { data } = await getAuthenticatedHttpClient()
     .patch(`${getCourseAdvancedSettingsApiUrl(courseId)}`, convertObjectToSnakeCase(settings));
+  // we start with: { camelCaseField: { value: { shouldBeSnakeCase: '123' } }, ... }
   const objectFormatted = camelCaseObject(data);
+  // we want: { camelCaseField: { value: { should_be_snake_case: '123' } }, ... }
   Object.keys(objectFormatted).forEach((key) => {
     if (objectFormatted[key]?.value) {
       objectFormatted[key].value = snakeCaseObject(objectFormatted[key].value);
