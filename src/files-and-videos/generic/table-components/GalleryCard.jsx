08041ab292570e6cf59537162057a51cdab278f2
krishnah802 <kjulakanti@primesoft.net>
2025-06-10 12:47:42 +0530

    indentation changes

--- src/files-and-videos/generic/table-components/GalleryCard.jsx
+++ src/files-and-videos/generic/table-components/GalleryCard.jsx
@@ -40,58 +40,58 @@ const GalleryCard = ({
         fileType,
       }}
     >
-      <Card className={`${className} w-100 gallery-card`} data-testid={`grid-card-${original.id}`}>
-        <Card.Header
-          className="pr-0 pt-2 pb-2"
-          actions={(
-            <ActionRow>
-              <FileMenu
-                externalUrl={original.externalUrl}
-                handleLock={lockFile}
-                locked={original.locked}
-                openAssetInfo={() => handleOpenFileInfo(original)}
-                portableUrl={original.portableUrl}
-                id={original.id}
-                fileType={fileType}
-                onDownload={() => handleBulkDownload([{
-                  original: {
-                    id: original.id,
-                    displayName:
-                  original.displayName,
-                    downloadLink: original?.downloadLink,
-                  },
-                }])}
-                openDeleteConfirmation={() => handleOpenDeleteConfirmation([{ original }])}
-              />
-            </ActionRow>
-        )}
-        />
-        <Card.Section className="pr-3 pl-3 pt-0 pb-0">
-          <div className="row align-items-center justify-content-center m-0">
-            <FileThumbnail
-              thumbnail={original.thumbnail}
-              wrapperType={original.wrapperType}
+    <Card className={`${className} w-100 gallery-card`} data-testid={`grid-card-${original.id}`}>
+      <Card.Header
+        className="pr-0 pt-2 pb-2"
+        actions={(
+          <ActionRow>
+            <FileMenu
               externalUrl={original.externalUrl}
-              displayName={original.displayName}
+              handleLock={lockFile}
+              locked={original.locked}
+              openAssetInfo={() => handleOpenFileInfo(original)}
+              portableUrl={original.portableUrl}
               id={original.id}
-              status={original.status}
-              imageSize={{ height: '76px', width: '135.71px' }}
-              thumbnailPreview={thumbnailPreview}
+              fileType={fileType}
+              onDownload={() => handleBulkDownload([{
+                original: {
+                  id: original.id,
+                  displayName:
+                  original.displayName,
+                  downloadLink: original?.downloadLink,
+                },
+              }])}
+              openDeleteConfirmation={() => handleOpenDeleteConfirmation([{ original }])}
             />
-          </div>
-          <div style={{ wordBreak: 'break-word' }}>
-            <Truncate lines={1} className="font-weight-bold mt-2 picture-title">
-              {original.displayName}
-            </Truncate>
-          </div>
-        </Card.Section>
-        <Card.Footer className="p-3 pt-4 row m-0 flex-row-reverse justify-content-between align-items-center">
-          <Chip>
-            {original.wrapperType}
-          </Chip>
-          {original.transcripts?.length > 0 && <Icon size="lg" src={ClosedCaption} className="m-0 text-primary-500" />}
-        </Card.Footer>
-      </Card>
+          </ActionRow>
+        )}
+      />
+      <Card.Section className="pr-3 pl-3 pt-0 pb-0">
+        <div className="row align-items-center justify-content-center m-0">
+          <FileThumbnail
+            thumbnail={original.thumbnail}
+            wrapperType={original.wrapperType}
+            externalUrl={original.externalUrl}
+            displayName={original.displayName}
+            id={original.id}
+            status={original.status}
+            imageSize={{ height: '76px', width: '135.71px' }}
+            thumbnailPreview={thumbnailPreview}
+          />
+        </div>
+        <div style={{ wordBreak: 'break-word' }}>
+          <Truncate lines={1} className="font-weight-bold mt-2 picture-title">
+            {original.displayName}
+          </Truncate>
+        </div>
+      </Card.Section>
+      <Card.Footer className="p-3 pt-4 row m-0 flex-row-reverse justify-content-between align-items-center">
+        <Chip>
+          {original.wrapperType}
+        </Chip>
+        {original.transcripts?.length > 0 && <Icon size="lg" src={ClosedCaption} className="m-0 text-primary-500" />}
+      </Card.Footer>
+    </Card>
     </PluginSlot>
   );
 };
