5df245992f08cf30526772fb2fb13c7e4171f6d5
Jillian Vogel <jill@opencraft.com>
2025-06-16 01:16:38 +0930

    fix: allow "add new" button to open new container modal
    instead of just opening the sidebar, when adding a container to another
    container.
    
    This saves the user a click and makes this consistent with the
    "add new component" button on unit page.

--- src/library-authoring/containers/FooterActions.tsx
+++ src/library-authoring/containers/FooterActions.tsx
@@ -1,21 +1,31 @@
 import { Button, useToggle } from '@openedx/paragon';
 import { Add } from '@openedx/paragon/icons';
+import { type ContainerType } from '../../generic/key-utils';
 import { PickLibraryContentModal } from '../add-content';
 import { useLibraryContext } from '../common/context/LibraryContext';
 import { useSidebarContext } from '../common/context/SidebarContext';
 
 interface FooterActionsProps {
+  addContentType?: ContainerType;
   addContentBtnText: string;
   addExistingContentBtnText: string;
 }
 
 export const FooterActions = ({
+  addContentType,
   addContentBtnText,
   addExistingContentBtnText,
 }: FooterActionsProps) => {
   const [isAddLibraryContentModalOpen, showAddLibraryContentModal, closeAddLibraryContentModal] = useToggle();
   const { openAddContentSidebar } = useSidebarContext();
-  const { readOnly } = useLibraryContext();
+  const { readOnly, setCreateContainerModalType } = useLibraryContext();
+  const addContent = () => {
+    if (addContentType) {
+      setCreateContainerModalType(addContentType);
+    } else {
+      openAddContentSidebar();
+    }
+  };
   return (
     <div className="d-flex">
       <div className="w-100 mr-2">
@@ -23,7 +33,7 @@ export const FooterActions = ({
           className="ml-2"
           iconBefore={Add}
           variant="outline-primary rounded-0"
-          onClick={openAddContentSidebar}
+          onClick={addContent}
           disabled={readOnly}
           block
         >
