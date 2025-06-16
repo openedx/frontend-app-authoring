4ba8cde5878062e4ebd47c051104f85c94a35753
bydawen <oleksandr.buhaienko@raccoongang.com>
2025-06-17 00:43:02 +0300

    fix: (backport) text truncate issue in the search modal (#2151)

--- src/search-modal/SearchResult.tsx
+++ src/search-modal/SearchResult.tsx
@@ -181,7 +181,7 @@ const SearchResult: React.FC<{ hit: ContentHit }> = ({ hit }) => {
         <div className="hit-name small">
           <Highlight text={hit.formatted.displayName} />
         </div>
-        <div className="hit-description x-small text-truncate">
+        <div className="hit-description x-small">
           <Highlight text={hit.formatted.content?.htmlContent ?? ''} />
           <Highlight text={hit.formatted.content?.capaContent ?? ''} />
         </div>
