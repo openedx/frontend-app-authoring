96a04d44923f00a7b5c8e92b61f642260f291ef5
Brian Smith <112954497+brian-smith-tcril@users.noreply.github.com>
2025-06-18 15:17:18 -0400

    feat!: add design tokens support (#2187)
    BREAKING CHANGE: Pre-design-tokens theming is no longer supported.
    
    Co-authored-by: Diana Olarte <diana.olarte@edunext.co>

--- src/optimizer-page/scan-results/BrokenLinkTable.tsx
+++ src/optimizer-page/scan-results/BrokenLinkTable.tsx
@@ -1,14 +1,16 @@
 import {
-  Card, Icon, OverlayTrigger, Table, Tooltip,
+  Card, Icon, DataTable,
 } from '@openedx/paragon';
 import {
-  OpenInNew, Lock, LinkOff, InfoOutline,
+  ArrowForwardIos,
+  LinkOff,
 } from '@openedx/paragon/icons';
-import { useIntl } from '@edx/frontend-platform/i18n';
 import { FC } from 'react';
-import { Unit } from '../types';
+import { Filters, Unit } from '../types';
 import messages from './messages';
-import LockedInfoIcon from './LockedInfoIcon';
+import CustomIcon from './CustomIcon';
+import lockedIcon from './lockedIcon';
+import ManualIcon from './manualIcon';
 
 const BrokenLinkHref: FC<{ href: string }> = ({ href }) => {
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
@@ -25,145 +27,139 @@ const BrokenLinkHref: FC<{ href: string }> = ({ href }) => {
   );
 };
 
-const GoToBlock: FC<{ block: { url: string } }> = ({ block }) => {
+const GoToBlock: FC<{ block: { url: string, displayName: string } }> = ({ block }) => {
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     window.open(block.url, '_blank');
   };
 
   return (
-    <span style={{ display: 'flex', gap: '.5rem' }}>
-      <Icon src={OpenInNew} />
-      <a href={block.url} onClick={handleClick} rel="noreferrer">
-        Go to Block
+    <div className="go-to-block-link-container">
+      <a href={block.url} onClick={handleClick} className="broken-link" rel="noreferrer">
+        {block.displayName}
       </a>
-    </span>
+    </div>
   );
 };
 
-const RecommendedManualCheckHeading = () => {
-  const intl = useIntl();
-  return (
-    <span className="d-flex align-items-center font-weight-bold py-2">
-      {intl.formatMessage(messages.recommendedManualCheckText)}
-      <OverlayTrigger
-        key="top"
-        placement="top"
-        overlay={(
-          <Tooltip id="tooltip-top">
-            {intl.formatMessage(messages.recommendedManualCheckTooltip)}
-          </Tooltip>
-            )}
-      >
-        <Icon className="ml-1 pl-1" src={InfoOutline} />
-      </OverlayTrigger>
-    </span>
-  );
+const iconsMap = {
+  broken: {
+    icon: LinkOff,
+    message1: messages.brokenLabel,
+    message2: messages.brokenInfoTooltip,
+  },
+  locked: {
+    icon: lockedIcon,
+    message1: messages.lockedLabel,
+    message2: messages.lockedInfoTooltip,
+  },
+  manual: {
+    icon: ManualIcon,
+    message1: messages.manualLabel,
+    message2: messages.manualInfoTooltip,
+  },
 };
+
+const LinksCol: FC<{ block: { url: string, displayName: string }, href: string, linkType: string }> = (
+  { block, href, linkType },
+) => (
+  <span className="links-container">
+    <GoToBlock block={{ url: block.url, displayName: block.displayName || 'Go to block' }} />
+    <Icon className="arrow-forward-ios" src={ArrowForwardIos} style={{ color: '#8F8F8F' }} />
+    <BrokenLinkHref href={href} />
+    <div style={{ marginLeft: 'auto', marginRight: '10px' }}>
+      <CustomIcon
+        icon={iconsMap[linkType].icon}
+        message1={iconsMap[linkType].message1}
+        message2={iconsMap[linkType].message2}
+      />
+    </div>
+  </span>
+);
 
 interface BrokenLinkTableProps {
   unit: Unit;
-  showLockedLinks: boolean;
+  filters: Filters;
 }
 
 type TableData = {
-  blockLink: JSX.Element;
-  brokenLink: JSX.Element;
-  status: JSX.Element;
+  Links: JSX.Element;
 }[];
 
 const BrokenLinkTable: FC<BrokenLinkTableProps> = ({
   unit,
-  showLockedLinks,
+  filters,
 }) => {
-  const intl = useIntl();
+  const brokenLinkList = unit.blocks.reduce(
+    (
+      acc: TableData,
+      block,
+    ) => {
+      if (
+        filters.brokenLinks
+            || (!filters.brokenLinks && !filters.externalForbiddenLinks && !filters.lockedLinks)
+      ) {
+        const blockBrokenLinks = block.brokenLinks.map((link) => ({
+          Links: (
+            <LinksCol
+              block={{ url: block.url, displayName: block.displayName || 'Go to block' }}
+              href={link}
+              linkType="broken"
+            />
+          ),
+        }));
+        acc.push(...blockBrokenLinks);
+      }
+
+      if (
+        filters.lockedLinks
+            || (!filters.brokenLinks && !filters.externalForbiddenLinks && !filters.lockedLinks)
+      ) {
+        const blockLockedLinks = block.lockedLinks.map((link) => ({
+          Links: (
+            <LinksCol
+              block={{ url: block.url, displayName: block.displayName || 'Go to block' }}
+              href={link}
+              linkType="locked"
+            />
+          ),
+        }));
+
+        acc.push(...blockLockedLinks);
+      }
+
+      if (
+        filters.externalForbiddenLinks
+            || (!filters.brokenLinks && !filters.externalForbiddenLinks && !filters.lockedLinks)
+      ) {
+        const externalForbiddenLinks = block.externalForbiddenLinks.map((link) => ({
+          Links: (
+            <LinksCol
+              block={{ url: block.url, displayName: block.displayName || 'Go to block' }}
+              href={link}
+              linkType="manual"
+            />
+          ),
+        }));
+
+        acc.push(...externalForbiddenLinks);
+      }
+
+      return acc;
+    },
+    [],
+  );
+
   return (
-    <Card className="unit-card rounded-sm pt-2 pl-3 pr-4 mb-2.5">
+    <Card className="unit-card rounded-sm pt-2 pb-3 pl-3 pr-4 mb-2.5">
       <p className="unit-header">{unit.displayName}</p>
-      <Table
-        data={unit.blocks.reduce(
-          (
-            acc: TableData,
-            block,
-          ) => {
-            const blockBrokenLinks = block.brokenLinks.map((link) => ({
-              blockLink: <GoToBlock block={block} />,
-              blockDisplayName: block.displayName || '',
-              brokenLink: <BrokenLinkHref href={link} />,
-              status: (
-                <span className="link-status-text">
-                  <Icon src={LinkOff} className="broken-link-icon" />
-                  <span>
-                    {intl.formatMessage(messages.brokenLinkStatus)}
-                  </span>
-                </span>
-              ),
-            }));
-            acc.push(...blockBrokenLinks);
-
-            if (showLockedLinks) {
-              const blockLockedLinks = block.lockedLinks.map((link) => ({
-                blockLink: <GoToBlock block={block} />,
-                blockDisplayName: block.displayName || '',
-                brokenLink: <BrokenLinkHref href={link} />,
-                status: (
-                  <span className="link-status-text">
-                    <Icon src={Lock} className="lock-icon" />
-                    {intl.formatMessage(messages.lockedLinkStatus)}{' '}
-                    <LockedInfoIcon />
-                  </span>
-                ),
-              }));
-
-              acc.push(...blockLockedLinks);
-            }
-
-            if (block.externalForbiddenLinks?.length > 0) {
-              const recommendedManualCheckHeading = {
-                blockLink: <div />,
-                blockDisplayName: <RecommendedManualCheckHeading />,
-                brokenLink: <div />,
-                status: <div />,
-              };
-              const externalForbiddenLinks = block.externalForbiddenLinks.map((link) => ({
-                blockLink: <GoToBlock block={block} />,
-                blockDisplayName: block.displayName || '',
-                brokenLink: <BrokenLinkHref href={link} />,
-                status: <div />,
-              }));
-
-              acc.push(recommendedManualCheckHeading);
-              acc.push(...externalForbiddenLinks);
-            }
-
-            return acc;
-          },
-          [],
-        )}
+      <DataTable
+        data={brokenLinkList}
+        itemCount={brokenLinkList.length}
         columns={[
           {
-            key: 'blockDisplayName',
-            columnSortable: false,
-            width: 'col-3',
-            hideHeader: true,
-          },
-          {
-            key: 'blockLink',
-            columnSortable: false,
-            width: 'col-3',
-            hideHeader: true,
-          },
-          {
-            key: 'brokenLink',
-            columnSortable: false,
-            width: 'col-6',
-            hideHeader: true,
-          },
-          {
-            key: 'status',
-            columnSortable: false,
-            width: 'col-6',
-            hideHeader: true,
+            accessor: 'Links',
+            width: 'col-12',
           },
         ]}
       />
