085cd7d05c08e8dfd46a10fc171b582872b7c020
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-06-24 03:50:54 -0600

    test: replacing snapshot tests with RTL tests part 10 (#2207)

--- src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/settingsComponents/ResetCard.test.jsx
+++ src/editors/containers/ProblemEditor/components/EditProblemView/SettingsWidget/settingsComponents/ResetCard.test.jsx
@@ -1,27 +1,14 @@
-import 'CourseAuthoring/editors/setupEditorTest';
 import React from 'react';
-import { shallow } from '@edx/react-unit-test-utils';
-import { useSelector } from 'react-redux';
+import {
+  render, screen, initializeMocks, fireEvent,
+} from '@src/testUtils';
+import * as reactredux from 'react-redux';
 import { formatMessage } from '../../../../../../testUtils';
-import { ResetCardInternal as ResetCard } from './ResetCard';
-import { resetCardHooks } from '../hooks';
-
-jest.mock('../hooks', () => ({
-  resetCardHooks: jest.fn(),
-}));
-
-jest.mock('../../../../../../data/redux', () => ({
-  selectors: {
-    app: {
-      studioEndpointUrl: 'sTuDioEndpOintUrl',
-      learningContextId: 'leArningCoNteXtId',
-    },
-  },
-}));
-
-useSelector.mockImplementation((args) => args);
+import ResetCard from './ResetCard';
+import * as hooks from '../hooks';
 
 describe('ResetCard', () => {
+  const resetText = "Determines whether a 'Reset' button is shown so the user may reset their answer, generally for use in practice or formative assessments.";
   const props = {
     showResetButton: false,
     updateSettings: jest.fn().mockName('args.updateSettings'),
@@ -33,21 +20,50 @@ describe('ResetCard', () => {
     setResetFalse: jest.fn().mockName('resetCardHooks.setResetFalse'),
   };
 
-  resetCardHooks.mockReturnValue(resetCardHooksProps);
+  beforeEach(() => {
+    initializeMocks();
+    jest.spyOn(reactredux, 'useSelector').mockImplementation((args) => args);
+  });
 
   describe('behavior', () => {
-    it(' calls resetCardHooks when initialized', () => {
-      shallow(<ResetCard {...props} />);
-      expect(resetCardHooks).toHaveBeenCalledWith(props.updateSettings);
+    it('calls resetCardHooks when initialized', () => {
+      jest.spyOn(hooks, 'resetCardHooks').mockReturnValue(resetCardHooksProps);
+      render(<ResetCard {...props} />);
+      expect(hooks.resetCardHooks).toHaveBeenCalledWith(props.updateSettings);
     });
   });
 
-  describe('snapshot', () => {
-    test('snapshot: renders reset true setting card', () => {
-      expect(shallow(<ResetCard {...props} />).snapshot).toMatchSnapshot();
+  describe('renders', () => {
+    test('renders reset true setting card', () => {
+      render(<ResetCard {...props} showResetButton />);
+      expect(screen.getByText('Show reset option')).toBeInTheDocument();
+      expect(screen.getByText('True')).toBeInTheDocument();
     });
-    test('snapshot: renders reset true setting card', () => {
-      expect(shallow(<ResetCard {...props} showResetButton />).snapshot).toMatchSnapshot();
+
+    test('renders reset false setting card', () => {
+      render(<ResetCard {...props} />);
+      expect(screen.getByText('Show reset option')).toBeInTheDocument();
+      expect(screen.getByText('False')).toBeInTheDocument();
+    });
+
+    test('renders link when isLibrary is false', () => {
+      jest.spyOn(reactredux, 'useSelector').mockReturnValueOnce(false); // mock isLibrary value
+      render(<ResetCard {...props} />);
+      const resetOption = screen.getByText('Show reset option');
+      expect(resetOption).toBeInTheDocument();
+      fireEvent.click(resetOption);
+      expect(screen.getByText(resetText)).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: 'Set a default value in advanced settings in a new tab' })).toBeInTheDocument();
+    });
+
+    test('do not render link when isLibrary is true', () => {
+      jest.spyOn(reactredux, 'useSelector').mockReturnValueOnce(true); // mock isLibrary value
+      render(<ResetCard {...props} />);
+      const resetOption = screen.getByText('Show reset option');
+      expect(resetOption).toBeInTheDocument();
+      fireEvent.click(resetOption);
+      expect(screen.getByText(resetText)).toBeInTheDocument();
+      expect(screen.queryByRole('link', { name: 'Set a default value in advanced settings in a new tab' })).not.toBeInTheDocument();
     });
   });
 });
