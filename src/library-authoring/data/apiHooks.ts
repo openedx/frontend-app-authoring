a2b30565bbffed1d612da180ecf9352ccb479614
Navin Karkera <navin@opencraft.com>
2025-06-24 16:42:14 +0200

    fix: library id in single index query

--- src/library-authoring/data/apiHooks.ts
+++ src/library-authoring/data/apiHooks.ts
@@ -876,17 +876,22 @@ export const usePublishContainer = (containerId: string) => {
  */
 export const useContentFromSearchIndex = (contentIds: string[]) => {
   const { client, indexName } = useContentSearchConnection();
-  let libraryId: string | undefined;
+  const extraFilter = [`usage_key IN ["${contentIds.join('","')}"]`];
   // NOTE: assuming that all contentIds are part of a single libraryId as we don't have a usecase
   // of passing multiple contentIds from different libraries.
   if (contentIds.length > 0) {
-    libraryId = getLibraryId(contentIds?.[0]);
+    try {
+      const libraryId = getLibraryId(contentIds?.[0]);
+      extraFilter.push(`context_key = "${libraryId}"`);
+    } catch {
+      // Ignore as the contentIds could be part of course instead of a library.
+    }
   }
   return useContentSearchResults({
     client,
     indexName,
     searchKeywords: '',
-    extraFilter: [`usage_key IN ["${contentIds.join('","')}"]`, `context_key = "${libraryId}"`],
+    extraFilter,
     limit: contentIds.length,
     enabled: !!contentIds.length,
     skipBlockTypeFetch: true,
