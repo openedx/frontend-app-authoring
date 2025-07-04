aefb90864843c4f819028f61ce11b854e0e198aa
Brayan Ceron <brayan.ceron@edunext.co>
2025-07-04 11:41:45 -0500

    fix: enable publish btn when an xblock is edited

--- src/course-unit/xblock-container-iframe/index.tsx
+++ src/course-unit/xblock-container-iframe/index.tsx
@@ -37,6 +37,7 @@ import { useIframeContent } from '../../generic/hooks/useIframeContent';
 import { useIframeMessages } from '../../generic/hooks/useIframeMessages';
 import VideoSelectorPage from '../../editors/VideoSelectorPage';
 import EditorPage from '../../editors/EditorPage';
+import { setXBlockPublishState } from '../data/slice';
 
 const XBlockContainerIframe: FC<XBlockContainerIframeProps> = ({
   courseId, blockId, unitXBlockActions, courseVerticalChildren, handleConfigureSubmit, isUnitVerticalType,
@@ -74,6 +75,8 @@ const XBlockContainerIframe: FC<XBlockContainerIframeProps> = ({
     closeXBlockEditorModal();
     closeVideoSelectorModal();
     sendMessageToIframe(messageTypes.completeXBlockEditing, { locator: newBlockId });
+    // This ensures the publish button is able
+    dispatch(setXBlockPublishState(false));
   }, [closeXBlockEditorModal, closeVideoSelectorModal, sendMessageToIframe, newBlockId]);
 
   const handleEditXBlock = useCallback((type: string, id: string) => {
