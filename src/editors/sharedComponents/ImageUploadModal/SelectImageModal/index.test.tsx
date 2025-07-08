3673c5f5611eb164d1b976c5c088b6a35911baa3
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-07-08 10:37:07 -0600

    test: replacing snapshot tests with RTL tests part 14 (#2236)

--- src/editors/sharedComponents/ImageUploadModal/SelectImageModal/index.test.tsx
+++ src/editors/sharedComponents/ImageUploadModal/SelectImageModal/index.test.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import { shallow } from '@edx/react-unit-test-utils';
-
-import { formatMessage } from '../../../testUtils';
-import SelectionModal from '../../SelectionModal';
-import * as hooks from './hooks';
+import { render, initializeMocks } from '@src/testUtils';
+import { formatMessage } from '@src/editors/testUtils';
 import { SelectImageModalInternal as SelectImageModal } from '.';
-
-const mockImage = {
-  displayName: 'DALL·E 2023-03-10.png',
-  contentType: 'image/png',
-  dateAdded: 1682009100000,
-  url: '/asset-v1:TestX+Test01+Test0101+type@asset+block@DALL_E_2023-03-10.png',
-  externalUrl: 'http://localhost:18000/asset-v1:TestX+Test01+Test0101+type@asset+block@DALL_E_2023-03-10.png',
-  portableUrl: '/static/DALL_E_2023-03-10.png',
-  thumbnail: '/asset-v1:TestX+Test01+Test0101+type@thumbnail+block@DALL_E_2023-03-10.jpg',
-  locked: false,
-  staticFullUrl: '/assets/courseware/v1/af2bf9ac70804e54c534107160a8e51e/asset-v1:TestX+Test01+Test0101+type@asset+block@DALL_E_2023-03-10.png',
-  id: 'asset-v1:TestX+Test01+Test0101+type@asset+block@DALL_E_2023-03-10.png',
-  width: 100,
-  height: 150,
-};
-
-const mockImagesRef = { current: [mockImage] };
 
 jest.mock('../../BaseModal', () => 'BaseModal');
 jest.mock('../../FileInput', () => 'FileInput');
@@ -80,35 +60,35 @@ describe('SelectImageModal', () => {
       close: jest.fn().mockName('props.close'),
       setSelection: jest.fn().mockName('props.setSelection'),
       clearSelection: jest.fn().mockName('props.clearSelection'),
-      images: mockImagesRef,
+      isLoaded: true,
+      isFetchError: false,
+      isUploadError: false,
+      imageCount: 1,
+      images: ['image.pgn'],
       intl: { formatMessage },
     };
-    let el;
-    const imgHooks = hooks.imgHooks();
     beforeEach(() => {
-      el = shallow(<SelectImageModal {...props} />);
+      initializeMocks();
     });
-    test('snapshot', () => {
-      expect(el.snapshot).toMatchSnapshot();
+    test('renders correctly', () => {
+      const { container } = render(<SelectImageModal {...props} />);
+      expect(container.querySelector('SelectionModal')).toBeInTheDocument();
     });
     it('provides confirm action, forwarding selectBtnProps from imgHooks', () => {
-      expect(el.instance.findByType(SelectionModal)[0].props.selectBtnProps).toEqual(
-        expect.objectContaining({ ...hooks.imgHooks().selectBtnProps }),
-      );
+      const { container } = render(<SelectImageModal {...props} />);
+      expect(container.querySelector('SelectionModal')?.getAttribute('selectBtnProps')).toBeTruthy();
     });
     it('provides file upload button linked to fileInput.click', () => {
-      expect(el.instance.findByType(SelectionModal)[0].props.fileInput.click).toEqual(
-        imgHooks.fileInput.click,
-      );
+      const { container } = render(<SelectImageModal {...props} />);
+      expect(container.querySelector('SelectionModal')?.getAttribute('fileInput')).toBeTruthy();
     });
     it('provides a SearchSort component with searchSortProps from imgHooks', () => {
-      expect(el.instance.findByType(SelectionModal)[0].props.searchSortProps).toEqual(imgHooks.searchSortProps);
+      const { container } = render(<SelectImageModal {...props} />);
+      expect(container.querySelector('SelectionModal')?.getAttribute('searchSortProps')).toBeTruthy();
     });
     it('provides a Gallery component with galleryProps from imgHooks', () => {
-      expect(el.instance.findByType(SelectionModal)[0].props.galleryProps).toEqual(imgHooks.galleryProps);
-    });
-    it('provides a FileInput component with fileInput props from imgHooks', () => {
-      expect(el.instance.findByType(SelectionModal)[0].props.fileInput).toMatchObject(imgHooks.fileInput);
+      const { container } = render(<SelectImageModal {...props} />);
+      expect(container.querySelector('SelectionModal')?.getAttribute('galleryProps')).toBeTruthy();
     });
   });
 });
