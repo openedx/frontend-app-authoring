7eae7332b86ecf8a7ea2b6a0a09bb087b6e9ab5c
Navin Karkera <navin@opencraft.com>
2025-07-16 19:40:10 +0530

    refactor: replace relative imports with @src/

--- src/course-outline/data/types.ts
+++ src/course-outline/data/types.ts
@@ -1,4 +1,4 @@
-import { XBlock, XBlockActions } from '../../data/types';
+import { XBlock, XBlockActions } from '@src/data/types';
 
 export interface CourseStructure {
   highlightsEnabledForMessaging: boolean,
