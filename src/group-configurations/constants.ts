9a0ca07ca2507153bdf1e8b5a6bebd7c2a99bd9c
Bryann Valderrama <bryann.valderrama@edunext.co>
2025-07-25 14:42:58 -0500

    chore: solve linter errors

--- src/group-configurations/constants.ts
+++ src/group-configurations/constants.ts
@@ -1,62 +1,62 @@
 import PropTypes from 'prop-types';
 
 export interface Usage {
-    label: string;
-    url: string;
+  label: string;
+  url: string;
 }
 
 export interface Group {
-    id: number;
-    name: string;
-    usage: Usage[];
-    version: number;
+  id: number;
+  name: string;
+  usage: Usage[];
+  version: number;
 }
 
 export interface AvailableGroupParameters {
-    courseId: string;
+  courseId: string;
 }
 
 export interface AvailableGroup {
-    active?: boolean;
-    description?: string;
-    groups: Group[];
-    id: number;
-    name: string;
-    parameters?: AvailableGroupParameters;
-    readOnly?: boolean;
-    scheme: string;
-    version: number;
+  active?: boolean;
+  description?: string;
+  groups: Group[];
+  id: number;
+  name: string;
+  parameters?: AvailableGroupParameters;
+  readOnly?: boolean;
+  scheme: string;
+  version: number;
 }
 
 export const availableGroupPropTypes = {
-    active: PropTypes.bool,
-    description: PropTypes.string,
-    groups: PropTypes.arrayOf(
+  active: PropTypes.bool,
+  description: PropTypes.string,
+  groups: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      usage: PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.number,
-            name: PropTypes.string,
-            usage: PropTypes.arrayOf(
-                PropTypes.shape({
-                    label: PropTypes.string,
-                    url: PropTypes.string,
-                }),
-            ),
-            version: PropTypes.number,
+          label: PropTypes.string,
+          url: PropTypes.string,
         }),
-    ),
-    id: PropTypes.number,
-    name: PropTypes.string,
-    parameters: PropTypes.shape({
-        courseId: PropTypes.string,
+      ),
+      version: PropTypes.number,
     }),
-    readOnly: PropTypes.bool,
-    scheme: PropTypes.string,
-    version: PropTypes.number,
+  ),
+  id: PropTypes.number,
+  name: PropTypes.string,
+  parameters: PropTypes.shape({
+    courseId: PropTypes.string,
+  }),
+  readOnly: PropTypes.bool,
+  scheme: PropTypes.string,
+  version: PropTypes.number,
 };
 
 export const MESSAGE_VALIDATION_TYPES = {
-    error: 'error',
-    warning: 'warning',
+  error: 'error',
+  warning: 'warning',
 } as const;
 
 export type MessageValidationType = typeof MESSAGE_VALIDATION_TYPES[keyof typeof MESSAGE_VALIDATION_TYPES];
