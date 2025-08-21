d9c7910b32a7f85b22b697490d35a7b9c53dc11d
Asad Ali <asad.ali@arbisoft.com>
2025-08-21 12:52:16 +0500

    fix: improve thumbnail upload impl

--- src/files-and-videos/videos-page/VideoThumbnail.jsx
+++ src/files-and-videos/videos-page/VideoThumbnail.jsx
@@ -47,11 +47,11 @@ const VideoThumbnail = ({
   const isFailed = VIDEO_FAILURE_STATUSES.includes(status);
   const failedMessage = intl.formatMessage(messages.failedCheckboxLabel);
 
-  const showThumbnail = allowThumbnailUpload && isUploaded;
+  const showThumbnail = allowThumbnailUpload && thumbnail && isUploaded;
 
   return (
     <div className="video-thumbnail row justify-content-center align-itmes-center">
-      {allowThumbnailUpload && showThumbnail && <div className="thumbnail-overlay" />}
+      {allowThumbnailUpload && <div className="thumbnail-overlay" />}
       {showThumbnail && !thumbnailError && pageLoadStatus === RequestStatus.SUCCESSFUL ? (
         <>
           <div className="border rounded">
@@ -81,6 +81,16 @@ const VideoThumbnail = ({
             style={imageSize}
           >
             <Icon src={VideoFile} style={{ height: '48px', width: '48px' }} />
+          </div>
+          <div className="add-thumbnail" data-testid={`video-thumbnail-${id}`}>
+            <Button
+              variant="primary"
+              size="sm"
+              onClick={fileInputControl.click}
+              tabIndex={0}
+            >
+              {addThumbnailMessage}
+            </Button>
           </div>
           <div className="status-badge">
             {!isUploaded && (
