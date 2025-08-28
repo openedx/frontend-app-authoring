deef558ca95be55af6a2d304cd7347577b509a66
Rômulo Penido <romulo.penido@gmail.com>
2025-08-28 17:42:19 -0300

    feat: add legacy library alert

---
+++ src/studio-home/tabs-section/libraries-tab/MigrateLegacyLibrariesAlert.tsx
@@ -0,0 +1,23 @@
+import { Alert, Button } from '@openedx/paragon';
+import { Warning } from '@openedx/paragon/icons';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
+
+import messages from './messages';
+
+export const MigrateLegacyLibrariesAlert = () => (
+  <Alert variant="warning" icon={Warning}>
+    <Alert.Heading>
+      <FormattedMessage {...messages.alertTitle} />
+    </Alert.Heading>
+    <div className="row">
+      <div className="col-8">
+        <FormattedMessage {...messages.alertDescription} />
+      </div>
+      <div className="col-4 d-flex justify-content-center align-items-start">
+        <Button>
+          <FormattedMessage {...messages.alertReviewButton} />
+        </Button>
+      </div>
+    </div>
+  </Alert>
+);
