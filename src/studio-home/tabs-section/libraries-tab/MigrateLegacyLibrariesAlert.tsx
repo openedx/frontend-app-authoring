308f876e233ce43ec44735e4f7151088ff6e02d7
XnpioChV <xnpiochv@gmail.com>
2025-09-02 14:39:46 -0500

    feat: Add link in the Migrate Legacy Libraries alert

--- src/studio-home/tabs-section/libraries-tab/MigrateLegacyLibrariesAlert.tsx
+++ src/studio-home/tabs-section/libraries-tab/MigrateLegacyLibrariesAlert.tsx
@@ -1,23 +1,33 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Alert, Button } from '@openedx/paragon';
 import { Warning } from '@openedx/paragon/icons';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
 
 import messages from '../messages';
 
-export const MigrateLegacyLibrariesAlert = () => (
-  <Alert variant="warning" icon={Warning}>
-    <Alert.Heading>
-      <FormattedMessage {...messages.alertTitle} />
-    </Alert.Heading>
-    <div className="row">
-      <div className="col-8">
-        <FormattedMessage {...messages.alertDescription} />
+export const MigrateLegacyLibrariesAlert = () => {
+  const navigate = useNavigate();
+
+  const handleClick = useCallback(() => {
+    navigate('migrate');
+  }, []);
+
+  return (
+    <Alert variant="warning" icon={Warning}>
+      <Alert.Heading>
+        <FormattedMessage {...messages.alertTitle} />
+      </Alert.Heading>
+      <div className="row">
+        <div className="col-8">
+          <FormattedMessage {...messages.alertDescription} />
+        </div>
+        <div className="col-4 d-flex justify-content-center align-items-start">
+          <Button onClick={handleClick}>
+            <FormattedMessage {...messages.alertReviewButton} />
+          </Button>
+        </div>
       </div>
-      <div className="col-4 d-flex justify-content-center align-items-start">
-        <Button>
-          <FormattedMessage {...messages.alertReviewButton} />
-        </Button>
-      </div>
-    </div>
-  </Alert>
-);
+    </Alert>
+  );
+};
