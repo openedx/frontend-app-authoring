72e3608b851d7019c3c9883512bde0a920956248
XnpioChV <xnpiochv@gmail.com>
2025-09-04 15:58:49 -0500

    style: Nits on the code

--- src/library-authoring/component-info/ComponentUsageTab.tsx
+++ src/library-authoring/component-info/ComponentUsageTab.tsx
@@ -1,14 +1,12 @@
-import { useIntl } from '@edx/frontend-platform/i18n';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import messages from './messages';
 import { ComponentHierarchy } from '../hierarchy/ComponentHierarchy';
 
-export const ComponentUsageTab = () => {
-  const intl = useIntl();
-
-  return (
-    <>
-      <h4>{intl.formatMessage(messages.usageTabHierarchyHeading)}</h4>
-      <ComponentHierarchy />
-    </>
-  );
-};
+export const ComponentUsageTab = () => (
+  <>
+    <h4>
+      <FormattedMessage {...messages.usageTabHierarchyHeading} />
+    </h4>
+    <ComponentHierarchy />
+  </>
+);
--- src/library-authoring/generic/publish-status-buttons/PublishedChip.tsx
+++ src/library-authoring/generic/publish-status-buttons/PublishedChip.tsx
@@ -1,16 +1,12 @@
-import { useIntl } from '@edx/frontend-platform/i18n';
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import { Container } from '@openedx/paragon';
 
 import messages from './messages';
 
-export const PublishedChip = () => {
-  const intl = useIntl();
-
-  return (
-    <Container
-      className="p-2 text-nowrap flex-grow-1 status-button published-status font-weight-bold"
-    >
-      {intl.formatMessage(messages.publishedChipText)}
-    </Container>
-  );
-};
+export const PublishedChip = () => (
+  <Container
+    className="p-2 text-nowrap flex-grow-1 status-button published-status font-weight-bold"
+  >
+    <FormattedMessage {...messages.publishedChipText} />
+  </Container>
+);
