1c32d057e2fc6e2aee291e7cad21b0c427d97d79
Dang Ninh <80091676+ninh-nd@users.noreply.github.com>
2025-09-25 15:30:39 +0700

    [META-DEMO] Correct design of Button (#13)
    * style: update brand config color
    
    * style: update button style to design

--- src/shared/Components/Common/Button.tsx
+++ src/shared/Components/Common/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
   name?: string;
   id?: string;
   type?: string;
-  variant?: 'brand' | 'link' | 'secondary' | 'tertiary';
+  variant?: 'brand' | 'link' | 'secondary' | 'secondaryGray' | 'tertiary';
   state?: string;
   labels?: Record<string, React.ReactNode>;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
@@ -17,42 +17,92 @@ interface ButtonProps {
   iconBefore?: React.FC;
   iconAfter?: React.FC;
   disabled?: boolean;
-  size?: 'sm' | 'md' | 'lg';
+  size?: 'xs' | 'sm' | 'md' | 'lg';
 }
 
 const buttonVariants = cva(
-  'tw-font-semibold focus:before:tw-hidden focus:!tw-outline-none focus:!tw-ring-0 !tw-m-0',
+  'tw-inline-flex tw-items-center tw-justify-center focus:before:tw-hidden focus:!tw-outline-none focus:!tw-ring-0 !tw-m-0',
   {
     variants: {
       size: {
-        sm: 'tw-text-[14px]',
-        md: 'tw-text-[16px]',
-        lg: 'tw-text-[18px]',
+        xs: 'tw-text-xs tw-font-medium tw-py-[3px] tw-px-[8px] tw-gap-1',
+        sm: 'tw-text-sm tw-font-semibold tw-py-[6px] tw-px-[10px] tw-gap-1',
+        md: 'tw-text-sm tw-font-semibold tw-py-[10px] tw-px-[14px] tw-gap-1',
+        lg: 'tw-text-md tw-font-semibold tw-py-[10px] tw-px-4 tw-gap-[6px]',
       },
       variant: {
         brand: [
-          'login-button-width',
-          'tw-py-[10px]',
-          'tw-px-[16px]',
           'tw-w-full',
           'tw-rounded-[100px]',
           'tw-bg-brand-600',
-          'hover:tw-bg-brand-700',
-          'active:!tw-bg-brand-700',
+          'tw-text-white',
           'tw-border-0',
+          'tw-shadow-xs',
+          // Normal state
+          'tw-cursor-pointer',
+          // Hover state
+          'hover:enabled:tw-bg-brand-700',
+          'hover:enabled:tw-shadow-lg',
+          // Focus state
+          'focus:enabled:tw-bg-brand-600',
+          'focus:enabled:tw-shadow-[0px_0px_0px_2px_#ffffff,0px_0px_0px_4px_#875bf7]',
+          // Disabled state
+          'disabled:tw-bg-gray-100',
+          'disabled:tw-text-gray-400',
+          'disabled:tw-border-gray-200',
+          'disabled:tw-border',
+          'disabled:tw-cursor-not-allowed',
         ],
-        link: ['tw-text-center', 'tw-text-brand-700', 'hover:tw-text-brand-600'],
-        secondary: [
-          'tw-py-[10px]',
-          'tw-px-[16px]',
+        secondaryGray: [
           'tw-w-full',
           'tw-rounded-[100px]',
           'tw-bg-white',
-          'tw-text-brand-600',
-          'tw-border-brand-600',
-          'tw-border-1',
-          'hover:tw-bg-brand-600',
-          'active:!tw-bg-brand-700',
+          'tw-text-gray-700',
+          'tw-border-gray-300',
+          'tw-border',
+          'tw-shadow-xs',
+          // Normal state
+          'tw-cursor-pointer',
+          // Hover state
+          'hover:enabled:tw-bg-gray-50',
+          'hover:enabled:tw-text-gray-800',
+          'hover:enabled:tw-border-gray-300',
+          // Focus state
+          'focus:enabled:tw-bg-white',
+          'focus:enabled:tw-text-gray-700',
+          'focus:enabled:tw-border-gray-300',
+          'focus:enabled:tw-shadow-[0px_0px_0px_2px_#ffffff,0px_0px_0px_4px_#875bf7]',
+          // Disabled state
+          'disabled:tw-bg-white',
+          'disabled:tw-text-gray-400',
+          'disabled:tw-border-gray-200',
+          'disabled:tw-cursor-not-allowed',
+        ],
+        link: ['tw-text-center', 'tw-text-brand-700', 'hover:tw-text-brand-600'],
+        secondary: [
+          'tw-w-full',
+          'tw-rounded-[100px]',
+          'tw-bg-white',
+          'tw-text-brand-700',
+          'tw-border-violet-300',
+          'tw-border',
+          'tw-shadow-xs',
+          // Normal state
+          'tw-cursor-pointer',
+          // Hover state
+          'hover:enabled:tw-bg-violet-50',
+          'hover:enabled:tw-text-brand-800',
+          'hover:enabled:tw-border-violet-300',
+          // Focus state
+          'focus:enabled:tw-bg-white',
+          'focus:enabled:tw-text-brand-700',
+          'focus:enabled:tw-border-violet-300',
+          'focus:enabled:tw-shadow-[0px_0px_0px_2px_#ffffff,0px_0px_0px_4px_#875bf7]',
+          // Disabled state
+          'disabled:tw-bg-white',
+          'disabled:tw-text-gray-400',
+          'disabled:tw-border-gray-200',
+          'disabled:tw-cursor-not-allowed',
         ],
         tertiary: [
           'tw-py-[10px]',
