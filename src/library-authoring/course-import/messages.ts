d7c6081feb44926e3c3a4b590ed45acaecc538ce
XnpioChV <xnpiochv@gmail.com>
2025-10-23 19:09:22 -0500

    feat: Import course modal created

---
+++ src/library-authoring/course-import/messages.ts
@@ -0,0 +1,41 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  importCourseModalTitle: {
+    id: 'library-authoring.import-course.modal.title',
+    defaultMessage: 'Import Course to Library',
+    description: 'Title for the modal to import a course into a library.',
+  },
+  importCourseButton: {
+    id: 'library-authoring.import-course.button.text',
+    defaultMessage: 'Import Course',
+    description: 'Label of the button to open the modal to import a course into a library.',
+  },
+  importCourseSelectCourseStep: {
+    id: 'library-authoring.import-course.select-course.title',
+    defaultMessage: 'Select Course',
+    description: 'Title for the step to select course in the modal to import a course into a library.',
+  },
+  importCourseReviewDetailsStep: {
+    id: 'library-authoring.import-course.review-details.title',
+    defaultMessage: 'Review Import Details',
+    description: 'Title for the step to review import details in the modal to import a course into a library.',
+  },
+  importCourseCalcel: {
+    id: 'library-authoring.import-course.cancel.text',
+    defaultMessage: 'Cancel',
+    description: 'Label of the button to cancel the course import.',
+  },
+  importCourseNext: {
+    id: 'library-authoring.import-course.next.text',
+    defaultMessage: 'Next step',
+    description: 'Label of the button go to the next step in the course import modal.',
+  },
+  importCourseBack: {
+    id: 'library-authoring.import-course.back.text',
+    defaultMessage: 'Back',
+    description: 'Label of the button to go to the previous step in the course import modal.',
+  },
+});
+
+export default messages;
