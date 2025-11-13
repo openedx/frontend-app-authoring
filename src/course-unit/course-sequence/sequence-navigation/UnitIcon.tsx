216403459723ae13e08d2782a97c7fa19ce92f24
Braden MacDonald <braden@opencraft.com>
2025-11-13 12:13:36 -0500

    refactor: convert <UnitButton> and <UnitIcon> to TSX

--- src/course-unit/course-sequence/sequence-navigation/UnitIcon.tsx
+++ src/course-unit/course-sequence/sequence-navigation/UnitIcon.tsx
@@ -1,17 +1,17 @@
-import PropTypes from 'prop-types';
+import { type FC } from 'react';
 import { Icon } from '@openedx/paragon';
 import { BookOpen as BookOpenIcon } from '@openedx/paragon/icons';
 
-import { UNIT_TYPE_ICONS_MAP, UNIT_ICON_TYPES } from '../../../generic/block-type-utils/constants';
+import { UNIT_TYPE_ICONS_MAP } from '@src/generic/block-type-utils/constants';
 
-const UnitIcon = ({ type }) => {
+interface Props {
+  type: keyof typeof UNIT_TYPE_ICONS_MAP;
+}
+
+const UnitIcon: FC<Props> = ({ type }) => {
   const icon = UNIT_TYPE_ICONS_MAP[type] || BookOpenIcon;
 
   return <Icon src={icon} screenReaderText={type} />;
-};
-
-UnitIcon.propTypes = {
-  type: PropTypes.oneOf(UNIT_ICON_TYPES).isRequired,
 };
 
 export default UnitIcon;
