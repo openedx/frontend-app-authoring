a92e5213e7125714b05781de721f406bb16aa6ca
Navin Karkera <navin@opencraft.com>
2025-12-04 18:36:15 +0530

    feat: new header action buttons

--- src/plugin-slots/CourseOutlineHeaderActionsSlot/index.tsx
+++ src/plugin-slots/CourseOutlineHeaderActionsSlot/index.tsx
@@ -1,6 +1,8 @@
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
+import { getConfig } from '@edx/frontend-platform';
 
 import HeaderNavigations, { HeaderNavigationsProps } from 'CourseAuthoring/course-outline/header-navigations/HeaderNavigations';
+import HeaderActions from 'CourseAuthoring/course-outline/header-navigations/HeaderActions';
 
 interface CourseOutlineHeaderActionsSlotProps extends HeaderNavigationsProps {
   sections: Array<({
@@ -18,31 +20,44 @@ const CourseOutlineHeaderActionsSlot = ({
   courseActions,
   errors,
   sections,
-}: CourseOutlineHeaderActionsSlotProps) => (
-  <PluginSlot
-    id="org.openedx.frontend.authoring.course_outline_header_actions.v1"
-    idAliases={['course_outline_header_actions_slot']}
-    pluginProps={{
-      isReIndexShow,
-      isSectionsExpanded,
-      isDisabledReindexButton,
-      headerNavigationsActions,
-      hasSections,
-      courseActions,
-      errors,
-      sections,
-    }}
-  >
-    <HeaderNavigations
-      headerNavigationsActions={headerNavigationsActions}
-      isReIndexShow={isReIndexShow}
-      isSectionsExpanded={isSectionsExpanded}
-      isDisabledReindexButton={isDisabledReindexButton}
-      hasSections={hasSections}
-      courseActions={courseActions}
-      errors={errors}
-    />
-  </PluginSlot>
-);
+}: CourseOutlineHeaderActionsSlotProps) => {
+  const showNewActionsBar = getConfig().ENABLE_COURSE_OUTLINE_NEW_DESIGN?.toString().toLowerCase() === 'true';
+  return (
+    <PluginSlot
+      id="org.openedx.frontend.authoring.course_outline_header_actions.v1"
+      idAliases={['course_outline_header_actions_slot']}
+      pluginProps={{
+        isReIndexShow,
+        isSectionsExpanded,
+        isDisabledReindexButton,
+        headerNavigationsActions,
+        hasSections,
+        courseActions,
+        errors,
+        sections,
+      }}
+    >
+      {showNewActionsBar
+        ? (
+          <HeaderActions
+            actions={headerNavigationsActions}
+            courseActions={courseActions}
+            errors={errors}
+          />
+        )
+        : (
+          <HeaderNavigations
+            headerNavigationsActions={headerNavigationsActions}
+            isReIndexShow={isReIndexShow}
+            isSectionsExpanded={isSectionsExpanded}
+            isDisabledReindexButton={isDisabledReindexButton}
+            hasSections={hasSections}
+            courseActions={courseActions}
+            errors={errors}
+          />
+        )}
+    </PluginSlot>
+  );
+};
 
 export default CourseOutlineHeaderActionsSlot;
