903df9bffa980ff5c9de42afc8611b92d35d2f53
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-12-11 12:09:38 +0500

    fix: js to ts

---
+++ src/plugin-slots/PageBannerSlot/index.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode } from 'react';
+import { PluginSlot } from '@openedx/frontend-plugin-framework';
+import { PageBanner } from '@openedx/paragon';
+
+export interface PageBannerSlotProps {
+  show?: boolean;
+  dismissible?: boolean;
+  onDismiss?: () => void;
+  className?: string;
+  children?: ReactNode;
+}
+
+const PageBannerSlot: React.FC<PageBannerSlotProps> = ({
+  show,
+  dismissible,
+  onDismiss,
+  className,
+  children,
+}) => (
+  <PluginSlot
+    id="org.openedx.frontend.authoring.page_banner.v1"
+    idAliases={["page_banner_slot"]}
+    pluginProps={{ show, dismissible, onDismiss, className }}
+  >
+    <PageBanner
+      show={show}
+      dismissible={dismissible}
+      onDismiss={onDismiss}
+      className={className}
+    >
+      {children}
+    </PageBanner>
+  </PluginSlot>
+);
+
+export default PageBannerSlot;
