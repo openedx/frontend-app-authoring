db5a22a920fc89309df6e06084746ccfb4157874
Navin Karkera <navin@opencraft.com>
2025-12-26 16:27:54 +0530

    feat: add new tab section in sidebar

---
+++ src/library-authoring/library-filters/messages.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  librariesFilterBtnText: {
+    id: 'course-authoring.library-authoring.library-filters.libraries.filter.btn',
+    defaultMessage: 'All libraries',
+    description: 'Button text for libraries filter',
+  },
+  additionalFilterBtnAltText: {
+    id: 'course-authoring.library-authoring.library-filters.additional.filter.alt-btn',
+    defaultMessage: 'See more',
+    description: 'Alt text for more fitlers',
+  },
+});
+
+export default messages;
