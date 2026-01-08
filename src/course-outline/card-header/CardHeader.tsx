b83935d0c4669ad1f21a7b0fac4069283530e296
XnpioChV <xnpiochv@gmail.com>
2026-01-08 15:48:29 -0500

    feat: Add legacy align sidebar

--- src/course-outline/card-header/CardHeader.tsx
+++ src/course-outline/card-header/CardHeader.tsx
@@ -11,6 +11,7 @@ import {
   Icon,
   IconButton,
   IconButtonWithTooltip,
+  useToggle,
 } from '@openedx/paragon';
 import {
   MoreVert as MoveVertIcon,
@@ -28,6 +29,7 @@ import { scrollToElement } from '../utils';
 import CardStatus from './CardStatus';
 import messages from './messages';
 import { useOutlineSidebarContext } from '../outline-sidebar/OutlineSidebarContext';
+import { ContentTagsDrawerSheet } from '@src/content-tags-drawer';
 
 interface CardHeaderProps {
   title: string;
@@ -108,11 +110,17 @@ const CardHeader = ({
   const [searchParams] = useSearchParams();
   const [titleValue, setTitleValue] = useState(title);
   const cardHeaderRef = useRef(null);
+  const [isLegacyManageTagsDrawerOpen, openLegacyTagsDrawer, closeLegacyTagsDrawer] = useToggle(false);
   const { setCurrentPageKey } = useOutlineSidebarContext();
 
   const openManageTagsDrawer = useCallback(() => {
-    setCurrentPageKey('align', { contentId: cardId });
-  }, [setCurrentPageKey, cardId]);
+    const showNewSidebar = getConfig().ENABLE_COURSE_OUTLINE_NEW_DESIGN?.toString().toLowerCase() === 'true';
+    if (showNewSidebar) {
+      setCurrentPageKey('align', { contentId: cardId });
+    } else {
+      openLegacyTagsDrawer();
+    }
+  }, [setCurrentPageKey, openLegacyTagsDrawer, cardId]);
 
   // Use studio url as base if proctoringExamConfigurationLink is a relative link
   const fullProctoringExamConfigurationLink = () => (
@@ -156,162 +164,169 @@ const CardHeader = ({
   });
 
   return (
-    <div
-      className="item-card-header"
-      data-testid={`${namePrefix}-card-header`}
-      ref={cardHeaderRef}
-    >
-      {isFormOpen ? (
-        <Form.Group className="m-0 w-75">
-          <Form.Control
-            data-testid={`${namePrefix}-edit-field`}
-            ref={(e) => e && e.focus()}
-            value={titleValue}
-            name="displayName"
-            onChange={(e) => setTitleValue(e.target.value)}
-            aria-label={intl.formatMessage(messages.editFieldAriaLabel)}
-            onBlur={() => onEditSubmit(titleValue)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                onEditSubmit(titleValue);
-              }
-            }}
-            disabled={isSaving}
-          />
-        </Form.Group>
-      ) : (
-        <>
-          {titleComponent}
+    <>
+      <div
+        className="item-card-header"
+        data-testid={`${namePrefix}-card-header`}
+        ref={cardHeaderRef}
+      >
+        {isFormOpen ? (
+          <Form.Group className="m-0 w-75">
+            <Form.Control
+              data-testid={`${namePrefix}-edit-field`}
+              ref={(e) => e && e.focus()}
+              value={titleValue}
+              name="displayName"
+              onChange={(e) => setTitleValue(e.target.value)}
+              aria-label={intl.formatMessage(messages.editFieldAriaLabel)}
+              onBlur={() => onEditSubmit(titleValue)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  onEditSubmit(titleValue);
+                }
+              }}
+              disabled={isSaving}
+            />
+          </Form.Group>
+        ) : (
+          <>
+            {titleComponent}
+            <IconButtonWithTooltip
+              className="item-card-button-icon"
+              data-testid={`${namePrefix}-edit-button`}
+              alt={intl.formatMessage(messages.altButtonRename)}
+              tooltipContent={<div>{intl.formatMessage(messages.altButtonRename)}</div>}
+              iconAs={EditIcon}
+              onClick={onClickEdit}
+                // @ts-ignore
+              disabled={isSaving}
+            />
+          </>
+        )}
+        <div className="ml-auto d-flex">
+          {(isVertical || isSequential) && (
+          <CardStatus status={status} showDiscussionsEnabledBadge={showDiscussionsEnabledBadge || false} />
+          )}
+          { getConfig().ENABLE_TAGGING_TAXONOMY_PAGES === 'true' && !!contentTagCount && (
+          <TagCount count={contentTagCount} onClick={openManageTagsDrawer} />
+          )}
+          {extraActionsComponent}
+          {readyToSync && (
           <IconButtonWithTooltip
-            className="item-card-button-icon"
-            data-testid={`${namePrefix}-edit-button`}
-            alt={intl.formatMessage(messages.altButtonRename)}
-            tooltipContent={<div>{intl.formatMessage(messages.altButtonRename)}</div>}
-            iconAs={EditIcon}
-            onClick={onClickEdit}
-              // @ts-ignore
-            disabled={isSaving}
+            data-testid={`${namePrefix}-sync-button`}
+            alt={intl.formatMessage(messages.readyToSyncButtonAlt)}
+            iconAs={SyncIcon}
+            tooltipContent={<div>{intl.formatMessage(messages.readyToSyncButtonAlt)}</div>}
+            onClick={onClickSync}
           />
-        </>
-      )}
-      <div className="ml-auto d-flex">
-        {(isVertical || isSequential) && (
-        <CardStatus status={status} showDiscussionsEnabledBadge={showDiscussionsEnabledBadge || false} />
-        )}
-        { getConfig().ENABLE_TAGGING_TAXONOMY_PAGES === 'true' && !!contentTagCount && (
-        <TagCount count={contentTagCount} onClick={openManageTagsDrawer} />
-        )}
-        {extraActionsComponent}
-        {readyToSync && (
-        <IconButtonWithTooltip
-          data-testid={`${namePrefix}-sync-button`}
-          alt={intl.formatMessage(messages.readyToSyncButtonAlt)}
-          iconAs={SyncIcon}
-          tooltipContent={<div>{intl.formatMessage(messages.readyToSyncButtonAlt)}</div>}
-          onClick={onClickSync}
-        />
-        )}
-        <Dropdown data-testid={`${namePrefix}-card-header__menu`} onClick={onClickMenuButton}>
-          <Dropdown.Toggle
-            className="item-card-header__menu"
-            id={`${namePrefix}-card-header__menu`}
-            data-testid={`${namePrefix}-card-header__menu-button`}
-            as={IconButton}
-            src={MoveVertIcon}
-            alt={`${namePrefix}-card-header__menu`}
-            iconAs={Icon}
-          />
-          <Dropdown.Menu>
-            {isSequential && proctoringExamConfigurationLink && (
-            <Dropdown.Item
-              as={Hyperlink}
-              target="_blank"
-              destination={fullProctoringExamConfigurationLink()}
-              href={fullProctoringExamConfigurationLink()}
-              externalLinkTitle={intl.formatMessage(messages.proctoringLinkTooltip)}
-            >
-              {intl.formatMessage(messages.menuProctoringLinkText)}
-            </Dropdown.Item>
-            )}
-            <Dropdown.Item
-              data-testid={`${namePrefix}-card-header__menu-publish-button`}
-              disabled={isDisabledPublish}
-              onClick={onClickPublish}
-            >
-              {intl.formatMessage(messages.menuPublish)}
-            </Dropdown.Item>
-            <Dropdown.Item
-              data-testid={`${namePrefix}-card-header__menu-configure-button`}
-              disabled={isSaving}
-              onClick={onClickConfigure}
-            >
-              {intl.formatMessage(messages.menuConfigure)}
-            </Dropdown.Item>
-            {getConfig().ENABLE_TAGGING_TAXONOMY_PAGES === 'true' && (
-            <Dropdown.Item
-              data-testid={`${namePrefix}-card-header__menu-manage-tags-button`}
-              disabled={isSaving}
-              onClick={openManageTagsDrawer}
-            >
-              {intl.formatMessage(messages.menuManageTags)}
-            </Dropdown.Item>
-            )}
+          )}
+          <Dropdown data-testid={`${namePrefix}-card-header__menu`} onClick={onClickMenuButton}>
+            <Dropdown.Toggle
+              className="item-card-header__menu"
+              id={`${namePrefix}-card-header__menu`}
+              data-testid={`${namePrefix}-card-header__menu-button`}
+              as={IconButton}
+              src={MoveVertIcon}
+              alt={`${namePrefix}-card-header__menu`}
+              iconAs={Icon}
+            />
+            <Dropdown.Menu>
+              {isSequential && proctoringExamConfigurationLink && (
+              <Dropdown.Item
+                as={Hyperlink}
+                target="_blank"
+                destination={fullProctoringExamConfigurationLink()}
+                href={fullProctoringExamConfigurationLink()}
+                externalLinkTitle={intl.formatMessage(messages.proctoringLinkTooltip)}
+              >
+                {intl.formatMessage(messages.menuProctoringLinkText)}
+              </Dropdown.Item>
+              )}
+              <Dropdown.Item
+                data-testid={`${namePrefix}-card-header__menu-publish-button`}
+                disabled={isDisabledPublish}
+                onClick={onClickPublish}
+              >
+                {intl.formatMessage(messages.menuPublish)}
+              </Dropdown.Item>
+              <Dropdown.Item
+                data-testid={`${namePrefix}-card-header__menu-configure-button`}
+                disabled={isSaving}
+                onClick={onClickConfigure}
+              >
+                {intl.formatMessage(messages.menuConfigure)}
+              </Dropdown.Item>
+              {getConfig().ENABLE_TAGGING_TAXONOMY_PAGES === 'true' && (
+              <Dropdown.Item
+                data-testid={`${namePrefix}-card-header__menu-manage-tags-button`}
+                disabled={isSaving}
+                onClick={openManageTagsDrawer}
+              >
+                {intl.formatMessage(messages.menuManageTags)}
+              </Dropdown.Item>
+              )}
 
-            {isVertical && enableCopyPasteUnits && (
-            <Dropdown.Item onClick={onClickCopy}>
-              {intl.formatMessage(messages.menuCopy)}
-            </Dropdown.Item>
-            )}
-            {actions.duplicable && (
-            <Dropdown.Item
-              data-testid={`${namePrefix}-card-header__menu-duplicate-button`}
-              onClick={onClickDuplicate}
-            >
-              {intl.formatMessage(messages.menuDuplicate)}
-            </Dropdown.Item>
-            )}
-            {actions.draggable && (
-            <>
-              <Dropdown.Item
-                data-testid={`${namePrefix}-card-header__menu-move-up-button`}
-                onClick={onClickMoveUp}
-                disabled={!actions.allowMoveUp}
-              >
-                {intl.formatMessage(messages.menuMoveUp)}
+              {isVertical && enableCopyPasteUnits && (
+              <Dropdown.Item onClick={onClickCopy}>
+                {intl.formatMessage(messages.menuCopy)}
               </Dropdown.Item>
+              )}
+              {actions.duplicable && (
               <Dropdown.Item
-                data-testid={`${namePrefix}-card-header__menu-move-down-button`}
-                onClick={onClickMoveDown}
-                disabled={!actions.allowMoveDown}
+                data-testid={`${namePrefix}-card-header__menu-duplicate-button`}
+                onClick={onClickDuplicate}
               >
-                {intl.formatMessage(messages.menuMoveDown)}
+                {intl.formatMessage(messages.menuDuplicate)}
               </Dropdown.Item>
-            </>
-            )}
-            {((actions.unlinkable ?? null) !== null || actions.deletable) && <Dropdown.Divider />}
-            {(actions.unlinkable ?? null) !== null && (
-            <Dropdown.Item
-              data-testid={`${namePrefix}-card-header__menu-unlink-button`}
-              onClick={onClickUnlink}
-              disabled={!actions.unlinkable}
-              className="allow-hover-on-disabled"
-              title={!actions.unlinkable ? intl.formatMessage(messages.menuUnlinkDisabledTooltip) : undefined}
-            >
-              {intl.formatMessage(messages.menuUnlink)}
-            </Dropdown.Item>
-            )}
-            {actions.deletable && (
-            <Dropdown.Item
-              data-testid={`${namePrefix}-card-header__menu-delete-button`}
-              onClick={onClickDelete}
-            >
-              {intl.formatMessage(messages.menuDelete)}
-            </Dropdown.Item>
-            )}
-          </Dropdown.Menu>
-        </Dropdown>
+              )}
+              {actions.draggable && (
+              <>
+                <Dropdown.Item
+                  data-testid={`${namePrefix}-card-header__menu-move-up-button`}
+                  onClick={onClickMoveUp}
+                  disabled={!actions.allowMoveUp}
+                >
+                  {intl.formatMessage(messages.menuMoveUp)}
+                </Dropdown.Item>
+                <Dropdown.Item
+                  data-testid={`${namePrefix}-card-header__menu-move-down-button`}
+                  onClick={onClickMoveDown}
+                  disabled={!actions.allowMoveDown}
+                >
+                  {intl.formatMessage(messages.menuMoveDown)}
+                </Dropdown.Item>
+              </>
+              )}
+              {((actions.unlinkable ?? null) !== null || actions.deletable) && <Dropdown.Divider />}
+              {(actions.unlinkable ?? null) !== null && (
+              <Dropdown.Item
+                data-testid={`${namePrefix}-card-header__menu-unlink-button`}
+                onClick={onClickUnlink}
+                disabled={!actions.unlinkable}
+                className="allow-hover-on-disabled"
+                title={!actions.unlinkable ? intl.formatMessage(messages.menuUnlinkDisabledTooltip) : undefined}
+              >
+                {intl.formatMessage(messages.menuUnlink)}
+              </Dropdown.Item>
+              )}
+              {actions.deletable && (
+              <Dropdown.Item
+                data-testid={`${namePrefix}-card-header__menu-delete-button`}
+                onClick={onClickDelete}
+              >
+                {intl.formatMessage(messages.menuDelete)}
+              </Dropdown.Item>
+              )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       </div>
-    </div>
+      <ContentTagsDrawerSheet
+        id={cardId}
+        onClose={/* istanbul ignore next */ () => closeLegacyTagsDrawer()}
+        showSheet={isLegacyManageTagsDrawerOpen}
+      />
+    </>
   );
 };
 
