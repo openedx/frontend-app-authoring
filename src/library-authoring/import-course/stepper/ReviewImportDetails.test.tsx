82e24193a8221f28470ce718ec171466db621ce3
Chris Chávez <xnpiochv@gmail.com>
2026-01-20 14:44:35 -0500

    refactor: Use preview migration API & feat: Block import when the import would exceed the block limit [FC-0112] (#2700)
    - Use the new preview migration API implemented in https://github.com/openedx/edx-platform/pull/37818
    - Clean the code for the preview.
    - Implements the commented in https://github.com/openedx/frontend-app-authoring/issues/2525#issuecomment-3554310315

--- src/library-authoring/import-course/stepper/ReviewImportDetails.test.tsx
+++ src/library-authoring/import-course/stepper/ReviewImportDetails.test.tsx
@@ -1,15 +1,15 @@
 import { useCourseDetails } from '@src/course-outline/data/apiHooks';
 import { useMigrationInfo } from '@src/library-authoring/data/apiHooks';
-import { useGetBlockTypes, useGetContentHits } from '@src/search-manager';
 import { render as baseRender, screen, initializeMocks } from '@src/testUtils';
 import { LibraryProvider } from '@src/library-authoring/common/context/LibraryContext';
 import { mockContentLibrary } from '@src/library-authoring/data/api.mocks';
+import { mockGetPreviewModulestoreMigration } from '@src/data/api.mocks';
 import { ReviewImportDetails } from './ReviewImportDetails';
 import messages from '../messages';
 
 mockContentLibrary.applyMock();
+mockGetPreviewModulestoreMigration.applyMock();
 const { libraryId } = mockContentLibrary;
-const markAnalysisComplete = jest.fn();
 
 // Mock the useCourseDetails hook
 jest.mock('@src/course-outline/data/apiHooks', () => ({
@@ -20,12 +20,6 @@ jest.mock('@src/course-outline/data/apiHooks', () => ({
 jest.mock('@src/library-authoring/data/apiHooks', () => ({
   useMigrationInfo: jest.fn().mockReturnValue({ isPending: true, data: null }),
   useContentLibrary: jest.fn().mockReturnValue({}),
-}));
-
-// Mock the useGetBlockTypes hook
-jest.mock('@src/search-manager', () => ({
-  useGetBlockTypes: jest.fn().mockReturnValue({ isPending: true, data: null }),
-  useGetContentHits: jest.fn().mockReturnValue({ isPending: true, data: null }),
 }));
 
 const render = (element: React.ReactElement) => {
@@ -49,11 +43,10 @@ describe('ReviewImportDetails', () => {
   });
 
   it('renders loading spinner when isPending is true', async () => {
-    render(<ReviewImportDetails markAnalysisComplete={markAnalysisComplete} courseId="test-course-id" />);
+    render(<ReviewImportDetails courseId="test-course-id" />);
 
     const spinners = await screen.findAllByRole('status');
     spinners.every((spinner) => expect(spinner.textContent).toEqual('Loading...'));
-    expect(markAnalysisComplete).toHaveBeenCalledWith(false);
   });
 
   it('renders import progress status when isBlockDataPending or migrationInfoIsPending is true', async () => {
@@ -63,30 +56,26 @@ describe('ReviewImportDetails', () => {
       data: null,
     });
 
-    render(<ReviewImportDetails markAnalysisComplete={markAnalysisComplete} courseId="test-course-id" />);
+    render(<ReviewImportDetails courseId="test-course-id" />);
 
     expect(await screen.findByRole('alert')).toBeInTheDocument();
     expect(await screen.findByText(/Import Analysis in Progress/i)).toBeInTheDocument();
-    expect(markAnalysisComplete).toHaveBeenCalledWith(false);
   });
 
   it('renders warning when reimport', async () => {
+    const courseKey = mockGetPreviewModulestoreMigration.sourceKeyGood;
     (useCourseDetails as jest.Mock).mockReturnValue({ isPending: false, data: { title: 'Test Course' } });
     (useMigrationInfo as jest.Mock).mockReturnValue({
       isPending: false,
       data: {
-        'test-course-id': [{
+        courseKey: [{
           targetKey: libraryId,
           targetTitle: 'Library title',
         }],
       },
     });
-    (useGetBlockTypes as jest.Mock).mockReturnValueOnce({
-      isPending: false,
-      data: { html: 1 },
-    });
 
-    render(<ReviewImportDetails markAnalysisComplete={markAnalysisComplete} courseId="test-course-id" />);
+    render(<ReviewImportDetails courseId={courseKey} />);
 
     expect(await screen.findByRole('alert')).toBeInTheDocument();
     expect(await screen.findByText(/Import Analysis Completed: Reimport/i)).toBeInTheDocument();
@@ -95,7 +84,6 @@ describe('ReviewImportDetails', () => {
         .replace('{courseName}', 'Test Course')
         .replace('{libraryName}', 'Library title'),
     )).toBeInTheDocument();
-    expect(markAnalysisComplete).toHaveBeenCalledWith(true);
   });
 
   it('renders warning when unsupportedBlockPercentage > 0', async () => {
@@ -104,85 +92,44 @@ describe('ReviewImportDetails', () => {
       isPending: false,
       data: null,
     });
-    (useGetBlockTypes as jest.Mock).mockReturnValueOnce({
-      isPending: false,
-      data: {
-        chapter: 1,
-        sequential: 2,
-        vertical: 3,
-        'problem-builder': 1,
-        html: 1,
-      },
-    });
 
-    render(<ReviewImportDetails markAnalysisComplete={markAnalysisComplete} courseId="test-course-id" />);
+    render(<ReviewImportDetails
+      courseId={mockGetPreviewModulestoreMigration.sourceKeyUnsupported}
+    />);
 
     expect(await screen.findByRole('alert')).toBeInTheDocument();
     expect(await screen.findByText(/Import Analysis Complete/i)).toBeInTheDocument();
     expect(await screen.findByText(
-      /88% of course content will be imported into a collection in your library called Test Course. Some content will not be imported. For details see below./i,
+      /75% of course content will be imported into a collection in your library called Test Course. Some content will not be imported. For details see below./i,
     )).toBeInTheDocument();
     expect(await screen.findByText(/Total Blocks/i)).toBeInTheDocument();
-    expect(await screen.findByText('7/8')).toBeInTheDocument();
+    expect(await screen.findByText('15/20')).toBeInTheDocument();
     expect(await screen.findByText('Sections')).toBeInTheDocument();
-    expect(await screen.findByText('1')).toBeInTheDocument();
-    expect(await screen.findByText('Subsections')).toBeInTheDocument();
     expect(await screen.findByText('2')).toBeInTheDocument();
-    expect(await screen.findByText('Units')).toBeInTheDocument();
+    expect(await screen.findByText('Subsections')).toBeInTheDocument();
     expect(await screen.findByText('3')).toBeInTheDocument();
+    expect(await screen.findByText('Units')).toBeInTheDocument();
+    expect(await screen.findByText('5')).toBeInTheDocument();
     expect(await screen.findByText('Components')).toBeInTheDocument();
-    expect(await screen.findByText('1/2')).toBeInTheDocument();
-    expect(markAnalysisComplete).toHaveBeenCalledWith(true);
+    expect(await screen.findByText('5/10')).toBeInTheDocument();
   });
 
-  it('skips children blocks from total counts', async () => {
+  it('renders warning when components exceed the limit', async () => {
     (useCourseDetails as jest.Mock).mockReturnValue({ isPending: false, data: { title: 'Test Course' } });
     (useMigrationInfo as jest.Mock).mockReturnValue({
       isPending: false,
       data: null,
     });
-    (useGetContentHits as jest.Mock).mockReturnValue({
-      isPending: false,
-      data: {
-        hits: [{ usage_key: 'some-usage-key' }],
-        estimatedTotalHits: 1,
-      },
-    });
-    (useGetBlockTypes as jest.Mock).mockReturnValueOnce({
-      isPending: false,
-      data: {
-        chapter: 1,
-        sequential: 2,
-        vertical: 3,
-        library_content: 1,
-        html: 1,
-        problem: 4,
-      },
-    }).mockReturnValueOnce({
-      isPending: false,
-      data: {
-        problem: 2, // should be ignored from total count.
-      },
-    });
 
-    render(<ReviewImportDetails markAnalysisComplete={markAnalysisComplete} courseId="test-course-id" />);
+    render(<ReviewImportDetails
+      courseId={mockGetPreviewModulestoreMigration.sourceKeyBlockLimit}
+    />);
 
     expect(await screen.findByRole('alert')).toBeInTheDocument();
-    expect(await screen.findByText(/Import Analysis Complete/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Import Blocked/i)).toBeInTheDocument();
     expect(await screen.findByText(
-      /90% of course content will be imported into a collection in your library called Test Course. Some content will not be imported. For details see below./i,
+      /This import would exceed the Content Library limit of 1000 items/i,
     )).toBeInTheDocument();
-    expect(await screen.findByText(/Total Blocks/i)).toBeInTheDocument();
-    expect(await screen.findByText('9/10')).toBeInTheDocument();
-    expect(await screen.findByText('Sections')).toBeInTheDocument();
-    expect(await screen.findByText('1')).toBeInTheDocument();
-    expect(await screen.findByText('Subsections')).toBeInTheDocument();
-    expect(await screen.findByText('2')).toBeInTheDocument();
-    expect(await screen.findByText('Units')).toBeInTheDocument();
-    expect(await screen.findByText('3')).toBeInTheDocument();
-    expect(await screen.findByText('Components')).toBeInTheDocument();
-    expect(await screen.findByText('3/4')).toBeInTheDocument();
-    expect(markAnalysisComplete).toHaveBeenCalledWith(true);
   });
 
   it('renders success alert when no unsupported blocks', async () => {
@@ -191,18 +138,10 @@ describe('ReviewImportDetails', () => {
       isPending: false,
       data: null,
     });
-    (useGetBlockTypes as jest.Mock).mockReturnValueOnce({
-      isPending: false,
-      data: {
-        chapter: 1,
-        sequential: 2,
-        vertical: 3,
-        html: 5,
-        problem: 3,
-      },
-    });
 
-    render(<ReviewImportDetails markAnalysisComplete={markAnalysisComplete} courseId="test-course-id" />);
+    render(<ReviewImportDetails
+      courseId={mockGetPreviewModulestoreMigration.sourceKeyGood}
+    />);
 
     expect(await screen.findByRole('alert')).toBeInTheDocument();
     expect(await screen.findByText(
@@ -210,7 +149,7 @@ describe('ReviewImportDetails', () => {
         .replace('{courseName}', 'Test Course'),
     )).toBeInTheDocument();
     expect(await screen.findByText(/Total Blocks/i)).toBeInTheDocument();
-    expect(await screen.findByText('14')).toBeInTheDocument();
+    expect(await screen.findByText('10')).toBeInTheDocument();
     expect(await screen.findByText('Sections')).toBeInTheDocument();
     expect(await screen.findByText('1')).toBeInTheDocument();
     expect(await screen.findByText('Subsections')).toBeInTheDocument();
@@ -218,7 +157,6 @@ describe('ReviewImportDetails', () => {
     expect(await screen.findByText('Units')).toBeInTheDocument();
     expect(await screen.findByText('3')).toBeInTheDocument();
     expect(await screen.findByText('Components')).toBeInTheDocument();
-    expect(await screen.findByText('8')).toBeInTheDocument();
-    expect(markAnalysisComplete).toHaveBeenCalledWith(true);
+    expect(await screen.findByText('5')).toBeInTheDocument();
   });
 });
