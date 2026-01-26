ef93e95dd76e3dba0b513b174558500e81dccfed
Chris Chávez <xnpiochv@gmail.com>
2026-01-26 12:52:50 -0500

    feat: New header in course unit page [FC-0114] (#2751)
    - `ENABLE_UNIT_PAGE_NEW_DESIGN` flag created
    - New Status Bard implemented in the header of the course unit page.
    - New buttons added in the header of the course unit page.
    - Which user roles will this change impact? "Course Author".

--- src/course-unit/CourseUnit.tsx
+++ src/course-unit/CourseUnit.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import type { MessageDescriptor } from 'react-intl';
 import {
   Alert, Container, Layout, Button, TransitionReplace,
+  Stack,
+  Badge,
+  Icon,
+  OverlayTrigger,
+  Tooltip,
 } from '@openedx/paragon';
-import { useIntl } from '@edx/frontend-platform/i18n';
+import { FormattedMessage, useIntl } from '@edx/frontend-platform/i18n';
 import {
   Warning as WarningIcon,
   CheckCircle as CheckCircleIcon,
+  Lock,
+  AccessTimeFilled,
+  Groups,
+  QuestionAnswer,
 } from '@openedx/paragon/icons';
 import { useCourseAuthoringContext } from '@src/CourseAuthoringContext';
+import DraftIcon from '@src/generic/DraftIcon';
 import { CourseAuthoringUnitSidebarSlot } from '../plugin-slots/CourseAuthoringUnitSidebarSlot';
 
 import { getProcessingNotification } from '../generic/processing-notification/data/selectors';
@@ -33,6 +44,119 @@ import XBlockContainerIframe from './xblock-container-iframe';
 import MoveModal from './move-modal';
 import IframePreviewLibraryXBlockChanges from './preview-changes';
 import CourseUnitHeaderActionsSlot from '../plugin-slots/CourseUnitHeaderActionsSlot';
+import { UNIT_VISIBILITY_STATES } from './constants';
+import { isUnitPageNewDesignEnabled } from './utils';
+
+const StatusBar = ({ courseUnit }: { courseUnit: any }) => {
+  const { selectedPartitionIndex, selectedGroupsLabel } = courseUnit.userPartitionInfo ?? {};
+  const hasGroups = selectedPartitionIndex !== -1 && !Number.isNaN(selectedPartitionIndex) && selectedGroupsLabel;
+  let groupsCount = 0;
+  if (hasGroups) {
+    groupsCount = selectedGroupsLabel.split(',').length;
+  }
+
+  let visibilityChipData = {
+    variant: 'warning',
+    className: 'draft-badge',
+    text: messages.statusBarDraftNeverPublished,
+    icon: DraftIcon,
+  } as {
+    variant: string,
+    className?: string,
+    text: MessageDescriptor,
+    icon: React.ComponentType,
+  };
+
+  if (courseUnit.currentlyVisibleToStudents) {
+    visibilityChipData = {
+      variant: 'success',
+      text: messages.statusBarLiveBadge,
+      icon: CheckCircleIcon,
+    };
+  } else if (courseUnit.visibilityState === UNIT_VISIBILITY_STATES.staffOnly) {
+    visibilityChipData = {
+      variant: 'secondary',
+      text: messages.statusBarStaffOnly,
+      icon: Lock,
+    };
+  } else if (courseUnit.published) {
+    visibilityChipData = {
+      variant: 'info',
+      text: messages.statusBarScheduledBadge,
+      icon: AccessTimeFilled,
+    };
+  }
+
+  return (
+    <Stack direction="horizontal" gap={3}>
+      <Badge
+        variant={visibilityChipData.variant}
+        className={`px-3 py-2 ${visibilityChipData.className || ''}`}
+      >
+        <Stack direction="horizontal" gap={2}>
+          <Icon size="xs" src={visibilityChipData.icon} />
+          <span className="badge-label">
+            <FormattedMessage {...visibilityChipData.text} />
+          </span>
+        </Stack>
+      </Badge>
+      {courseUnit.published && courseUnit.hasChanges && (
+        <Badge
+          variant="warning"
+          className="px-3 py-2 draft-badge"
+        >
+          <Stack direction="horizontal" gap={1}>
+            <Icon size="xs" src={DraftIcon} />
+            <span className="badge-label">
+              <FormattedMessage {...messages.statusBarDraftChangesBadge} />
+            </span>
+          </Stack>
+        </Badge>
+      )}
+      {groupsCount === 1 && (
+        <Stack direction="horizontal" gap={1}>
+          <Icon src={Groups} />
+          <span>
+            <FormattedMessage
+              {...messages.statusBarGroupAccessOneGroup}
+              values={{
+                groupName: selectedGroupsLabel,
+              }}
+            />
+          </span>
+        </Stack>
+      )}
+      {groupsCount > 1 && (
+        <OverlayTrigger
+          placement="top"
+          overlay={(
+            <Tooltip id="unit-group-access-tooltip">
+              {selectedGroupsLabel}
+            </Tooltip>
+          )}
+        >
+          <Stack direction="horizontal" gap={1}>
+            <Icon src={Groups} />
+            <span>
+              <FormattedMessage
+                {...messages.statusBarGroupAccessMultipleGroup}
+                values={{
+                  groupsCount,
+                }}
+              />
+            </span>
+          </Stack>
+        </OverlayTrigger>
+      )}
+      {courseUnit.discussionEnabled && (
+        <Stack direction="horizontal" gap={1}>
+          <Icon src={QuestionAnswer} />
+          <FormattedMessage {...messages.statusBarDiscussionsEnabled} />
+        </Stack>
+      )}
+    </Stack>
+  );
+};
 
 const CourseUnit = () => {
   const { blockId } = useParams();
@@ -74,6 +198,7 @@ const CourseUnit = () => {
     handleNavigateToTargetUnit,
     addComponentTemplateData,
   } = useCourseUnit({ courseId, blockId });
+
   const layoutGrid = useLayoutGrid(unitCategory, isUnitLibraryType);
 
   const readOnly = !!courseUnit.readOnly;
@@ -121,7 +246,7 @@ const CourseUnit = () => {
                   : intl.formatMessage(messages.alertMoveSuccessDescription, { title: movedXBlockParams.title })}
                 aria-hidden={movedXBlockParams.isSuccess}
                 dismissible
-                actions={movedXBlockParams.isUndo ? null : [
+                actions={movedXBlockParams.isUndo ? undefined : [
                   <Button
                     onClick={handleRollbackMovedXBlock}
                     key="xblock-moved-alert-undo-move-button"
@@ -146,7 +271,6 @@ const CourseUnit = () => {
                 {
                   link: (
                     <Alert.Link
-                      className="ml-1"
                       href={courseUnit.upstreamInfo.upstreamLink}
                     >
                       {intl.formatMessage(messages.alertLibraryUnitReadOnlyLinkText)}
@@ -183,6 +307,11 @@ const CourseUnit = () => {
               />
             )}
           />
+          <div className="unit-header-status-bar h5 mt-2 mb-4 font-weight-normal">
+            {isUnitPageNewDesignEnabled() && isUnitVerticalType && (
+              <StatusBar courseUnit={courseUnit} />
+            )}
+          </div>
           {isUnitVerticalType && (
             <Sequence
               courseId={courseId}
@@ -208,15 +337,17 @@ const CourseUnit = () => {
                   courseId={courseId}
                 />
               )}
-              <XBlockContainerIframe
-                courseId={courseId}
-                blockId={blockId}
-                isUnitVerticalType={isUnitVerticalType}
-                unitXBlockActions={unitXBlockActions}
-                courseVerticalChildren={courseVerticalChildren.children}
-                handleConfigureSubmit={handleConfigureSubmit}
-              />
-              {!readOnly && (
+              {blockId && (
+                <XBlockContainerIframe
+                  courseId={courseId}
+                  blockId={blockId}
+                  isUnitVerticalType={isUnitVerticalType}
+                  unitXBlockActions={unitXBlockActions}
+                  courseVerticalChildren={courseVerticalChildren.children}
+                  handleConfigureSubmit={handleConfigureSubmit}
+                />
+              )}
+              {!readOnly && blockId && (
                 <AddComponent
                   parentLocator={blockId}
                   isSplitTestType={isSplitTestType}
@@ -226,7 +357,7 @@ const CourseUnit = () => {
                   addComponentTemplateData={addComponentTemplateData}
                 />
               )}
-              {!readOnly && showPasteXBlock && canPasteComponent && isUnitVerticalType && (
+              {!readOnly && showPasteXBlock && canPasteComponent && isUnitVerticalType && sharedClipboardData && (
                 <PasteComponent
                   clipboardData={sharedClipboardData}
                   onClick={
@@ -244,15 +375,17 @@ const CourseUnit = () => {
               <IframePreviewLibraryXBlockChanges />
             </Layout.Element>
             <Layout.Element>
-              <CourseAuthoringUnitSidebarSlot
-                courseId={courseId}
-                blockId={blockId}
-                unitTitle={unitTitle}
-                xBlocks={courseVerticalChildren.children}
-                readOnly={readOnly}
-                isUnitVerticalType={isUnitVerticalType}
-                isSplitTestType={isSplitTestType}
-              />
+              {blockId && (
+                <CourseAuthoringUnitSidebarSlot
+                  courseId={courseId}
+                  blockId={blockId}
+                  unitTitle={unitTitle}
+                  xBlocks={courseVerticalChildren.children}
+                  readOnly={readOnly}
+                  isUnitVerticalType={isUnitVerticalType}
+                  isSplitTestType={isSplitTestType}
+                />
+              )}
             </Layout.Element>
           </Layout>
         </section>
--- src/course-unit/messages.ts
+++ src/course-unit/messages.ts
@@ -53,6 +53,46 @@ const messages = defineMessages({
     defaultMessage: 'library',
     description: 'Text of the link in the alert when the unit is read only because is a library unit',
   },
+  statusBarDraftChangesBadge: {
+    id: 'course-authoring.course-unit.status-bar.publish-status.draft-changes',
+    defaultMessage: 'Unpublished changes',
+    description: 'Text for the Draft Changes Badge in the status bar.',
+  },
+  statusBarDiscussionsEnabled: {
+    id: 'course-authoring.course-unit.status-bar.discussions-enabled',
+    defaultMessage: 'Discussions Enabled',
+    description: 'Text for the Discussions enabled Badge in the status bar.',
+  },
+  statusBarDraftNeverPublished: {
+    id: 'course-authoring.course-unit.status-bar.visibility.draft',
+    defaultMessage: 'Draft (Never Published)',
+    description: 'Text for the Discussions enabled Badge in the status bar.',
+  },
+  statusBarGroupAccessOneGroup: {
+    id: 'course-authoring.course-unit.status-bar.access.one-group',
+    defaultMessage: 'Access: {groupName}',
+    description: 'Text in the status bar when the access for the unit is for one group',
+  },
+  statusBarGroupAccessMultipleGroup: {
+    id: 'course-authoring.course-unit.status-bar.access.multiple-group',
+    defaultMessage: 'Access: {groupsCount} Groups',
+    description: 'Text in the status bar when the access for the unit is for one group',
+  },
+  statusBarLiveBadge: {
+    id: 'course-authoring.course-unit.status-bar.visibility.chip',
+    defaultMessage: 'Live',
+    description: 'Text for the Live Badge in the status bar.',
+  },
+  statusBarStaffOnly: {
+    id: 'course-authoring.course-unit.status-bar.visibility.staff-only',
+    defaultMessage: 'Staff Only',
+    description: 'Text for the Staff Only Badge in the status bar.',
+  },
+  statusBarScheduledBadge: {
+    id: 'course-authoring.course-unit.status-bar.visibility.scheduled',
+    defaultMessage: 'Scheduled',
+    description: 'Text for the Upcoming Badge in the status bar.',
+  },
 });
 
 export default messages;
