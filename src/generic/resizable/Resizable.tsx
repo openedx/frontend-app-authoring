6ea433168d06657c9053c8e60236eb1c677905c7
Navin Karkera <navin@opencraft.com>
2026-01-27 20:22:10 +0530

    feat: resizable and responsive sidebar

---
+++ src/generic/resizable/Resizable.tsx
@@ -0,0 +1,71 @@
+import { useWindowSize } from "@openedx/paragon";
+import React, { useRef, useState, useCallback, useMemo } from "react";
+
+const MIN_WIDTH = 440; // px
+
+interface ResizableBoxProps {
+  children: React.ReactNode;
+  minWidth?: number;
+  maxWidth?: number
+}
+
+/**
+ * Creates a resizable box that can be dragged to resize the width from the left side.
+ */
+export const ResizableBox = ({
+  children,
+  minWidth = MIN_WIDTH,
+  maxWidth,
+}: ResizableBoxProps) => {
+  const boxRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState<number>(minWidth); // initial width
+  const { width: windowWidth } = useWindowSize();
+
+  // Store the start values while dragging
+  const startXRef = useRef<number>(0);
+  const startWidthRef = useRef<number>(0);
+  const defaultMaxWidth = useMemo(() => {
+    if (!windowWidth) {
+      return Infinity;
+    }
+    return Math.abs(windowWidth * 0.65);
+  }, [windowWidth]);
+
+  const onMouseDown = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault(); // prevent text selection
+    startXRef.current = e.clientX;
+    startWidthRef.current = width;
+
+    // Attach listeners to the whole document so dragging works even outside the box
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  }, [width]);
+
+  const onMouseMove = useCallback((e: MouseEvent) => {
+    const dx = e.clientX - startXRef.current; // positive = mouse moved right
+    const newWidth = Math.min(
+      Math.max(startWidthRef.current - dx, minWidth),
+      maxWidth || defaultMaxWidth,
+    );
+    setWidth(newWidth);
+  }, [maxWidth, minWidth, defaultMaxWidth]);
+
+  const onMouseUp = useCallback(() => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  }, [onMouseMove]);
+
+  return (
+      <div
+        className="resizable"
+        ref={boxRef}
+        style={{ width: `${width}px` }}
+      >
+        <div className="resizable-handle" onMouseDown={onMouseDown} />
+        <div className="w-100">
+          {children}
+        </div>
+      </div>
+  );
+};
+
