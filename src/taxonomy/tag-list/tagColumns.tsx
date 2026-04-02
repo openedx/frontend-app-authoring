9b79bf928066cabf23692bb1ce03e93b72c2a62c
Navin Karkera <navin@opencraft.com>
2026-04-02 16:27:22 +0530

    fix: unrelated lint issues

--- src/taxonomy/tag-list/tagColumns.tsx
+++ src/taxonomy/tag-list/tagColumns.tsx
@@ -12,6 +12,7 @@ import {
 import { FormattedMessage, useIntl } from '@edx/frontend-platform/i18n';
 import type { Row } from '@tanstack/react-table';
 
+import type { IntlShape } from 'react-intl';
 import messages from './messages';
 import type {
   RowId,
