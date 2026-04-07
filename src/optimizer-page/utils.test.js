1450e0affe6d9ff1dab7c901671ed8c7f2347530
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2026-04-07 19:43:52 +0500

    test: code coverage fixed

--- src/optimizer-page/utils.test.js
+++ src/optimizer-page/utils.test.js
@@ -117,4 +117,18 @@ describe('buildBlockContainerUrl', () => {
       `${window.location.origin}/course/${courseId}/container/${unitId}#${blockId}`,
     );
   });
+
+  it('should return only the internal path when window is undefined', () => {
+    const courseId = 'course-v1:Test+Course+2024';
+    const unitId = 'unit123';
+    const blockId = 'block456';
+
+    Object.defineProperty(globalThis, 'window', { value: undefined, configurable: true, writable: true });
+
+    const result = buildBlockContainerUrl(courseId, unitId, blockId);
+
+    Object.defineProperty(globalThis, 'window', { value: globalThis, configurable: true, writable: true });
+
+    expect(result).toBe(`/course/${courseId}/container/${unitId}#${blockId}`);
+  });
 });
