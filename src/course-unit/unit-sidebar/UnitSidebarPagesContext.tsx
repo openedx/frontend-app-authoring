fe9bfa6a0ad468c42c44a487c1c5fe8bdebbcd13
Rômulo Penido <romulo@opencraft.com>
2026-04-29 17:24:11 -0300

    fix: fixes from review
    Co-authored-by: Navin Karkera <navin@opencraft.com>

--- src/course-unit/unit-sidebar/UnitSidebarPagesContext.tsx
+++ src/course-unit/unit-sidebar/UnitSidebarPagesContext.tsx
@@ -93,7 +93,7 @@ export const UnitSidebarPagesProvider = ({ children }: UnitSidebarPagesProviderP
 
   const sidebarPages = useMemo(
     () => getUnitSidebarPages(readOnly, disableAdd),
-    [readOnly, hasComponentSelected],
+    [readOnly, disableAdd],
   );
 
   return (
