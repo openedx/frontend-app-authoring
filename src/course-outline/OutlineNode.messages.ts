c132c6cbea3d91858f6d3a62b1f3f4bf5ad4ba60
Navin Karkera <navin@opencraft.com>
2026-07-02 16:18:42 +0530

    refactor(course-outline): consolidate outline node messages

--- src/course-outline/OutlineNode.messages.ts
+++ src/course-outline/OutlineNode.messages.ts
@@ -1,6 +1,10 @@
 import { defineMessages } from '@edx/frontend-platform/i18n';
 
 const messages = defineMessages({
+  sectionHighlightsBadge: {
+    id: 'course-authoring.course-outline.section.badge.section-highlights',
+    defaultMessage: 'Section highlights',
+  },
   pasteButton: {
     id: 'course-authoring.course-outline.subsection.button.paste-unit',
     defaultMessage: 'Paste unit',
