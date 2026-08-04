2b1b1ae10804bb3fb3663e85de123372c1b3ec63
Navin Karkera <navin@opencraft.com>
2026-08-04 15:03:59 +0530

    fix: lint issues

--- src/course-updates/data/api.ts
+++ src/course-updates/data/api.ts
@@ -1,6 +1,5 @@
 import { getConfig } from '@edx/frontend-platform';
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
-import { courseIDtoBlockID } from '@src/course-outline/utils';
 
 export interface CourseUpdate {
   id: number;
