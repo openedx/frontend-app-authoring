99ca9c9ae5f5248d17fa18fe3f6ec352ee063e70
Anshu Anjna <137943025+anshuanjna@users.noreply.github.com>
2026-08-24 12:36:40 -0400

    fix: Fix course file sorting regression(#3096) (backport #3184) (#3203)

---
+++ src/files-and-videos/files-page/data/utils.d.ts
@@ -0,0 +1,31 @@
+export interface RawFile {
+  id: string;
+  displayName: string;
+  contentType: string;
+  dateAdded: string;
+  locked: boolean;
+  thumbnail?: string | null;
+  externalUrl?: string;
+  usageLocations: unknown[];
+  [key: string]: unknown;
+}
+
+export interface UpdatedFile extends Omit<RawFile, 'dateAdded'> {
+  wrapperType: string;
+  lockStatus: string;
+  activeStatus: string;
+  dateAdded: number;
+}
+
+export declare function updateFileValues(files: RawFile[]): UpdatedFile[];
+
+export declare function getSrc(file: {
+  thumbnail?: string | null;
+  wrapperType: string;
+  externalUrl?: string;
+}): string;
+
+export declare function getUploadConflicts(
+  filesToUpload: File[],
+  assets: { displayName: string; }[],
+): [Record<string, File>, File[]];
